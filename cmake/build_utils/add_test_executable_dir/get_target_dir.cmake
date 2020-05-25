3d86d05420f8d9dbe23a509743f785821f522c8a
Ananta Yudica <nta040689@gmail.com>
2020-05-25 08:29:33 +0700

    Added implementation function add_test_executable_dir_get_target_dir

---
+++ cmake/build_utils/add_test_executable_dir/get_target_dir.cmake
@@ -0,0 +1,18 @@
+function(add_test_executable_dir_get_target_dir out_dir_name)
+    
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
+    cmake_parse_arguments(add_test_executable_dir_get_target_dir "" 
+        "${one_options}" "ARGS" ${ARGN}) 
+
+    set(dirname "${add_test_executable_dir_get_target_dir_DIRNAME}")
+    set(include_dir ${add_test_executable_dir_get_target_name_INCLUDE_DIR})
+
+    include_build_util(NAME upper_str 
+        INCLUDE_DIR ${include_dir})
+        
+    upper_str("${dirname}" dirname INDEX 0 LENGTH 1)
+
+    set(${out_dir_name} "${dirname}" PARENT_SCOPE)
+
+endfunction(add_test_executable_dir_get_target_dir)
