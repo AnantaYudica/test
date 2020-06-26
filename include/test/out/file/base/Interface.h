f813a0481466b569f08bad3a9c0c084bacf52ba7
Ananta Yudica <nta040689@gmail.com>
2020-06-26 09:07:21 +0700

    Added class test::out::file::base::Interface

---
+++ include/test/out/file/base/Interface.h
@@ -0,0 +1,54 @@
+#ifndef TEST_OUT_FILE_BASE_INTERFACE_H_
+#define TEST_OUT_FILE_BASE_INTERFACE_H_
+
+#include "../../../Guard.h"
+
+namespace test
+{
+namespace out
+{
+namespace file
+{
+namespace base
+{
+
+template<typename TStatus>
+class Interface
+{
+protected:
+    inline Interface() = default;
+public:
+    virtual ~Interface() = default;
+public:
+    Interface(const Interface<TStatus> &) = delete;
+    Interface(Interface<TStatus> &&) = delete;
+public:
+    Interface<TStatus>& operator=(const Interface<TStatus>&) = delete;
+    Interface& operator=(Interface<TStatus>&&) = delete;
+protected:
+    virtual TStatus& GetStatus() = 0;
+    virtual TStatus GetStatus() const = 0;
+public:
+    virtual bool BeginPrint() = 0;
+    virtual void EndPrint() = 0;
+public:
+    virtual bool BeginPuts() = 0;
+    virtual void EndPuts() = 0;
+public:
+    virtual bool BeginLastOutput() = 0;
+    virtual void EndLastOutput() = 0;
+protected:
+    virtual test::Guard<Interface<TStatus>> PrintGuard() = 0;
+    virtual test::Guard<Interface<TStatus>> PutsGuard() = 0;
+    virtual test::Guard<Interface<TStatus>> LastOutputGuard() = 0;
+};
+
+} //!base
+
+} //!file
+
+} //!out
+
+} //!test
+
+#endif //!TEST_OUT_FILE_BASE_INTERFACE_H_
