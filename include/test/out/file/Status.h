9f1cd4951b3bffdd69ff3bdc37f1f24986056ac2
Ananta Yudica <nta040689@gmail.com>
2020-06-28 04:30:32 +0700

    Added bad values Added require values Added / Updated member functions

--- include/test/out/file/Status.h
+++ include/test/out/file/Status.h
@@ -22,26 +22,39 @@ public:
 public:
     typedef TValue ValueType;
     typedef TIntegerValue IntegerValueType;
-public:
-    static constexpr IntegerValueType output_mask = 0x03;
+private:
+    static constexpr IntegerValueType require_mask = 0x01;
+    static constexpr IntegerValueType output_mask = 0x06;
+    static constexpr IntegerValueType bad_offset = 3;
 public:
     enum : IntegerValueType
     {
         good = 0,
         bad = IntegerValueType(~((IntegerValueType)-1 >> 1)),
-        standard_output = 1,
-        file_output = 2,
-        open_failed = bad | (IntegerValueType(1) << 2),
-        print_buffer_failed = bad | (2 << 2),
-        print_output_failed = bad | (3 << 2),
-        print_buffer_overflow = bad | (4 << 2),
-        puts_output_failed = bad | (5 << 2),
-        puts_buffer_overflow = bad | (6 << 2)
+        require_initialize = 1,
+        standard_output = 2,
+        file_output = 4,
+        open_file_failed = bad | (1 << bad_offset),
+        open_file_mode_undefined = bad | (2 << bad_offset),
+        close_file_failed = bad | (3 << bad_offset),
+        size_file_close_failed = bad | (4 << bad_offset),
+        size_file_seek_failed = bad | (5 << bad_offset),
+        size_file_tell_failed = bad | (6 << bad_offset),
+        size_overflow = bad | (7 << bad_offset),
+        unresolve_size_overflow = bad | (8 << bad_offset),
+        initialize_failed = bad | (9 << bad_offset),
+        print_buffer_failed = bad | (10 << bad_offset),
+        print_output_failed = bad | (11 << bad_offset),
+        print_buffer_overflow = bad | (12 << bad_offset),
+        puts_output_failed = bad | (13 << bad_offset),
+        puts_buffer_overflow = bad | (14 << bad_offset)
     };
 private:
     ValueType m_val;
 public:
     Status();
+    Status(const IntegerValueType& val);
+public:
     ~Status();
 public:
     Status(const Status<TValue, TIntegerValue>& cpy);
@@ -55,8 +68,15 @@ public:
     bool IsGood() const;
     bool IsBad() const;
 public:
+    bool IsRequire(const IntegerValueType& val) const;
+public:
+    bool IsRequireInitialize() const;
+public:
     bool IsStandardOutput() const;
     bool IsFileOutput() const;
+public:
+    void Require(const IntegerValueType& val);
+    void Unrequire(const IntegerValueType& val);
 public:
     bool StandardOutput();
     bool FileOutput();
@@ -65,12 +85,18 @@ public:
 public:
     bool Bad(const IntegerValueType& code);
 public:
-    IntegerValueType BadCode() const;
+    IntegerValueType GetBadCode() const;
 };
 
 template<typename TValue, typename TIntegerValue>
 Status<TValue, TIntegerValue>::Status() :
     m_val(good)
+{}
+
+template<typename TValue, typename TIntegerValue>
+Status<TValue, TIntegerValue>::
+    Status(const IntegerValueType& val) :
+        m_val(val & (require_mask | output_mask))
 {}
 
 template<typename TValue, typename TIntegerValue>
@@ -123,6 +149,19 @@ bool Status<TValue, TIntegerValue>::IsBad() const
 }
 
 template<typename TValue, typename TIntegerValue>
+bool Status<TValue, TIntegerValue>::
+    IsRequire(const IntegerValueType& val) const
+{
+    return m_val & (val & require_mask);
+}
+
+template<typename TValue, typename TIntegerValue>
+bool Status<TValue, TIntegerValue>::IsRequireInitialize() const
+{
+    return m_val & require_initialize;
+}
+
+template<typename TValue, typename TIntegerValue>
 bool Status<TValue, TIntegerValue>::IsStandardOutput() const
 {
     return m_val & standard_output;
@@ -132,6 +171,18 @@ template<typename TValue, typename TIntegerValue>
 bool Status<TValue, TIntegerValue>::IsFileOutput() const
 {
     return m_val & file_output;
+}
+
+template<typename TValue, typename TIntegerValue>
+void Status<TValue, TIntegerValue>::Require(const IntegerValueType& val)
+{
+    m_val |= (val & require_mask);
+}
+
+template<typename TValue, typename TIntegerValue>
+void Status<TValue, TIntegerValue>::Unrequire(const IntegerValueType& val)
+{
+    m_val ^= (val & require_mask);
 }
 
 template<typename TValue, typename TIntegerValue>
@@ -160,15 +211,15 @@ template<typename TValue, typename TIntegerValue>
 bool Status<TValue, TIntegerValue>::Bad(const IntegerValueType& code)
 {
     if (m_val & bad) return false;
-    m_val |= (code & ~output_mask);
+    m_val |= (code & ~(output_mask | require_mask));
     return true;
 }
 
 template<typename TValue, typename TIntegerValue>
 typename Status<TValue, TIntegerValue>::IntegerValueType 
-Status<TValue, TIntegerValue>::BadCode() const
+Status<TValue, TIntegerValue>::GetBadCode() const
 {
-    return m_val & ~output_mask;
+    return m_val & ~(output_mask | require_mask);
 }
 
 } //!file
