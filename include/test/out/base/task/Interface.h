14adc07255afd485b38a17b59403cad2588c9235
Ananta Yudica <nta040689@gmail.com>
2020-07-03 12:53:31 +0700

    Updated template class parameters Updated constructor Removed member variable Removed member functions

--- include/test/out/base/task/Interface.h
+++ include/test/out/base/task/Interface.h
@@ -1,11 +1,9 @@
 #ifndef TEST_OUT_BASE_TASK_INTERFACE_H_
 #define TEST_OUT_BASE_TASK_INTERFACE_H_
 
-#include "../../CString.h"
-#include "../../../Guard.h"
+#include "../../Interface.h"
 #include "Buffer.h"
 
-#include <utility>
 #include <cstdint>
 
 namespace test
@@ -17,96 +15,31 @@ namespace base
 namespace task
 {
 
-template<typename TChar, typename TStatus>
+template<typename TChar>
 class Interface
 {
 public:
-    typedef TStatus StatusType;
     typedef test::out::base::task::Buffer<TChar,
-        test::out::base::task::Interface<TChar, TStatus>> BufferType;
-private:
-    StatusType m_status;
+        test::out::base::task::Interface<TChar>> BufferType;
 protected:
-    Interface();
-    template<typename... TArgs>
-    Interface(TArgs&&... args);
+    Interface() = default;
 public:
-    virtual ~Interface();
+    virtual ~Interface() = default;
 public:
-    Interface(const Interface<TChar, TStatus>&) = delete;
-    Interface(Interface<TChar, TStatus>&& mov);
+    Interface(const Interface<TChar>&) = delete;
+    Interface(Interface<TChar>&& mov) = default;
 public:
-    Interface<TChar, TStatus>& 
-        operator=(const Interface<TChar, TStatus>&) = delete;
-    Interface<TChar, TStatus>& 
-        operator=(Interface<TChar, TStatus>&&) = delete;
+    Interface<TChar>& operator=(const Interface<TChar>&) = delete;
+    Interface<TChar>& operator=(Interface<TChar>&&) = delete;
 public:
-    virtual bool _BeginExecute() = 0;
-    virtual void _EndExecute() = 0;
-public:
-    virtual bool _BeginAssign() = 0;
-    virtual void _EndAssign() = 0;
-public:
-    virtual bool _BeginRelease() = 0;
-    virtual void _EndRelease() = 0;
-public:
-    virtual bool _BeginBuffer() = 0;
-    virtual void _EndBuffer() = 0;
-public:
-    virtual bool Execute(test::out::Interface& out) = 0;
+    virtual bool Execute(test::out::Interface<TChar>& out) = 0;
 public:
     virtual bool Assign(const std::intptr_t& deleg_id) = 0;
 public:
     virtual bool Release(const std::intptr_t& deleg_id) = 0;
 public:
     virtual BufferType Buffer(const std::intptr_t& deleg_id) = 0;
-public:
-    virtual test::Guard<test::out::base::task::Interface<TChar, TStatus>>
-        ExecuteGuard() = 0;
-    virtual test::Guard<test::out::base::task::Interface<TChar, TStatus>>
-        AssignGuard() = 0;
-    virtual test::Guard<test::out::base::task::Interface<TChar, TStatus>>
-        ReleaseGuard() = 0;
-    virtual test::Guard<test::out::base::task::Interface<TChar, TStatus>>
-        BufferGuard() = 0;
-public:
-    StatusType& GetStatus();
-    StatusType GetStatus() const;
 };
-
-template<typename TChar, typename TStatus>
-Interface<TChar, TStatus>::Interface() :
-    m_status()
-{}
-
-template<typename TChar, typename TStatus>
-template<typename... TArgs>
-Interface<TChar, TStatus>::Interface(TArgs&&... args) :
-    m_status(std::forward<TArgs>(args)...)
-{}
-
-template<typename TChar, typename TStatus>
-Interface<TChar, TStatus>::~Interface()
-{}
-
-template<typename TChar, typename TStatus>
-Interface<TChar, TStatus>::Interface(Interface<TChar, TStatus>&& mov) :
-    m_status(std::move(mov.m_status))
-{}
-
-template<typename TChar, typename TStatus>
-typename Interface<TChar, TStatus>::StatusType& 
-Interface<TChar, TStatus>::GetStatus()
-{
-    return m_status;
-}
-
-template<typename TChar, typename TStatus>
-typename Interface<TChar, TStatus>::StatusType 
-Interface<TChar, TStatus>::GetStatus() const
-{
-    return {m_status};
-}
 
 } //!task
 
