dc3be66ed4dd97d79f049da3b48adf85bae8ea15
Ananta Yudica <nta040689@gmail.com>
2020-07-07 19:05:56 +0700

    Updated test

--- test/out/base/deleg/Status.main.cpp
+++ test/out/base/deleg/Status.main.cpp
@@ -15,24 +15,24 @@ int main()
         assert(s1.GetBadCode() == StatusType::good);
 
         StatusType s2;
-        assert(s2.Bad(StatusType::task_and_output_nullptr) == true);
+        assert(s2.Bad(StatusType::task_and_base_intf_nullptr) == true);
         assert(s2.IsGood() == false);
         assert(s2.IsBad() == true);
-        assert(s2.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s2.GetBadCode() == StatusType::task_and_base_intf_nullptr);
         assert(s2.Bad(StatusType::task_nullptr) == false);
-        assert(s2.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s2.GetBadCode() == StatusType::task_and_base_intf_nullptr);
 
         StatusType s3{s2};
         
         assert(s3.IsGood() == false);
         assert(s3.IsBad() == true);
-        assert(s3.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s3.GetBadCode() == StatusType::task_and_base_intf_nullptr);
 
         StatusType s4{std::move(s2)};
         
         assert(s4.IsGood() == false);
         assert(s4.IsBad() == true);
-        assert(s4.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s4.GetBadCode() == StatusType::task_and_base_intf_nullptr);
         
         assert(s2.IsGood() == true);
         assert(s2.IsBad() == false);
@@ -43,14 +43,14 @@ int main()
 
         assert(s5.IsGood() == false);
         assert(s5.IsBad() == true);
-        assert(s5.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s5.GetBadCode() == StatusType::task_and_base_intf_nullptr);
         
         StatusType s6;
         s6 = std::move(s3);
         
         assert(s6.IsGood() == false);
         assert(s6.IsBad() == true);
-        assert(s6.GetBadCode() == StatusType::task_and_output_nullptr);
+        assert(s6.GetBadCode() == StatusType::task_and_base_intf_nullptr);
         
         assert(s3.IsGood() == true);
         assert(s3.IsBad() == false);
