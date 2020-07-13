eaeaddbb1f31adcd4353b83244536018305926fa
Ananta Yudica <nta040689@gmail.com>
2020-07-13 23:22:22 +0700

    Updated test

--- test/msg/fmt/Status.main.cpp
+++ test/msg/fmt/Status.main.cpp
@@ -12,30 +12,50 @@ int main()
         StatusType s1;
         assert(s1.IsGood() == true);
         assert(s1.IsBad() == false);
+        assert(s1.IsSetValue() == false);
         assert(s1.GetBadCode() == StatusType::good);
 
         StatusType s2;
+
+        auto res = s2.SetValue();
+        assert(res == true);
+        assert(s2.IsGood() == true);
+        assert(s2.IsBad() == false);
+        assert(s2.IsSetValue() == true);
+        assert(s2.GetBadCode() == StatusType::good);
+
         assert(s2.Bad(StatusType::bad) == true);
         assert(s2.IsGood() == false);
         assert(s2.IsBad() == true);
+        assert(s2.IsSetValue() == true);
         assert(s2.GetBadCode() == StatusType::bad);
         assert(s2.Bad(StatusType::bad) == false);
+        assert(s2.GetBadCode() == StatusType::bad);
+
+        res = s2.SetValue();
+        assert(res == false);
+        assert(s2.IsGood() == false);
+        assert(s2.IsBad() == true);
+        assert(s2.IsSetValue() == true);
         assert(s2.GetBadCode() == StatusType::bad);
 
         StatusType s3{s2};
         
         assert(s3.IsGood() == false);
         assert(s3.IsBad() == true);
+        assert(s3.IsSetValue() == true);
         assert(s3.GetBadCode() == StatusType::bad);
 
         StatusType s4{std::move(s2)};
         
         assert(s4.IsGood() == false);
         assert(s4.IsBad() == true);
+        assert(s4.IsSetValue() == true);
         assert(s4.GetBadCode() == StatusType::bad);
         
         assert(s2.IsGood() == true);
         assert(s2.IsBad() == false);
+        assert(s2.IsSetValue() == false);
         assert(s2.GetBadCode() == StatusType::good);
 
         StatusType s5;
@@ -43,6 +63,7 @@ int main()
 
         assert(s5.IsGood() == false);
         assert(s5.IsBad() == true);
+        assert(s5.IsSetValue() == true);
         assert(s5.GetBadCode() == StatusType::bad);
         
         StatusType s6;
@@ -50,16 +71,19 @@ int main()
         
         assert(s6.IsGood() == false);
         assert(s6.IsBad() == true);
+        assert(s6.IsSetValue() == true);
         assert(s6.GetBadCode() == StatusType::bad);
         
         assert(s3.IsGood() == true);
         assert(s3.IsBad() == false);
+        assert(s3.IsSetValue() == false);
         assert(s3.GetBadCode() == StatusType::good);
 
         s6.Reset();
         
         assert(s6.IsGood() == true);
         assert(s6.IsBad() == false);
+        assert(s6.IsSetValue() == false);
         assert(s6.GetBadCode() == StatusType::good);
     }
     
