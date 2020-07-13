6b96d0a5814a70e79db890390cbea4593bc5d922
Ananta Yudica <nta040689@gmail.com>
2020-07-13 22:40:10 +0700

    Added member alias types Added constructor Updated return type member functions Added member functions

--- include/test/msg/fmt/Parameter.h
+++ include/test/msg/fmt/Parameter.h
@@ -2,9 +2,12 @@
 #define TEST_MSG_FMT_PARAMETER_H_
 
 #include "../../out/Interface.h"
+#include "Status.h"
 
 #include <cstdarg>
 #include <cstddef>
+#include <cstdint>
+#include <utility>
 
 namespace test
 {
@@ -16,15 +19,50 @@ namespace fmt
 template<typename TChar>
 class Parameter
 {
+public:
+    typedef test::msg::fmt::Status<std::uint8_t> StatusType;
+    typedef test::out::Interface<TChar> OutputInterfaceType;
+private:
+    StatusType m_status;
 protected:
-    Parameter() = default;
+    Parameter();
+    template<typename... TArgs>
+    Parameter(TArgs&&... args);
 public:
     virtual ~Parameter() = default;
 public:
-    virtual bool VLoad(std::size_t size, std::size_t index, 
+    virtual std::size_t VLoad(std::size_t size, std::size_t index, 
         va_list args) = 0;
-    virtual bool Load(std::size_t size, ...) = 0;
+    virtual std::size_t Load(std::size_t size, ...) = 0;
+protected:
+    StatusType& GetStatus();
+    StatusType GetStatus() const;
 };
+
+template<typename TChar>
+Parameter<TChar>::Parameter() :
+    m_status()
+{}
+
+template<typename TChar>
+template<typename... TArgs>
+Parameter<TChar>::Parameter(TArgs&&... args) :
+    m_status(std::forward<TArgs>(args)...)
+{}
+
+template<typename TChar>
+typename Parameter<TChar>::StatusType& 
+Parameter<TChar>::GetStatus()
+{
+    return m_status;
+}
+
+template<typename TChar>
+typename Parameter<TChar>::StatusType 
+Parameter<TChar>::GetStatus() const
+{
+    return {m_status};
+}
 
 } //!fmt
 
