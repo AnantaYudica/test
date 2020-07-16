888a4cfcdc953900b40430613bbabe5ab88bdabd
Ananta Yudica <nta040689@gmail.com>
2020-07-17 03:43:09 +0700

    Added class Width

---
+++ include/test/msg/fmt/param/flag/arg/Width.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_WIDTH_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_WIDTH_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Width
+{
+public:
+    constexpr Width() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_WIDTH_H_
+
