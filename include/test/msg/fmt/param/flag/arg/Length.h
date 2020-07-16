a9ba46f0cf9441d8be85a1215345cfe49640ea78
Ananta Yudica <nta040689@gmail.com>
2020-07-17 03:43:22 +0700

    Added class Length

---
+++ include/test/msg/fmt/param/flag/arg/Length.h
@@ -0,0 +1,37 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+template<typename T>
+class Length
+{
+public:
+    constexpr Length() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+
