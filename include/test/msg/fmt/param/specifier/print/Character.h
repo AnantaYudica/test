7c293a113ecbb0af726c17e1c8c3f1869bb0cfc9
Ananta Yudica <nta040689@gmail.com>
2020-07-18 06:13:46 +0700

    Added class Character for handle print

---
+++ include/test/msg/fmt/param/specifier/print/Character.h
@@ -0,0 +1,163 @@
+#ifndef TEST_MSG_FMT_PARAM_SPECIFIER_PRINT_CHARACTER_H_
+#define TEST_MSG_FMT_PARAM_SPECIFIER_PRINT_CHARACTER_H_
+
+#include "../../flag/Character.h"
+#include "../val/Character.h"
+#include "../../Specifier.h"
+#include "../../Width.h"
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace specifier
+{
+namespace print
+{
+
+template<typename TChar, int ValueFlag>
+class Character
+{
+public:
+    typedef test::msg::fmt::param::Width<TChar> WidthType;
+    typedef test::msg::fmt::param::specifier::val::Character ValueType;
+    typedef test::msg::fmt::param::Specifier<TChar> SpecifierBaseType;
+    typedef typename SpecifierBaseType::OutputInterfaceType 
+        OutputInterfaceType;
+    typedef typename OutputInterfaceType::SizeType SizeType;
+public:
+    static SizeType Print(OutputInterfaceType& out, const WidthType& width,
+        const ValueType& val);
+public:
+    Character() = delete;
+};
+
+template<typename TChar>
+class Character<TChar, test::msg::fmt::param::flag::Character::char_format>
+{
+public:
+    typedef test::msg::fmt::param::Width<TChar> WidthType;
+    typedef test::msg::fmt::param::specifier::val::Character ValueType;
+    typedef test::msg::fmt::param::Specifier<TChar> SpecifierBaseType;
+    typedef typename SpecifierBaseType::OutputInterfaceType 
+        OutputInterfaceType;
+    typedef typename OutputInterfaceType::SizeType SizeType;
+public:
+    static SizeType Print(OutputInterfaceType& out, const WidthType& width,
+        const ValueType& val);
+public:
+    Character() = delete;
+};
+
+template<typename TChar>
+class Character<TChar, test::msg::fmt::param::flag::Character::w_char_format>
+{
+public:
+    typedef test::msg::fmt::param::Width<TChar> WidthType;
+    typedef test::msg::fmt::param::specifier::val::Character ValueType;
+    typedef test::msg::fmt::param::Specifier<TChar> SpecifierBaseType;
+    typedef typename SpecifierBaseType::OutputInterfaceType 
+        OutputInterfaceType;
+    typedef typename OutputInterfaceType::SizeType SizeType;
+public:
+    static SizeType Print(OutputInterfaceType& out, const WidthType& width,
+        const ValueType& val);
+public:
+    Character() = delete;
+};
+
+template<typename TChar>
+class Character<TChar, test::msg::fmt::param::flag::Character::wchar_format>
+{
+public:
+    typedef test::msg::fmt::param::Width<TChar> WidthType;
+    typedef test::msg::fmt::param::specifier::val::Character ValueType;
+    typedef test::msg::fmt::param::Specifier<TChar> SpecifierBaseType;
+    typedef typename SpecifierBaseType::OutputInterfaceType 
+        OutputInterfaceType;
+    typedef typename OutputInterfaceType::SizeType SizeType;
+public:
+    static SizeType Print(OutputInterfaceType& out, const WidthType& width,
+        const ValueType& val);
+public:
+    Character() = delete;
+};
+
+template<typename TChar>
+class Character<TChar, test::msg::fmt::param::flag::Character::w_wchar_format>
+{
+public:
+    typedef test::msg::fmt::param::Width<TChar> WidthType;
+    typedef test::msg::fmt::param::specifier::val::Character ValueType;
+    typedef test::msg::fmt::param::Specifier<TChar> SpecifierBaseType;
+    typedef typename SpecifierBaseType::OutputInterfaceType 
+        OutputInterfaceType;
+    typedef typename OutputInterfaceType::SizeType SizeType;
+public:
+    static SizeType Print(OutputInterfaceType& out, const WidthType& width,
+        const ValueType& val);
+public:
+    Character() = delete;
+};
+
+template<typename TChar, int ValueFlag>
+typename Character<TChar, ValueFlag>::SizeType 
+Character<TChar, ValueFlag>::Print(OutputInterfaceType&, 
+    const WidthType&, const ValueType&)
+{
+    return 0;
+}
+
+template<typename TChar>
+typename Character<TChar, 
+    test::msg::fmt::param::flag::Character::char_format>::SizeType
+Character<TChar, test::msg::fmt::param::flag::Character::char_format>::
+    Print(OutputInterfaceType& out, const WidthType&, const ValueType& val)
+{
+    return out.Print("%c", val.char_value);
+}
+
+template<typename TChar>
+typename Character<TChar, 
+    test::msg::fmt::param::flag::Character::w_char_format>::SizeType
+Character<TChar, test::msg::fmt::param::flag::Character::w_char_format>::
+    Print(OutputInterfaceType& out, const WidthType& width, const ValueType& val)
+{
+    return out.Print("%*c", width.GetValue(), val.char_value);
+}
+
+template<typename TChar>
+typename Character<TChar, 
+    test::msg::fmt::param::flag::Character::wchar_format>::SizeType
+Character<TChar, test::msg::fmt::param::flag::Character::wchar_format>::
+    Print(OutputInterfaceType& out, const WidthType&, const ValueType& val)
+{
+    return out.Print("%lc", val.char_value);
+}
+
+template<typename TChar>
+typename Character<TChar, 
+    test::msg::fmt::param::flag::Character::w_wchar_format>::SizeType
+Character<TChar, test::msg::fmt::param::flag::Character::w_wchar_format>::
+    Print(OutputInterfaceType& out, const WidthType& width, const ValueType& val)
+{
+    return out.Print("%*lc", width.GetValue(), val.char_value);
+}
+
+} //!print
+
+} //!specifier
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_SPECIFIER_PRINT_CHARACTER_H_
