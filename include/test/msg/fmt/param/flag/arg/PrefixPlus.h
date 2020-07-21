2d3a3cba22712ec9f211fbd3e9857b93f176894e
Ananta Yudica <nta040689@gmail.com>
2020-07-21 13:32:19 +0700

    Added class PrefixPlus

---
+++ include/test/msg/fmt/param/flag/arg/PrefixPlus.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXPLUS_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXPLUS_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class PrefixPlus
+{
+public:
+    constexpr PrefixPlus() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXPLUS_H_
+
