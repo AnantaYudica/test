952956476f5298f8577cf4d45add2805f3ed83b1
Ananta Yudica <nta040689@gmail.com>
2020-07-21 13:31:42 +0700

    Added class Lower

---
+++ include/test/msg/fmt/param/flag/arg/Lower.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_LOWER_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_LOWER_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Lower
+{
+public:
+    constexpr Lower() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_LOWER_H_
+
