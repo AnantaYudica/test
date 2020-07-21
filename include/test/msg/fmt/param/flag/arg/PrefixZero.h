6a504ec7b72e29d65d6ae090547b73ef4ee80e4e
Ananta Yudica <nta040689@gmail.com>
2020-07-21 13:32:51 +0700

    Added class PrefixZero

---
+++ include/test/msg/fmt/param/flag/arg/PrefixZero.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXZERO_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXZERO_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class PrefixZero
+{
+public:
+    constexpr PrefixZero() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_PREFIXZERO_H_
+
