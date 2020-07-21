4076ba68f056e2c7b442a4f9d941345fc93488df
Ananta Yudica <nta040689@gmail.com>
2020-07-21 13:31:54 +0700

    Added class Upper

---
+++ include/test/msg/fmt/param/flag/arg/Upper.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_UPPER_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_UPPER_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Upper
+{
+public:
+    constexpr Upper() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_UPPER_H_
+
