2d2005eaddf37ad42074b906aa46a9d7b4fd683e
Ananta Yudica <nta040689@gmail.com>
2020-07-21 13:31:30 +0700

    Added class Fixed

---
+++ include/test/msg/fmt/param/flag/arg/Fixed.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_FIXED_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_FIXED_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Fixed
+{
+public:
+    constexpr Fixed() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_FIXED_H_
+
