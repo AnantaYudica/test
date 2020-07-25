24201e2f7b6d7dee9f0d4a730e21af53ae472530
Ananta Yudica <nta040689@gmail.com>
2020-07-25 12:40:16 +0700

    Updated test

--- test/msg/fmt/param/specifier/val/FloatingPoint.main.cpp
+++ test/msg/fmt/param/specifier/val/FloatingPoint.main.cpp
@@ -11,21 +11,21 @@ int main()
     
     FloatingPoint ff1{0};
     ff1.float_value = pi_f;
-    assert(FloatingPoint::GetValue<float>(ff1) == pi_f);
-    assert(FloatingPoint::GetValue<double>(ff1) == pi_d);
-    assert(FloatingPoint::GetValue<long double>(ff1) == pi_ld);
+    assert(FloatingPoint::CastTo<float>(ff1) == pi_f);
+    assert(FloatingPoint::CastTo<double>(ff1) == pi_d);
+    assert(FloatingPoint::CastTo<long double>(ff1) == pi_ld);
 
     FloatingPoint ff2{0};
     ff2.double_value = pi_d;
-    assert(FloatingPoint::GetValue<float>(ff2) == pi_f);
-    assert(FloatingPoint::GetValue<double>(ff2) == pi_d);
-    assert(FloatingPoint::GetValue<long double>(ff2) == pi_ld);
+    assert(FloatingPoint::CastTo<float>(ff2) == pi_f);
+    assert(FloatingPoint::CastTo<double>(ff2) == pi_d);
+    assert(FloatingPoint::CastTo<long double>(ff2) == pi_ld);
 
     FloatingPoint ff3{0};
     ff3.long_double_value = pi_ld;
-    assert(FloatingPoint::GetValue<float>(ff3) == pi_f);
-    assert(FloatingPoint::GetValue<double>(ff3) == pi_d);
-    assert(FloatingPoint::GetValue<long double>(ff3) == pi_ld);
+    assert(FloatingPoint::CastTo<float>(ff3) == pi_f);
+    assert(FloatingPoint::CastTo<double>(ff3) == pi_d);
+    assert(FloatingPoint::CastTo<long double>(ff3) == pi_ld);
     
     return 1;
 }
