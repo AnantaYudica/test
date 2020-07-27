4672fd44ba9482b0b67c9ce087d7ce9e7796d5ad
Ananta Yudica <nta040689@gmail.com>
2020-07-27 14:40:36 +0700

    Added alias type 'Length'

---
+++ include/test/msg/fmt/param/flag/arg/Length.h
@@ -0,0 +1,34 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+
+#include "Precision.h"
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+typedef test::msg::fmt::param::flag::arg::Precision Length;
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_LENGTH_H_
+
