d1a23e47e99c3e9a1ecea0f6ba7090c3788be223
Ananta Yudica <nta040689@gmail.com>
2020-07-27 14:40:58 +0700

    Added class Signed

---
+++ include/test/msg/fmt/param/flag/arg/Signed.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_SIGNED_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_SIGNED_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Signed
+{
+public:
+    constexpr Signed() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_SIGNED_H_
+
