da1cf77085cb07d5d11dc08815ee4c5a81deb9cc
Ananta Yudica <nta040689@gmail.com>
2020-07-27 14:40:47 +0700

    Added class Octal

---
+++ include/test/msg/fmt/param/flag/arg/Octal.h
@@ -0,0 +1,36 @@
+#ifndef TEST_MSG_FMT_PARAM_FLAG_ARG_OCTAL_H_
+#define TEST_MSG_FMT_PARAM_FLAG_ARG_OCTAL_H_
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace param
+{
+namespace flag
+{
+namespace arg
+{
+
+class Octal
+{
+public:
+    constexpr Octal() = default;
+};
+
+} //!arg
+
+} //!flag
+
+} //!param
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_PARAM_FLAG_ARG_OCTAL_H_
+
