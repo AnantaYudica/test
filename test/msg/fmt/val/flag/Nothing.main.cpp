cd28c200a45bd65a409afc10eca0e7c57b7c251b
Ananta Yudica <nta040689@gmail.com>
2020-08-07 06:21:57 +0700

    Updated test

--- test/msg/fmt/val/flag/Nothing.main.cpp
+++ test/msg/fmt/val/flag/Nothing.main.cpp
@@ -1,68 +1,7 @@
 #include "test/msg/fmt/val/flag/Nothing.h"
-#include "test/out/Interface.h"
-#include "test/out/CString.h"
 
 #include <cassert>
-#include <cstring>
-
-template<int Val>
-struct NtFormat
-{
-    auto Output(test::out::Interface<char>& out) -> decltype(out.Print(""))
-    {
-        return 0;
-    }
-};
-
-template<>
-struct NtFormat<test::msg::fmt::val::flag::Nothing::define_char>
-{
-    static auto Output(test::out::Interface<char>& out) -> 
-        decltype(out.Print("_"))
-    {
-        return out.Print("%s", "%hhn");
-    }
-};
-
-template<>
-struct NtFormat<test::msg::fmt::val::flag::Nothing::define_short>
-{
-    static auto Output(test::out::Interface<char>& out) -> 
-        decltype(out.Print("_"))
-    {
-        return out.Print("%s", "%hn");
-    }
-};
-
-template<>
-struct NtFormat<test::msg::fmt::val::flag::Nothing::define_int>
-{
-    static auto Output(test::out::Interface<char>& out) -> 
-        decltype(out.Print("_"))
-    {
-        return out.Print("%s", "%n");
-    }
-};
-
-template<>
-struct NtFormat<test::msg::fmt::val::flag::Nothing::define_long>
-{
-    static auto Output(test::out::Interface<char>& out) -> 
-        decltype(out.Print("_"))
-    {
-        return out.Print("%s", "%ln");
-    }
-};
-
-template<>
-struct NtFormat<test::msg::fmt::val::flag::Nothing::define_long_long>
-{
-    static auto Output(test::out::Interface<char>& out) -> 
-        decltype(out.Print("_"))
-    {
-        return out.Print("%s", "%lln");
-    }
-};
+#include <utility>
 
 int main()
 {
@@ -71,169 +10,112 @@ int main()
     {
         constexpr Nothing nt1{};
 
-        assert(nt1.GetValue() == Nothing::define_int);
+        assert(nt1.GetValue() == Nothing::good);
 
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%n", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<char>{}};
-
-        assert(nt1.GetValue() == Nothing::define_char);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%hhn", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<signed char>{}};
-
-        assert(nt1.GetValue() == Nothing::define_char);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%hhn", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned char>{}};
-
-        assert(nt1.GetValue() == Nothing::define_char);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%hhn", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<short>{}};
-
-        assert(nt1.GetValue() == Nothing::define_short);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%hn", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned short>{}};
-
-        assert(nt1.GetValue() == Nothing::define_short);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%hn", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<int>{}};
-
-        assert(nt1.GetValue() == Nothing::define_int);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%n", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned int>{}};
-
-        assert(nt1.GetValue() == Nothing::define_int);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%n", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<long>{}};
-
-        assert(nt1.GetValue() == Nothing::define_long);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%ln", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned long>{}};
-
-        assert(nt1.GetValue() == Nothing::define_long);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%ln", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<long long>{}};
-
-        assert(nt1.GetValue() == Nothing::define_long_long);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%lln", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned long long>{}};
-
-        assert(nt1.GetValue() == Nothing::define_long_long);
-
-        test::out::CString<char> out_cstr1;
-
-        NtFormat<nt1.GetValue()>::Output(out_cstr1);
-
-        assert(strncmp(*(out_cstr1.Get()), "%lln", 3) == 0);
-    }
-    {
-        constexpr Nothing nt1{arg::Define<unsigned long long>{}};
         constexpr Nothing nt2{nt1};
 
-        assert(nt2.GetValue() == Nothing::define_long_long);
+        assert(nt2.GetValue() == Nothing::good);
 
-        {
-            test::out::CString<char> out_cstr1;
+        constexpr Nothing nt3{std::move(nt2)};
 
-            NtFormat<nt2.GetValue()>::Output(out_cstr1);
+        assert(nt3.GetValue() == Nothing::good);
+        assert(nt2.GetValue() == Nothing::good);
 
-            assert(strncmp(*(out_cstr1.Get()), "%lln", 3) == 0);
-        }
+        Nothing nt4{};
+        nt4 = nt3;
 
-        constexpr Nothing nt3{std::move(nt1)};
+        assert(nt4.GetValue() == Nothing::good);
         
-        assert(nt3.GetValue() == Nothing::define_long_long);
-        {
-            test::out::CString<char> out_cstr1;
+        Nothing nt5{};
+        nt5 = std::move(nt4);
 
-            NtFormat<nt3.GetValue()>::Output(out_cstr1);
+        assert(nt5.GetValue() == Nothing::good);
+        assert(nt4.GetValue() == Nothing::good);
+    }
+    {
+        constexpr Nothing nt1{arg::Width{}};
 
-            assert(strncmp(*(out_cstr1.Get()), "%lln", 3) == 0);
-        }
+        assert(nt1.GetValue() == Nothing::width);
         
-        Nothing nt4, nt5;
-        nt4 = nt1;
+        constexpr Nothing nt2{nt1};
 
-        assert(nt4.GetValue() == Nothing::define_long_long);
-
-        nt5 = std::move(nt1);
+        assert(nt1.GetValue() == Nothing::width);
         
-        assert(nt5.GetValue() == Nothing::define_long_long);
+        constexpr Nothing nt3{std::move(nt2)};
 
+        assert(nt3.GetValue() == Nothing::width);
+        assert(nt2.GetValue() == Nothing::width);
+
+        Nothing nt4{};
+        nt4 = nt3;
+
+        assert(nt4.GetValue() == Nothing::width);
+        
+        Nothing nt5{};
+        nt5 = std::move(nt4);
+
+        assert(nt5.GetValue() == Nothing::width);
+        assert(nt4.GetValue() == Nothing::width);
+    }
+    {
+        constexpr Nothing nt1{arg::Precision{}};
+
+        assert(Nothing::precision == Nothing::length);
+        assert(nt1.GetValue() == Nothing::precision);
+        
+        constexpr Nothing nt2{nt1};
+
+        assert(nt1.GetValue() == Nothing::precision);
+        
+        constexpr Nothing nt3{std::move(nt2)};
+
+        assert(nt3.GetValue() == Nothing::precision);
+        assert(nt2.GetValue() == Nothing::precision);
+
+        Nothing nt4{};
+        nt4 = nt3;
+
+        assert(nt4.GetValue() == Nothing::precision);
+        
+        Nothing nt5{};
+        nt5 = std::move(nt4);
+
+        assert(nt5.GetValue() == Nothing::precision);
+        assert(nt4.GetValue() == Nothing::precision);
+    }
+    
+    {
+        constexpr Nothing nt1{arg::Width{},
+            arg::Precision{}};
+
+        assert(nt1.GetValue() == (Nothing::width |   
+            Nothing::precision));
+        
+        constexpr Nothing nt2{nt1};
+
+        assert(nt1.GetValue() == (Nothing::width |   
+            Nothing::precision));
+        
+        constexpr Nothing nt3{std::move(nt2)};
+
+        assert(nt3.GetValue() == (Nothing::width |   
+            Nothing::precision));
+        assert(nt2.GetValue() == (Nothing::width |   
+            Nothing::precision));
+
+        Nothing nt4{};
+        nt4 = nt3;
+
+        assert(nt4.GetValue() == (Nothing::width |   
+            Nothing::precision));
+        
+        Nothing nt5{};
+        nt5 = std::move(nt4);
+
+        assert(nt5.GetValue() == (Nothing::width |   
+            Nothing::precision));
+        assert(nt4.GetValue() == (Nothing::width |   
+            Nothing::precision));
     }
     return 1;
 }
