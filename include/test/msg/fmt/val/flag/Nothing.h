989e34eb1f9d52677844084342e11ce025951c8f
Ananta Yudica <nta040689@gmail.com>
2020-08-07 06:21:41 +0700

    Updated dependencies Updated constructor Updated member Alias type Updated member enum type Updated member function _Value Removed static member variables

--- include/test/msg/fmt/val/flag/Nothing.h
+++ include/test/msg/fmt/val/flag/Nothing.h
@@ -1,7 +1,9 @@
 #ifndef TEST_MSG_FMT_VAL_FLAG_NOTHING_H_
 #define TEST_MSG_FMT_VAL_FLAG_NOTHING_H_
 
-#include "../../var/arg/Define.h"
+#include "../../var/arg/Width.h"
+#include "../../var/arg/Precision.h"
+#include "../../var/arg/Length.h"
 
 #include <cstdint>
 #include <type_traits>
@@ -22,27 +24,17 @@ class Nothing
 {
 public:
     typedef std::uint8_t ValueType;
-    template<typename T>
-    using DefineType = test::msg::fmt::var::arg::Define<T>;
+    typedef test::msg::fmt::var::arg::Width WidthType;
+    typedef test::msg::fmt::var::arg::Precision PrecisionType;
+    typedef test::msg::fmt::var::arg::Length LengthType; 
 public:
     enum : ValueType
     {
         good = 0,
-        define_char = 1 << 1,
-        define_short = 1 << 2,
-        define_int = 1 << 3,
-        define_long = 1 << 4,
-        define_long_long = 1 << 5,
+        width = 1,
+        precision = 2,
+        length = 2
     }; 
-private:
-    static constexpr ValueType define_type_mask = define_char | define_short |
-        define_int | define_long | define_long_long;
-public:
-    static constexpr ValueType char_format = define_char;
-    static constexpr ValueType short_format = define_short;
-    static constexpr ValueType int_format = define_int;
-    static constexpr ValueType long_format = define_long;
-    static constexpr ValueType long_long_format = define_long_long;
 private:
     ValueType m_value;
 public:
@@ -62,51 +54,24 @@ public:
 private:
     inline constexpr ValueType _Value(ValueType val);
     template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<signed char>&&, TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, DefineType<char>&&, 
+    inline constexpr ValueType _Value(ValueType val, WidthType&&, 
         TArgs&&... args);
     template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<unsigned char>&&, TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, DefineType<short>&&, 
+    inline constexpr ValueType _Value(ValueType val, PrecisionType&&, 
         TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<unsigned short>&&, TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, DefineType<int>&&, 
-        TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<unsigned int>&&, TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, DefineType<long>&&, 
-        TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<unsigned long>&&, TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, DefineType<long long>&&, 
-        TArgs&&... args);
-    template<typename... TArgs>
-    inline constexpr ValueType _Value(ValueType val, 
-        DefineType<unsigned long long>&&, TArgs&&... args);
 public:
     inline constexpr ValueType GetValue() const;
 };
 
 inline constexpr Nothing::Nothing() :
-    m_value(_Value(define_int))
+    m_value(_Value(good))
 {}
 
 template<typename TArg, typename... TArgs, typename std::enable_if<
     !std::is_same<typename std::remove_cv<TArg>::type, Nothing>::value, 
     int>::type>
 inline constexpr Nothing::Nothing(TArg&& arg, TArgs&& ... args) :
-    m_value(_Value(define_int, std::forward<TArg>(arg), 
+    m_value(_Value(good, std::forward<TArg>(arg), 
         std::forward<TArgs>(args)...))
 {}
 
@@ -138,91 +103,16 @@ Nothing::_Value(ValueType val)
 
 template<typename... TArgs>
 inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<signed char>&&, TArgs&&... args)
+Nothing::_Value(ValueType val, WidthType&&, TArgs&&... args)
 {
-    return _Value((val & ~define_type_mask) | define_char, 
-        std::forward<TArgs>(args)...);
+    return _Value(val | width, std::forward<TArgs>(args)...);
 }
 
 template<typename... TArgs>
 inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<char>&&, TArgs&&... args)
+Nothing::_Value(ValueType val, PrecisionType&&, TArgs&&... args)
 {
-    return _Value((val & ~define_type_mask) | define_char, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<unsigned char>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_char, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<short>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_short, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<unsigned short>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_short, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<int>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_int, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<unsigned int>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_int, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<long>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_long, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<unsigned long>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_long, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<long long>&&, TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_long_long, 
-        std::forward<TArgs>(args)...);
-}
-
-template<typename... TArgs>
-inline constexpr typename Nothing::ValueType 
-Nothing::_Value(ValueType val, DefineType<unsigned long long>&&, 
-    TArgs&&... args)
-{
-    return _Value((val & ~define_type_mask) | define_long_long, 
-        std::forward<TArgs>(args)...);
+    return _Value(val | precision, std::forward<TArgs>(args)...);
 }
 
 inline constexpr typename Nothing::ValueType 
