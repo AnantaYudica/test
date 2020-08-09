735de1970759aec9f82e13a2883578651c186039
Ananta Yudica <nta040689@gmail.com>
2020-08-10 06:37:15 +0700

    Added class var::FloatingPoint

---
+++ include/test/msg/fmt/var/FloatingPoint.h
@@ -0,0 +1,150 @@
+#ifndef TEST_MSG_FMT_VAR_FLOATINGPOINT_H_
+#define TEST_MSG_FMT_VAR_FLOATINGPOINT_H_
+
+#include "../Parameter.h"
+#include "../val/specifier/FloatingPoint.h"
+
+#include <cstdarg>
+#include <utility>
+#include <type_traits>
+
+namespace test
+{
+namespace msg
+{
+namespace fmt
+{
+namespace var
+{
+
+template<typename TChar>
+class FloatingPoint :
+    public test::msg::fmt::Parameter<TChar>
+{
+public:
+    typedef test::msg::fmt::val::specifier::FloatingPoint<TChar> SpecifierType;
+    typedef test::msg::fmt::Parameter<TChar> BaseType;
+    typedef typename BaseType::OutputInterfaceType OutputInterfaceType;
+private:
+    SpecifierType m_specifier;
+public:
+    FloatingPoint();
+    template<typename TArg, typename... TArgs, typename _TArg = 
+        typename std::remove_reference<
+            typename std::remove_cv<TArg>::type>::type,
+        typename std::enable_if<!std::is_same<_TArg, 
+            FloatingPoint<TChar>>::value, int>::type = 0>
+    FloatingPoint(TArg&& arg, TArgs&&... args);
+public:
+    ~FloatingPoint();
+public:
+    FloatingPoint(const FloatingPoint<TChar>& cpy);
+    FloatingPoint(FloatingPoint<TChar>&& mov);
+public:
+    FloatingPoint<TChar>& operator=(const FloatingPoint<TChar>& cpy);
+    FloatingPoint<TChar>& operator=(FloatingPoint<TChar>&& mov);
+public:
+    std::size_t VLoad(std::size_t size, std::size_t index, va_list args);
+    std::size_t Load(std::size_t size, ...);
+public:
+    typename OutputInterfaceType::SizeType Output(OutputInterfaceType& out);
+public:
+    void Unset();
+public:
+    bool IsSet() const;
+};
+
+template<typename TChar>
+FloatingPoint<TChar>::FloatingPoint() :
+    test::msg::fmt::Parameter<TChar>(),
+    m_specifier()
+{}
+
+template<typename TChar>
+template<typename TArg, typename... TArgs, typename _TArg,
+    typename std::enable_if<!std::is_same<_TArg, 
+        FloatingPoint<TChar>>::value, int>::type>
+FloatingPoint<TChar>::FloatingPoint(TArg&& arg, TArgs&&... args) :
+    test::msg::fmt::Parameter<TChar>(),
+    m_specifier(std::forward<TArg>(arg), std::forward<TArgs>(args)...)
+{}
+
+template<typename TChar>
+FloatingPoint<TChar>::~FloatingPoint()
+{}
+
+template<typename TChar>
+FloatingPoint<TChar>::FloatingPoint(const FloatingPoint<TChar>& cpy) :
+    test::msg::fmt::Parameter<TChar>(cpy),
+    m_specifier(cpy.m_specifier)
+{}
+
+template<typename TChar>
+FloatingPoint<TChar>::FloatingPoint(FloatingPoint<TChar>&& mov) :
+    test::msg::fmt::Parameter<TChar>(std::move(mov)),
+    m_specifier(std::move(mov.m_specifier))
+{}
+
+template<typename TChar>
+FloatingPoint<TChar>& 
+FloatingPoint<TChar>::operator=(const FloatingPoint<TChar>& cpy)
+{
+    test::msg::fmt::Parameter<TChar>::operator=(cpy);
+    m_specifier = cpy.m_specifier;
+    return *this;
+}
+
+template<typename TChar>
+FloatingPoint<TChar>& 
+FloatingPoint<TChar>::operator=(FloatingPoint<TChar>&& mov)
+{
+    test::msg::fmt::Parameter<TChar>::operator=(std::move(mov));
+    m_specifier = std::move(mov.m_specifier);
+    return *this;
+}
+
+template<typename TChar>
+std::size_t FloatingPoint<TChar>::VLoad(std::size_t size, std::size_t index, 
+    va_list args)
+{
+    return m_specifier.VLoad(size, index, args);
+}
+
+template<typename TChar>
+std::size_t FloatingPoint<TChar>::Load(std::size_t size, ...)
+{
+    va_list args;
+    va_start(args, size);
+    auto ret = VLoad(size, 0, args);
+    va_end(args);
+    return ret;
+}
+
+template<typename TChar>
+typename FloatingPoint<TChar>::OutputInterfaceType::SizeType 
+FloatingPoint<TChar>::Output(OutputInterfaceType& out)
+{
+    return m_specifier.Output(out);
+}
+
+template<typename TChar>
+void FloatingPoint<TChar>::Unset()
+{
+    m_specifier.Unset();
+}
+
+template<typename TChar>
+bool FloatingPoint<TChar>::IsSet() const
+{
+    return m_specifier.IsSet();
+}
+
+} //!var
+
+} //!fmt
+
+} //!msg
+
+} //!test
+
+#endif //!TEST_MSG_FMT_VAR_FLOATINGPOINT_H_
