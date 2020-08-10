a8466a3845e170da69ee791380bc456af087a718
Ananta Yudica <nta040689@gmail.com>
2020-08-10 13:19:23 +0700

    Fixed load values

--- include/test/msg/fmt/val/specifier/Nothing.h
+++ include/test/msg/fmt/val/specifier/Nothing.h
@@ -2,6 +2,8 @@
 #define TEST_MSG_FMT_VAL_SPECIFIER_NOTHING_H_
 
 #include "../Specifier.h"
+#include "../Width.h"
+#include "../Precision.h"
 #include "../flag/Nothing.h"
 
 #include <type_traits>
@@ -33,43 +35,42 @@ public:
     typedef typename OutputInterfaceType::SizeType SizeType;
     typedef test::msg::fmt::val::flag::Nothing FlagType;
     typedef typename FlagType::ValueType IntegerFlagType;
+    typedef test::msg::fmt::val::Width<TChar> WidthType;
+    typedef test::msg::fmt::val::Precision<TChar> PrecisionType;
 private:
-    static bool _SetWidth(std::uint8_t& _default);
+    static bool _SetWidth(WidthType& width);
     template<typename TArg, typename... TArgs, 
         typename _TArg = typename std::remove_reference<
             typename std::remove_cv<TArg>::type>::type,
         typename std::enable_if<!std::is_same<_TArg, 
                 test::msg::fmt::var::arg::Width>::value, 
             int>::type = 0>
-    static bool _SetWidth(std::uint8_t& _default, TArg&& arg, 
+    static bool _SetWidth(WidthType& width, TArg&& arg, 
         TArgs&&... args);
     template<typename... TArgs>
-    static bool _SetWidth(std::uint8_t& _default, 
+    static bool _SetWidth(WidthType& width, 
         test::msg::fmt::var::arg::Width&& set, TArgs&&... args);
 private:
-    static bool _SetPrecision(std::uint8_t& _default);
+    static bool _SetPrecision(PrecisionType& precision);
     template<typename TArg, typename... TArgs, 
         typename _TArg = typename std::remove_reference<
             typename std::remove_cv<TArg>::type>::type,
         typename std::enable_if<!std::is_same<_TArg, 
                 test::msg::fmt::var::arg::Precision>::value, 
             int>::type = 0>
-    static bool _SetPrecision(std::uint8_t& _default, TArg&& arg, 
+    static bool _SetPrecision(PrecisionType& precision, TArg&& arg, 
         TArgs&&... args);
     template<typename... TArgs>
-    static bool _SetPrecision(std::uint8_t& _default, 
+    static bool _SetPrecision(PrecisionType& precision, 
         test::msg::fmt::var::arg::Precision&& set, TArgs&&... args);
 private:
     template<typename... TArgs>
-    static bool _Set(std::uint8_t& _default, TArgs&&... args);
-private:
-    static constexpr std::uint8_t default_width = 1;
-    static constexpr std::uint8_t default_set_width = 2;
-    static constexpr std::uint8_t default_precision = 4;
-    static constexpr std::uint8_t default_set_precision = 8;
+    static bool _Set(WidthType& width, PrecisionType& precision, 
+        TArgs&&... args);
 private:
     FlagType m_flag;
-    std::uint8_t m_default;
+    WidthType m_width;
+    PrecisionType m_precision;
 public:
     Nothing();
     template<typename TArg, typename... TArgs, 
@@ -138,7 +139,7 @@ public:
 };
 
 template<typename TChar>
-bool Nothing<TChar>::_SetWidth(std::uint8_t&)
+bool Nothing<TChar>::_SetWidth(WidthType&)
 {
     return true;
 }
@@ -148,23 +149,23 @@ template<typename TArg, typename... TArgs, typename _TArg,
     typename std::enable_if<!std::is_same<_TArg, 
             test::msg::fmt::var::arg::Width>::value, 
         int>::type>
-bool Nothing<TChar>::_SetWidth(std::uint8_t& _default, TArg&&, 
+bool Nothing<TChar>::_SetWidth(WidthType& width, TArg&&, 
     TArgs&&... args)
 {
-    return _SetWidth(_default, std::forward<TArgs>(args)...);
+    return _SetWidth(width, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
-bool Nothing<TChar>::_SetWidth(std::uint8_t& _default, 
+bool Nothing<TChar>::_SetWidth(WidthType& width, 
     test::msg::fmt::var::arg::Width&& set, TArgs&&... args)
 {
-    if (!set.IsDefault()) _default |= (default_width | default_set_width);
-    return _SetWidth(_default, std::forward<TArgs>(args)...);;
+    if (!set.IsDefault()) width = std::move(WidthType(set.GetValue()));
+    return _SetWidth(width, std::forward<TArgs>(args)...);;
 }
 
 template<typename TChar>
-bool Nothing<TChar>::_SetPrecision(std::uint8_t&)
+bool Nothing<TChar>::_SetPrecision(PrecisionType&)
 {
     return true;
 }
@@ -174,28 +175,28 @@ template<typename TArg, typename... TArgs, typename _TArg,
     typename std::enable_if<!std::is_same<_TArg, 
             test::msg::fmt::var::arg::Precision>::value, 
         int>::type>
-bool Nothing<TChar>::_SetPrecision(std::uint8_t& _default, TArg&&, 
+bool Nothing<TChar>::_SetPrecision(PrecisionType& precision, TArg&&, 
     TArgs&&... args)
 {
-    return _SetPrecision(_default, std::forward<TArgs>(args)...);
+    return _SetPrecision(precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
-bool Nothing<TChar>::_SetPrecision(std::uint8_t& _default, 
+bool Nothing<TChar>::_SetPrecision(PrecisionType& precision,
     test::msg::fmt::var::arg::Precision&& set, TArgs&&... args)
 {
-    if (!set.IsDefault()) _default |= (default_precision
-        | default_set_precision);
-    return _SetPrecision(_default, std::forward<TArgs>(args)...);;
+    if (!set.IsDefault()) precision = std::move(PrecisionType(set.GetValue()));
+    return _SetPrecision(precision, std::forward<TArgs>(args)...);;
 }
     
 template<typename TChar>
 template<typename... TArgs>
-bool Nothing<TChar>::_Set(std::uint8_t& _default, TArgs&&... args)
+bool Nothing<TChar>::_Set(WidthType& width, PrecisionType& precision, 
+        TArgs&&... args)
 {
-    _SetWidth(_default, std::forward<TArgs>(args)...);
-    _SetPrecision(_default, std::forward<TArgs>(args)...);
+    _SetWidth(width, std::forward<TArgs>(args)...);
+    _SetPrecision(precision, std::forward<TArgs>(args)...);
     return true;
 }
 
@@ -203,9 +204,10 @@ template<typename TChar>
 Nothing<TChar>::Nothing() :
     SpecifierBaseType(),
     m_flag(),
-    m_default(0)
+    m_width(),
+    m_precision()
 {
-    _Set(m_default);
+    _Set(m_width, m_precision);
 }
 
 template<typename TChar>
@@ -218,169 +220,203 @@ template<typename TArg, typename... TArgs, typename _TArg,
 Nothing<TChar>::Nothing(TArg&& arg, TArgs&&... args):
     SpecifierBaseType(),
     m_flag{std::forward<TArg>(arg), std::forward<TArgs>(args)...},
-    m_default(0)
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArg>(arg), std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArg>(arg), 
+        std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const char&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<char>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const signed char&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<signed char>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const short&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<short>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const int&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<int>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const long&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<long>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const long long&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<long long>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const unsigned char&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<unsigned char>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const unsigned short&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<unsigned short>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const unsigned int&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<unsigned int>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const unsigned long&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<unsigned long>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const unsigned long long&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<unsigned long long>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const float&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<float>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const double&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<double>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const long double&, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<long double>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const void*, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<const void*>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
 template<typename... TArgs>
 Nothing<TChar>::Nothing(const char*, TArgs&&... args) :
     SpecifierBaseType(StatusType::default_value),
-    m_flag{std::forward<TArgs>(args)...},
-    m_default(0)
+    m_flag{test::msg::fmt::var::arg::Define<const char*>{},
+        std::forward<TArgs>(args)...},
+    m_width(),
+    m_precision()
 {
-    _Set(m_default, std::forward<TArgs>(args)...);
+    _Set(m_width, m_precision, std::forward<TArgs>(args)...);
 }
 
 template<typename TChar>
@@ -391,27 +427,25 @@ template<typename TChar>
 Nothing<TChar>::Nothing(const Nothing<TChar>& cpy) :
     SpecifierBaseType(cpy),
     m_flag{cpy.m_flag},
-    m_default(cpy.m_default)
+    m_width(cpy.m_width),
+    m_precision(cpy.m_precision)
 {}
 
 template<typename TChar>
 Nothing<TChar>::Nothing(Nothing<TChar>&& mov) :
     SpecifierBaseType(std::move(mov)),
     m_flag{std::move(mov.m_flag)},
-    m_default(mov.m_default)
-{
-    if (!(mov.m_default & default_width))
-        mov.m_default &= ~default_set_width;
-    if (!(mov.m_default & default_precision))
-        mov.m_default &= ~default_set_precision;
-}
+    m_width(std::move(mov.m_width)),
+    m_precision(std::move(mov.m_precision))
+{}
 
 template<typename TChar>
 Nothing<TChar>& Nothing<TChar>::operator=(const Nothing<TChar>& cpy)
 {
     SpecifierBaseType::operator=(cpy);
     m_flag = cpy.m_flag;
-    m_default = cpy.m_default;
+    m_width = cpy.m_width;
+    m_precision = cpy.m_precision;
     return *this;
 }
 
@@ -420,17 +454,15 @@ Nothing<TChar>& Nothing<TChar>::operator=(Nothing<TChar>&& mov)
 {
     SpecifierBaseType::operator=(std::move(mov));
     m_flag = std::move(mov.m_flag);
-    m_default = std::move(mov.m_default);
-    if (!(mov.m_default & default_width))
-        mov.m_default &= ~default_set_width;
-    if (!(mov.m_default & default_precision))
-        mov.m_default &= ~default_set_precision;
+    m_width = std::move(mov.m_width);
+    m_precision = std::move(mov.m_precision);
     return *this;
 }
 
 template<typename TChar>
 std::size_t 
-Nothing<TChar>::VLoad(std::size_t size, std::size_t index, va_list&)
+Nothing<TChar>::VLoad(std::size_t size, std::size_t index, 
+    va_list& args)
 {
     auto& status = SpecifierBaseType::GetStatus();
     
@@ -440,31 +472,30 @@ Nothing<TChar>::VLoad(std::size_t size, std::size_t index, va_list&)
     }
 
     std::size_t skip = 0;
-
-    if ((m_flag.GetValue() & FlagType::width) && 
-        !(m_default & default_set_width))
+    std::size_t total_skip = 0;
+    if ((m_flag.GetValue() & FlagType::width) && !m_width.IsSet())
     {
         if (size <= index) 
         {
             status.Bad(StatusType::load_failed);
             return size;
         }
-        m_default |= default_set_width;
-        skip += 1;
+        skip = m_width.VLoad(size, index, args) - index;
+        total_skip += skip;
     }
-    if ((m_flag.GetValue() & FlagType::precision) && 
-        !(m_default & default_set_precision))
+
+    if ((m_flag.GetValue() & FlagType::precision) && !m_precision.IsSet())
     {
         if (size <= (index + skip)) 
         {
             status.Bad(StatusType::load_failed);
             return size;
         }
-        m_default |= default_set_precision;
-        skip += 1;
+        skip = m_precision.VLoad(size, index, args) - index;
+        total_skip += skip;
     }
-
-    const std::size_t next_index = index + (skip);
+    
+    const std::size_t next_index = index + (total_skip);
 
     if (status.IsSetValue()) return next_index;
 
@@ -472,6 +503,24 @@ Nothing<TChar>::VLoad(std::size_t size, std::size_t index, va_list&)
     {
         status.Bad(StatusType::load_failed);
         return size;
+    }
+    if (m_flag.GetValue() & (FlagType::define_int | 
+        FlagType::define_char | FlagType::define_short))
+        va_arg(args, int);
+    else if (m_flag.GetValue() & FlagType::define_long)
+        va_arg(args, long);
+    else if (m_flag.GetValue() & FlagType::define_long_long)
+        va_arg(args, long long);
+    else if (m_flag.GetValue() & FlagType::define_double)
+        va_arg(args, double);
+    else if (m_flag.GetValue() & FlagType::define_long_double)
+        va_arg(args, long double);
+    else if (m_flag.GetValue() & FlagType::define_pointer)
+        va_arg(args, const void*);
+    else
+    {
+        status.Bad(StatusType::flag_undefined);
+        return next_index;
     }
     status.SetValue();
     return next_index + 1;
@@ -501,21 +550,16 @@ void Nothing<TChar>::Unset()
 {
     auto& status = SpecifierBaseType::GetStatus();
     status.UnsetValue();
-     if ((m_flag.GetValue() & FlagType::width) && 
-        !(m_default & default_width)) m_default &= ~default_set_width;
-    if ((m_flag.GetValue() & FlagType::precision) && 
-        !(m_default & default_precision)) 
-            m_default &= ~default_set_precision;
+    m_width.Unset();
+    m_precision.Unset();
 }
 
 template<typename TChar>
 bool Nothing<TChar>::IsSet() const
 {
-    return (SpecifierBaseType::GetStatus().IsSetValue() &&
-        (!(m_flag.GetValue() & FlagType::width) || 
-            (m_default & default_set_width))) &&
-        (!(m_flag.GetValue() & FlagType::precision) || 
-            (m_default & default_set_precision));
+    return ((!(m_flag.GetValue() & FlagType::width) || m_width.IsSet()) && 
+        (!(m_flag.GetValue() & FlagType::precision) || m_precision.IsSet())) &&
+        SpecifierBaseType::GetStatus().IsSetValue();
 }
 
 template<typename TChar>
