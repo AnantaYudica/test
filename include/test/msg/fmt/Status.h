cc46ca475bb2d8bae424885c963ccd92e03993d2
Ananta Yudica <nta040689@gmail.com>
2020-08-12 01:55:49 +0700

    Updated constructor Removed variable value_mask, default_value and set_value Removed and Updated member functions

--- include/test/msg/fmt/Status.h
+++ include/test/msg/fmt/Status.h
@@ -19,14 +19,11 @@ public:
     typedef TIntegerValue IntegerValueType;
 private:
     static constexpr IntegerValueType bad_offset = 2;
-    static constexpr IntegerValueType value_mask = 3;
 public:
     enum : IntegerValueType
     {
         good = 0,
         bad = IntegerValueType(~((IntegerValueType)-1 >> 1)),
-        default_value = 1 | 2,
-        set_value = 2,
         value_not_set = (1 << bad_offset) | bad,
         load_failed = (2 << bad_offset) | bad,
         flag_undefined = (3 << bad_offset) | bad
@@ -35,7 +32,6 @@ private:
     ValueType m_value; 
 public: 
     Status();
-    Status(const IntegerValueType& val);
 public:
     ~Status();
 public:
@@ -49,11 +45,6 @@ public:
 public:
     bool IsGood() const;
     bool IsBad() const;
-    bool IsDefaultValue() const;
-    bool IsSetValue() const;
-public:
-    bool SetValue();
-    void UnsetValue();
 public:
     void Reset();
     bool Reset(const IntegerValueType& bad_code);
@@ -66,11 +57,6 @@ public:
 template<typename TValue, typename TIntegerValue>
 Status<TValue, TIntegerValue>::Status() :
     m_value(good)
-{}
-
-template<typename TValue, typename TIntegerValue>
-Status<TValue, TIntegerValue>::Status(const IntegerValueType& val) :
-    m_value(val)
 {}
 
 template<typename TValue, typename TIntegerValue>
@@ -90,10 +76,7 @@ Status<TValue, TIntegerValue>::
     Status(Status<TValue, TIntegerValue>&& mov) :
         m_value((TIntegerValue)mov.m_value)
 {
-    if ((mov.m_value & default_value) == set_value)
-        mov.m_value = good;
-    else
-        mov.m_value = default_value;
+    mov.m_value = good;
 }
 
 template<typename TValue, typename TIntegerValue>
@@ -110,10 +93,7 @@ Status<TValue, TIntegerValue>&
 Status<TValue, TIntegerValue>::operator=(Status<TValue, TIntegerValue>&& mov)
 {
     m_value = (TIntegerValue)mov.m_value;
-    if ((mov.m_value & default_value) == set_value)
-        mov.m_value = good;
-    else
-        mov.m_value = default_value;
+    mov.m_value = good;
     return *this;
 }
 
@@ -128,44 +108,11 @@ bool Status<TValue, TIntegerValue>::IsBad() const
 {
     return m_value & bad;
 }
-
-template<typename TValue, typename TIntegerValue>
-bool Status<TValue, TIntegerValue>::IsDefaultValue() const
-{
-    return m_value & (default_value ^ set_value);
-}
-
-template<typename TValue, typename TIntegerValue>
-bool Status<TValue, TIntegerValue>::IsSetValue() const
-{
-    return m_value & set_value;
-}
-
-template<typename TValue, typename TIntegerValue>
-bool Status<TValue, TIntegerValue>::SetValue()
-{
-    if ((m_value & (bad | value_mask)) != good)
-        return false;
-    m_value |= set_value;
-    return true;
-}
-
-template<typename TValue, typename TIntegerValue>
-void Status<TValue, TIntegerValue>::UnsetValue()
-{
-    if ((m_value & default_value) == set_value)
-    {
-        m_value ^= set_value;
-    }
-}
-    
+ 
 template<typename TValue, typename TIntegerValue>
 void Status<TValue, TIntegerValue>::Reset()
 {
-    if ((m_value & default_value) == default_value)
-        m_value = default_value;
-    else
-        m_value = good;
+    m_value = good;
 }
 
 template<typename TValue, typename TIntegerValue>
@@ -183,7 +130,7 @@ template<typename TValue, typename TIntegerValue>
 bool Status<TValue, TIntegerValue>::Bad(const IntegerValueType& code)
 {
     if (m_value & bad) return false;
-    m_value |= (code & ~value_mask);
+    m_value |= code;
     return true;
 }
 
@@ -191,7 +138,7 @@ template<typename TValue, typename TIntegerValue>
 typename Status<TValue, TIntegerValue>::IntegerValueType 
 Status<TValue, TIntegerValue>::GetBadCode() const
 {
-    return m_value & ~value_mask;
+    return m_value;
 }
 
 } //!fmt
