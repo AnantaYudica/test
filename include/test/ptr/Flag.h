f8e678f58979f2320283ebd6575bf8a7ee9413ad
Ananta Yudica <nta040689@gmail.com>
2020-08-14 15:47:48 +0700

    Added enum ptr::Flag

---
+++ include/test/ptr/Flag.h
@@ -0,0 +1,24 @@
+#ifndef TEST_PTR_FLAG_H_
+#define TEST_PTR_FLAG_H_
+
+#include <cstdint>
+
+namespace test
+{
+namespace ptr
+{
+
+typedef std::uint8_t FlagIntegerType;
+
+enum class Flag : FlagIntegerType
+{
+    empty = 0,
+    default_initialization = 1,
+    array_allocation = 2
+};
+
+} //!ptr
+
+} //!test
+
+#endif //!TEST_PTR_FLAG_H_
