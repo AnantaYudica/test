15b8460e7360b3d1abe3255fea88239973e49054
Ananta Yudica <nta040689@gmail.com>
2020-08-19 23:37:35 +0700

    Updated function with ATRIBUTE macro function Updated parameter function Updated call standard print

--- include/test/cstr/Format.h
+++ include/test/cstr/Format.h
@@ -7,22 +7,38 @@
 #include <utility>
 #include <type_traits>
 
+#ifndef __ATTRIBUTE__
+#ifdef __GNUC__
+#define __ATTRIBUTE__(...) __attribute__(__VA_ARGS__)
+#else
+#define __ATTRIBUTE__(...)
+#endif
+#endif //!__ATTRIBUTE__
+
 namespace test
 {
 namespace cstr
 {
 
-template<typename TChar = char, typename... TArgs>
+template<typename TChar = char>
 test::CString<typename std::remove_const<TChar>::type> 
-    Format(const std::size_t& size, const char* format, TArgs&&... args)
+    Format(const std::size_t& size, const char* format, ...)
+        __ATTRIBUTE__((format(printf, 2, 3)));
+
+template<typename TChar = char>
+test::CString<typename std::remove_const<TChar>::type> 
+    Format(const std::size_t& size, const char* format, ...)
 {
     if (size != 0)
     {
+        va_list args;
+        va_start(args, format);
         auto* cstr = new typename std::remove_const<TChar>::type[size];
-        int res = snprintf(cstr, size, format, std::forward<TArgs>(args)...);
+        int res = vsnprintf(cstr, size, format, args);
+        va_end(args);
         if (res < 0 || (std::size_t)res >= size)
             cstr[size - 1] = '\0';
-        return {std::move(cstr), size};
+        return {std::move(cstr), size - 1};
     }
     return {""};
 }
