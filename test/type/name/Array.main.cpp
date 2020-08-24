6b32438ac5531b849492b4729f480ead436e5a9d
Ananta Yudica <nta040689@gmail.com>
2020-08-24 12:15:01 +0700

    Updated test

--- test/type/name/Array.main.cpp
+++ test/type/name/Array.main.cpp
@@ -9,5 +9,12 @@ int main()
 {
     assert(strcmp(*(test::type::name::Array<int[]>::CStr()), "[]") == 0);
     assert(strcmp(*(test::type::name::Array<int[10]>::CStr()), "[10]") == 0);
+    assert(strcmp(*(test::type::name::Array<int[100]>::CStr()), "[100]") == 0);
+    assert(strcmp(*(test::type::name::Array<int[1][2]>::CStr()), 
+        "[1][2]") == 0);
+    assert(strcmp(*(test::type::name::Array<int[1][2][3]>::CStr()), 
+        "[1][2][3]") == 0);
+    assert(strcmp(*(test::type::name::Array<int[1][2][3][4]>::CStr()), 
+        "[1][2][3][4]") == 0);
     return TEST::GetInstance().Status().Get();
 }
