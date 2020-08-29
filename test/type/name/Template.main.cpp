6a6f5f121a0e052cc4a79be728f01cc4a207c8de
Ananta Yudica <nta040689@gmail.com>
2020-08-29 19:45:55 +0700

    Updated test

--- test/type/name/Template.main.cpp
+++ test/type/name/Template.main.cpp
@@ -47,62 +47,6 @@ inline auto test::type::name::tmpl::Parameter(Obj4<T, I, F>*) ->
     test::type::name::Parameter<T, test::type::name::Value<int, I>,
         test::type::name::Value<int (*)(), F>>;
 
-
-template<int ...I>
-struct test::type::name::Template<Obj1<I...>>
-{
-    template<typename TChar = char>
-    static test::CString<const TChar> CStr()
-    {
-        static test::CString<char> _tmpl = test::cstr::Format((3 +
-            test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(std::declval<Obj1<I...>*>()))>::
-                template CStr<TChar>().Size()),
-            "<%s>", *( test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(std::declval<Obj1<I...>*>()))>::
-                template CStr<TChar>()));
-        return {_tmpl};
-    }
-};
-
-template<typename T, int I, int (&F)()>
-struct test::type::name::Template<Obj3<T, I, F>>
-{
-    template<typename TChar = char>
-    static test::CString<const TChar> CStr()
-    {
-        static test::CString<char> _tmpl = test::cstr::Format((3 +
-            test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(
-                std::declval<Obj3<T, I, F>*>()))>::
-                template CStr<TChar>().Size()),
-            "<%s>", *( test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(
-                std::declval<Obj3<T, I, F>*>()))>::
-                template CStr<TChar>()));
-        return {_tmpl};
-    }
-};
-
-template<typename T, int I, int (*F)()>
-struct test::type::name::Template<Obj4<T, I, F>>
-{
-    template<typename TChar = char>
-    static test::CString<const TChar> CStr()
-    {
-        static test::CString<char> _tmpl = test::cstr::Format((3 +
-            test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(
-                std::declval<Obj4<T, I, F>*>()))>::
-                template CStr<TChar>().Size()),
-            "<%s>", *( test::type::name::tmpl::Get<decltype(
-                test::type::name::tmpl::Parameter(
-                std::declval<Obj4<T, I, F>*>()))>::
-                template CStr<TChar>()));
-        return {_tmpl};
-    }
-};
-
 template<>
 struct test::type::Name<int>
 {
@@ -140,28 +84,56 @@ int main()
     assert(strcmp(*(test::type::name::Template<Obj2<int, int, int, 
         int>>::CStr()), "<int, int, int, int>") == 0);
 
-    assert(strcmp(*(test::type::name::Template<Obj1<>>::CStr()),
-        "<>") == 0);
-    assert(strcmp(*(test::type::name::Template<Obj1<0, 100>>::CStr()),
-        "<(int) 0, (int) 100>") == 0);
-    assert(strcmp(*(test::type::name::Template<Obj1<0, 100, -99>>::CStr()),
-        "<(int) 0, (int) 100, (int) -99>") == 0);
-    assert(strcmp(*(test::type::name::Template<Obj1<0, 100, -99, 9>>::CStr()),
-        "<(int) 0, (int) 100, (int) -99, (int) 9>") == 0);
-
     {
-        test::out::CString<char> cstr_out1;
-        cstr_out1.Print("<int, (int) %d, (int (&)()) %p>", -999, &Func1);
-        assert(strncmp(*(test::type::name::Template<Obj3<int, -999, Func1>>::
-            CStr()),*(cstr_out1.Get()), test::type::name::Template<Obj3<int, 
-            -999, Func1>>::CStr().Size()) == 0);
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj1<>*>())) Param1Type;
+        typedef test::type::name::Template<Obj1<>, 
+            Param1Type> Template1Type;
+        assert(strcmp(*(Template1Type::CStr()), "<>") == 0);
     }
     {
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj1<0, 100>*>())) Param1Type;
+        typedef test::type::name::Template<Obj1<0, 100>, 
+            Param1Type> Template1Type;
+        assert(strcmp(*(Template1Type::CStr()),
+            "<(int) 0, (int) 100>") == 0);
+    }
+    {
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj1<0, 100, -99>*>())) Param1Type;
+        typedef test::type::name::Template<Obj1<0, 100, -99>, 
+            Param1Type> Template1Type;
+        assert(strcmp(*(Template1Type::CStr()),
+            "<(int) 0, (int) 100, (int) -99>") == 0);
+    }
+    {
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj1<0, 100, -99, 9>*>())) Param1Type;
+        typedef test::type::name::Template<Obj1<0, 100, -99, 9>, 
+            Param1Type> Template1Type;
+        assert(strcmp(*(Template1Type::CStr()),
+            "<(int) 0, (int) 100, (int) -99, (int) 9>") == 0);
+    }
+    {
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj3<int, -999, Func1>*>())) Param1Type;
+        typedef test::type::name::Template<Obj3<int, -999, Func1>, 
+            Param1Type> Template1Type;
+        test::out::CString<char> cstr_out1;
+        cstr_out1.Print("<int, (int) %d, (int (&)()) %p>", -999, &Func1);
+        assert(strncmp(*(Template1Type::CStr()),*(cstr_out1.Get()), 
+            Template1Type::CStr().Size()) == 0);
+    }
+    {
+        typedef decltype(test::type::name::tmpl::Parameter(
+            std::declval<Obj4<int, 12903, &Func1>*>())) Param1Type;
+        typedef test::type::name::Template<Obj4<int, 12903, &Func1>, 
+            Param1Type> Template1Type;
         test::out::CString<char> cstr_out1;
         cstr_out1.Print("<int, (int) %d, (int (*)()) %p>", 12903, &Func1);
-        assert(strncmp(*(test::type::name::Template<Obj4<int, 12903, &Func1>>::
-            CStr()),*(cstr_out1.Get()), test::type::name::Template<Obj4<int, 
-            12903, &Func1>>::CStr().Size()) == 0);
+        assert(strncmp(*(Template1Type::CStr()),*(cstr_out1.Get()), 
+            Template1Type::CStr().Size()) == 0);
     }
 
     return TEST::GetInstance().Status().Get();
