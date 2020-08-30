e2cbcf0a403a45d9288b4af479f7f68fee52cd5a
Ananta Yudica <nta040689@gmail.com>
2020-08-31 03:41:04 +0700

    Added dependencies

---
+++ include/test/def/type/Name.h
@@ -0,0 +1,16 @@
+#ifndef TEST_DEF_TYPE_NAME_H_
+#define TEST_DEF_TYPE_NAME_H_
+
+#include "../../type/Name.h"
+#include "name/Void.h"
+#include "name/Nullptr.h"
+#include "name/Boolean.h"
+#include "name/Character.h"
+#include "name/Integer.h"
+#include "name/FloatingPoint.h"
+#include "name/val/Nullptr.h"
+#include "name/val/Boolean.h"
+#include "name/val/Character.h"
+#include "name/val/Integer.h"
+
+#endif //!TEST_DEF_TYPE_NAME_H_
