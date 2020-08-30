06de6a9e4b1b4ae5b283e4f666ac8c55b695dd92
Ananta Yudica <nta040689@gmail.com>
2020-08-30 21:57:16 +0700

    Updated test

--- test/type/name/Value.main.cpp
+++ test/type/name/Value.main.cpp
@@ -8,6 +8,12 @@ TEST_CONSTRUCT;
 #include <cstring>
 #include <cassert>
 
+enum EObj1
+{
+    value_a,
+    value_b
+};
+
 template<>
 struct test::type::Name<int>
 {
@@ -31,14 +37,25 @@ struct test::type::Name<bool>
 }; 
 
 template<>
+struct test::type::Name<EObj1>
+{
+    template<typename TChar= char>
+    static test::CString<const TChar> CStr()
+    {
+        static TChar name[] = "EObj1";
+        return {name};
+    }
+}; 
+
+template<>
 struct test::type::name::val::Get<int>
 {
-    template<typename TChar = char>
-    static test::CString<TChar> CStr(int v)
+    template<int V, typename TChar = char>
+    static test::CString<TChar> CStr()
     {
         test::CString<TChar> _val = test::cstr::Format(
-            (v > 0 ? (log10(v) + 1) : (v < 0 ? (log10(abs(v)) + 2) : 1)) + 1, 
-            "%d", v);
+            (V > 0 ? (log10(V) + 1) : (V < 0 ? (log10(abs(V)) + 2) : 1)) + 1, 
+            "%d", V);
         return {_val};
     }
 };
@@ -46,11 +63,33 @@ struct test::type::name::val::Get<int>
 template<>
 struct test::type::name::val::Get<bool>
 {
-    template<typename TChar = char>
-    static test::CString<TChar> CStr(bool v)
+    template<bool V, typename TChar = char>
+    static test::CString<TChar> CStr()
     {
         test::CString<TChar> _val = test::cstr::Format(
-            v ? 5 : 6 , "%s", v ? "true" : "false");
+            V ? 5 : 6 , "%s", V ? "true" : "false");
+        return {_val};
+    }
+};
+
+template<>
+struct test::type::name::val::Enumeration<EObj1, EObj1::value_a>
+{
+    template<typename TChar= char>
+    static test::CString<TChar> CStr()
+    {
+        static TChar _val[] = "value_a";
+        return {_val};
+    }
+};
+
+template<>
+struct test::type::name::val::Enumeration<EObj1, EObj1::value_b>
+{
+    template<typename TChar= char>
+    static test::CString<TChar> CStr()
+    {
+        static TChar _val[] = "value_b";
         return {_val};
     }
 };
@@ -67,6 +106,10 @@ int main()
         "(bool) true") == 0);
     assert(strcmp(*(test::type::name::Value<bool, false>::CStr()), 
         "(bool) false") == 0);
+    assert(strcmp(*(test::type::name::Value<EObj1, EObj1::value_a>::CStr()), 
+        "(EObj1) value_a") == 0);
+    assert(strcmp(*(test::type::name::Value<EObj1, EObj1::value_b>::CStr()), 
+        "(EObj1) value_b") == 0);
 
     return TEST::GetInstance().Status().Get();
 }
