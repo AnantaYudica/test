c45a676d1ebe4f5ade0dfc6e9ba15f91f0429e3f
Ananta Yudica <nta040689@gmail.com>
2020-09-06 06:12:37 +0700

    Added function

---
+++ include/test/out/log/make/Tag.h
@@ -0,0 +1,28 @@
+#ifndef TEST_OUT_LOG_MAKE_TAG_H_
+#define TEST_OUT_LOG_MAKE_TAG_H_
+
+#include "../Tag.h"
+
+namespace test
+{
+namespace out
+{
+namespace log
+{
+namespace make
+{
+
+inline test::out::log::Tag Tag(...)
+{
+    return {};
+}
+
+} //!make
+
+} //!log
+
+} //!out
+
+} //!test
+
+#endif //!TEST_OUT_LOG_MAKE_TAG_H_
