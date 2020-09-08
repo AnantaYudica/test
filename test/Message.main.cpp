4648bdca104174735335aefb311518c4652d1be3
Ananta Yudica <nta040689@gmail.com>
2020-09-08 10:57:17 +0700

    Updated test

--- test/Message.main.cpp
+++ test/Message.main.cpp
@@ -2,110 +2,157 @@
 #include "Test.h"
 TEST_CONSTRUCT;
 
+#include "test/def/type/Name.h"
 #include "test/Register.h"
 
 #include "test/Message.h"
+#include "test/out/CString.h"
 
-#include <cstdio>
 #include <utility>
+#include <cassert>
 
 struct TestA1 {};
 struct TestA2 {};
 struct TestA3 {};
 struct TestA4 {};
 
-typedef test::msg::Argument<TestA1, 
-    test::msg::arg::type::Name<0>> InfoArgTestA1;
-typedef test::msg::Argument<TestA2, 
-    test::msg::arg::type::Name<0>> InfoArgTestA2;
-typedef test::msg::Argument<TestA3, 
-    test::msg::arg::type::Name<0>> InfoArgTestA3;
+typedef test::msg::Argument< 
+    test::msg::arg::type::Name<0>> TName0;
 
-typedef test::msg::Argument<TestA1, 
-    test::msg::arg::type::Value<1>> DebugArgTestA1;
-typedef test::msg::Argument<TestA2, 
-    test::msg::arg::type::Value<1>> DebugArgTestA2;
-typedef test::msg::Argument<TestA3, 
-    test::msg::arg::type::Value<1>> DebugArgTestA3;
+typedef test::msg::Argument< 
+    test::msg::arg::type::Value<1>> TValue1;
     
-typedef test::msg::Argument<TestA1, 
-    test::msg::arg::Value<2>> ErrorArgTestA1;
-typedef test::msg::Argument<TestA2, 
-    test::msg::arg::Value<2>> ErrorArgTestA2;
-typedef test::msg::Argument<TestA3, 
-    test::msg::arg::Value<2>> ErrorArgTestA3;
+typedef test::msg::Argument< 
+    test::msg::arg::Value<2>> Value2;
 
-typedef test::msg::Argument<TestA4, 
+typedef test::msg::Argument< 
     test::msg::arg::type::Index<1,
-        test::msg::arg::type::val::seq::At>> ArgTestA4Index;
+        test::msg::arg::type::val::seq::At>> TIndex1;
 
-typedef test::msg::Base<TestA1, char, InfoArgTestA1,
-    DebugArgTestA1, ErrorArgTestA1> MessageBaseTestA1;
-typedef test::msg::Base<TestA2, char, InfoArgTestA2,
-    DebugArgTestA2, ErrorArgTestA2> MessageBaseTestA2;
-typedef test::msg::Base<TestA3, char, InfoArgTestA3,
-    DebugArgTestA3, ErrorArgTestA3> MessageBaseTestA3;
-typedef test::msg::Base<TestA4, char, ArgTestA4Index,
-    ArgTestA4Index, ArgTestA4Index> MessageBaseTestA4;
+typedef test::msg::Case<TestA1, char, 
+    test::msg::Tag<test::msg::tag::Info, TName0>,
+    test::msg::Tag<test::msg::tag::Debug, TValue1>, 
+    test::msg::Tag<test::msg::tag::Error, Value2>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Info, 1>, TName0>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Debug, 1>, TValue1>, 
+    test::msg::Tag<test::type::Index<test::msg::tag::Error, 1>, Value2>
+    > CaseTestA1;
+typedef test::msg::Case<TestA2, char, 
+    test::msg::Tag<test::msg::tag::Info, TName0>,
+    test::msg::Tag<test::msg::tag::Debug, TValue1>, 
+    test::msg::Tag<test::msg::tag::Error, Value2>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Info, 1>, TName0>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Debug, 1>, TValue1>, 
+    test::msg::Tag<test::type::Index<test::msg::tag::Error, 1>, Value2>> 
+        CaseTestA2;
+typedef test::msg::Case<TestA3, char, 
+    test::msg::Tag<test::msg::tag::Info, TName0>,
+    test::msg::Tag<test::msg::tag::Debug, TValue1>, 
+    test::msg::Tag<test::msg::tag::Error, Value2>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Info, 1>, TName0>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Debug, 1>, TValue1>, 
+    test::msg::Tag<test::type::Index<test::msg::tag::Error, 1>, Value2>> 
+        CaseTestA3;
+typedef test::msg::Case<TestA4, char, 
+    test::msg::Tag<test::msg::tag::Info, TIndex1>,
+    test::msg::Tag<test::msg::tag::Debug, TIndex1>, 
+    test::msg::Tag<test::msg::tag::Error, TIndex1>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Info, 1>, TIndex1>,
+    test::msg::Tag<test::type::Index<test::msg::tag::Debug, 1>, TIndex1>, 
+    test::msg::Tag<test::type::Index<test::msg::tag::Error, 1>, TIndex1>> 
+        CaseTestA4;
+
+typedef test::msg::Base<char, CaseTestA1, CaseTestA2, CaseTestA3, 
+    CaseTestA4> MessageBase;
 
 class TestA :
-    public MessageBaseTestA1,
-    public MessageBaseTestA2,
-    public MessageBaseTestA3,
-    public MessageBaseTestA4,
+    public MessageBase,
     public test::Message<TEST, TestA>
 {
-public:
-    using MessageBaseTestA1::Format;
-    using MessageBaseTestA2::Format;
-    using MessageBaseTestA3::Format;
-    using MessageBaseTestA4::Format;
-    using MessageBaseTestA1::SetFormat;
-    using MessageBaseTestA2::SetFormat;
-    using MessageBaseTestA3::SetFormat;
-    using MessageBaseTestA4::SetFormat;
-    using MessageBaseTestA1::Argument;
-    using MessageBaseTestA2::Argument;
-    using MessageBaseTestA3::Argument;
-    using MessageBaseTestA4::Argument;
 public:
     TestA() :
         test::Message<TEST, TestA>(*this)
     {
-        test::msg::base::Info info;
-        test::msg::base::Debug debug;
-        test::msg::base::Error error;
+        test::msg::tag::Info info;
+        test::msg::tag::Debug debug;
+        test::msg::tag::Error error;
+        test::type::Index<test::msg::tag::Info, 1> info1;
+        test::type::Index<test::msg::tag::Debug, 1> debug1;
+        test::type::Index<test::msg::tag::Error, 1> error1;
         TestA1 testa1;
         TestA2 testa2;
         TestA3 testa3;
         TestA4 testa4;
 
-        test::msg::Format<char> formatTestA1("Test TestA1 : %s\n");
-        SetFormat(debug, testa1, {formatTestA1, 
-            formatTestA1.Size() + 10, "type %d"});
-        SetFormat(info, testa1, std::move(formatTestA1));
-        SetFormat(error, testa1, "Error TestA1 msg %s\n");
+        test::msg::fmt::var::String<char> var_str_a1{"Test TestA1 : "};
 
-        test::msg::Format<char> formatTestA2("Test TestA1 : %s\n");
-        SetFormat(debug, testa2, {formatTestA2, 
-            formatTestA2.Size() + 10, "type %d"});
-        SetFormat(info, testa2, std::move(formatTestA2));
-        SetFormat(error, testa2, "Error TestA2 msg %s\n");
-
-        test::msg::Format<char> formatTestA3("Test TestA3 : %s\n");
-        SetFormat(debug, testa3, {formatTestA3, 
-            formatTestA3.Size() + 10, "type %d"});
-        SetFormat(info, testa3, std::move(formatTestA3));
-        SetFormat(error, testa3, "Error TestA3 msg %s\n");
+        test::msg::Format<char> formatTestA1_i{
+            var_str_a1,
+            test::msg::fmt::var::String<char>{}};
+        SetFormat(testa1, debug, var_str_a1, 
+            test::msg::fmt::var::String<char>{"type "},
+            test::msg::fmt::var::Integer<char>{});
+        SetFormat(testa1, info, std::move(formatTestA1_i));
+        SetFormat(testa1, error, 
+            test::msg::fmt::var::String<char>{"Error TestA1 msg "},
+            test::msg::fmt::var::String<char>{});
         
-        test::msg::Format<char> formatTestA4("Test TestA4 : %d\n");
-        SetFormat(debug, testa4, "debug type TestA4 %d\n");
-        SetFormat(info, testa4, std::move(formatTestA4));
-        SetFormat(error, testa4, "Error TestA4 msg %d\n");
+        test::msg::Format<char> formatEmptyTestA1{
+            test::msg::fmt::var::Nothing<char>{},
+        };
+        SetFormat(testa1, debug1, formatEmptyTestA1);
+        SetFormat(testa1, info1, formatEmptyTestA1);
+        SetFormat(testa1, error1, formatEmptyTestA1);
+
+        test::msg::fmt::var::String<char> var_str_a2{"Test TestA2 : "};
+        
+        test::msg::Format<char> formatTestA2_i(var_str_a2,
+            test::msg::fmt::var::String<char>{});
+        SetFormat(testa2, debug, var_str_a2,
+            test::msg::fmt::var::String<char>{"type "},
+            test::msg::fmt::var::Integer<char>{});
+        SetFormat(testa2, info, std::move(formatTestA2_i));
+        SetFormat(testa2, error, 
+            test::msg::fmt::var::String<char>{"Error TestA2 msg "},
+            test::msg::fmt::var::String<char>{});
+        
+        SetFormat(testa2, debug1, formatEmptyTestA1);
+        SetFormat(testa2, info1, formatEmptyTestA1);
+        SetFormat(testa2, error1, formatEmptyTestA1);
+        
+        test::msg::fmt::var::String<char> var_str_a3{"Test TestA3 : "};
+        
+        test::msg::Format<char> formatTestA3_i(var_str_a3,
+            test::msg::fmt::var::String<char>{});
+        SetFormat(testa3, debug, var_str_a3,
+            test::msg::fmt::var::String<char>{"type "},
+            test::msg::fmt::var::Integer<char>{});
+        SetFormat(testa3, info, std::move(formatTestA3_i));
+        SetFormat(testa3, error,
+            test::msg::fmt::var::String<char>{"Error TestA3 msg "},
+            test::msg::fmt::var::String<char>{});
+        
+        SetFormat(testa3, debug1, formatEmptyTestA1);
+        SetFormat(testa3, info1, formatEmptyTestA1);
+        SetFormat(testa3, error1, formatEmptyTestA1);
+        
+        test::msg::fmt::var::String<char> var_str_a4{"Test TestA4 : "};
+
+        test::msg::Format<char> formatTestA4_i(var_str_a4,
+            test::msg::fmt::var::Integer<char>{});
+        SetFormat(testa4, debug, 
+            test::msg::fmt::var::String<char>{"debug type "},
+            var_str_a4, test::msg::fmt::var::Integer<char>{});
+        SetFormat(testa4, info, std::move(formatTestA4_i));
+        SetFormat(testa4, error, 
+            test::msg::fmt::var::String<char>{"error type "},
+            var_str_a4, test::msg::fmt::var::Integer<char>{});
+
+        SetFormat(testa4, debug1, formatEmptyTestA1);
+        SetFormat(testa4, info1, formatEmptyTestA1);
+        SetFormat(testa4, error1, formatEmptyTestA1);
     }
 };
-
-TEST_TYPE_NAME("int", int);
 
 int main()
 {
@@ -114,24 +161,195 @@ int main()
     test::CString<char> cstr1("test");
     test::Variable<int, test::type::Value<int, 4>,
         test::Value<const char*>> var1(*cstr1);
-
-    a.Info(TestA1(), var1);
-    a.Debug(TestA1(), var1);
-    a.Error(TestA1(), var1);
-    
-    a.Info(TestA2(), var1);
-    a.Debug(TestA2(), var1);
-    a.Error(TestA2(), var1);
-    
-    a.Info(TestA3(), var1);
-    a.Debug(TestA3(), var1);
-    a.Error(TestA3(), var1);
     
     test::Variable<int, 
-        test::type::val::Sequence<int, 14, 41, 44>,
+        test::type::val::Sequence<int, 143, -141, 30044>,
         test::Value<const char*>> var2(*cstr1);
 
-    a.Info(test::type::Index<TestA4, 0>{}, var2);
-    a.Debug(test::type::Index<TestA4, 1>{}, var2);
-    a.Error(test::type::Index<TestA4, 2>{}, var2);
+    auto empty_info_a1 = a.IndexInfo<1>(TestA1(), var1);
+    auto empty_debug_a1 = a.IndexDebug<1>(TestA1(), var1);
+    auto empty_error_a1 = a.IndexError<1>(TestA1(), var1);
+    auto empty_info_a2 = a.IndexInfo<1>(TestA2(), var1);
+    auto empty_debug_a2 = a.IndexDebug<1>(TestA2(), var1);
+    auto empty_error_a2 = a.IndexError<1>(TestA2(), var1);
+    auto empty_info_a3 = a.IndexInfo<1>(TestA3(), var1);
+    auto empty_debug_a3 = a.IndexDebug<1>(TestA3(), var1);
+    auto empty_error_a3 = a.IndexError<1>(TestA3(), var1);
+    auto empty_info_a4 = a.IndexInfo<1>(TestA4(), var2);
+    auto empty_debug_a4 = a.IndexDebug<1>(TestA4(), var2);
+    auto empty_error_a4 = a.IndexError<1>(TestA4(), var2);
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA1 : %s", "int");
+        auto res_b = a.Info(TestA1(), var1);
+        assert(res_b == (empty_info_a1 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA1 : type %d", 4);
+        auto res_b = a.Debug(TestA1(), var1);
+        assert(res_b == (empty_debug_a1 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("Error TestA1 msg %s", "test");
+        auto res_b = a.Error(TestA1(), var1);
+        assert(res_b == (empty_error_a1 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA2 : %s", "int");
+        auto res_b = a.Info(TestA2(), var1);
+        assert(res_b == (empty_info_a2 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA2 : type %d", 4);
+        auto res_b = a.Debug(TestA2(), var1);
+        assert(res_b == (empty_debug_a2 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("Error TestA2 msg %s", "test");
+        auto res_b = a.Error(TestA2(), var1);
+        assert(res_b == (empty_error_a2 + res_a));
+    }
+    
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA3 : %s", "int");
+        auto res_b = a.Info(TestA3(), var1);
+        assert(res_b == (empty_info_a3 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA3 : type %d", 4);
+        auto res_b = a.Debug(TestA3(), var1);
+        assert(res_b == (empty_debug_a3 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("Error TestA3 msg %s", "test");
+        auto res_b = a.Error(TestA3(), var1);
+        assert(res_b == (empty_error_a3 + res_a));
+    }
+
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA4 : %d", 143);
+        auto res_b = a.Info(TestA4(), var2);
+        assert(res_b == (empty_info_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("debug type Test TestA4 : %d", 143);
+        auto res_b = a.Debug(TestA4(), var2);
+        assert(res_b == (empty_debug_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("error type Test TestA4 : %d", 143);
+        auto res_b = a.Error(TestA4(), var2);
+        assert(res_b == (empty_error_a4 + res_a));
+    }
+    
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA4 : %d", 143);
+        auto res_b = a.Info(test::type::Index<TestA4, 0>(), var2);
+        assert(res_b == (empty_info_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("debug type Test TestA4 : %d", 143);
+        auto res_b = a.Debug(test::type::Index<TestA4, 0>(), var2);
+        assert(res_b == (empty_debug_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("error type Test TestA4 : %d", 143);
+        auto res_b = a.Error(test::type::Index<TestA4, 0>(), var2);
+        assert(res_b == (empty_error_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA4 : %d", -141);
+        auto res_b = a.Info(test::type::Index<TestA4, 1>(), var2);
+        assert(res_b == (empty_info_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("debug type Test TestA4 : %d", -141);
+        auto res_b = a.Debug(test::type::Index<TestA4, 1>(), var2);
+        assert(res_b == (empty_debug_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("error type Test TestA4 : %d", -141);
+        auto res_b = a.Error(test::type::Index<TestA4, 1>(), var2);
+        assert(res_b == (empty_error_a4 + res_a));
+    }
+    
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("Test TestA4 : %d", 30044);
+        auto res_b = a.Info(test::type::Index<TestA4, 2>(), var2);
+        assert(res_b == (empty_info_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_a = out_cstr1.Print("debug type Test TestA4 : %d", 30044);
+        auto res_b = a.Debug(test::type::Index<TestA4, 2>(), var2);
+        assert(res_b == (empty_debug_a4 + res_a));
+    }
+    {
+        test::out::CString<char> out_cstr1;
+        auto res_a = out_cstr1.Print("error type Test TestA4 : %d", 30044);
+        auto res_b = a.Error(test::type::Index<TestA4, 2>(), var2);
+        assert(res_b == (empty_error_a4 + res_a));
+    }
+    TEST::GetInstance().Output().InfoEnable(false);
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_b = a.Info(TestA1(), var1);
+        assert(res_b == 0);
+    }
+    TEST::GetInstance().Output().DebugEnable(false);
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_b = a.Debug(TestA1(), var1);
+        assert(res_b == 0);
+    }
+    TEST::GetInstance().Output().DebugEnable(false);
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_b = a.Debug(TestA1(), var1);
+        assert(res_b == 0);
+    }
+    TEST::GetInstance().Output().Enable(false);
+    {
+        test::out::CString<char> out_cstr1;
+
+        auto res_b = a.Error(TestA1(), var1);
+        assert(res_b == 0);
+    }
+   
+    return TEST::GetInstance().Status().Get();
 }
