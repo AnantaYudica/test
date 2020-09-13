fdabd9f572f6b809299a2fbe842bed511126378c
Ananta Yudica <nta040689@gmail.com>
2020-09-13 12:07:41 +0700

    Added class test::trait::ptr::defn::IsBaseOf

---
+++ include/test/trait/ptr/defn/IsBaseOf.h
@@ -0,0 +1,41 @@
+#ifndef TEST_TRAIT_PTR_DEFN_ISBASEOF_H_
+#define TEST_TRAIT_PTR_DEFN_ISBASEOF_H_
+
+#include "../../../ptr/Definition.h"
+
+#include <type_traits>
+#include <utility>
+
+namespace test
+{
+namespace trait
+{
+namespace ptr
+{
+namespace defn
+{
+
+template<typename T>
+class IsBaseOf
+{
+private:
+    template<typename T, T(*Func)()>
+    static constexpr std::true_type _IsBaseOf(
+        const test::ptr::Definition<T, Func>&);
+    static constexpr std::false_type _IsBaseOf(...);
+public:
+    IsBaseOf() = delete;
+public:
+    static constexpr bool Value = 
+        decltype(_IsBaseOf(std::declval<T>()))::value;
+};
+
+} //!defn
+
+} //!ptr
+
+} //!trait
+
+} //!test
+
+#endif //!TEST_TRAIT_PTR_DEFN_ISBASEOF_H_
