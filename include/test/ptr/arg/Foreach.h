36266a174e6b6db8f8791864fcef872584ac8c81
Ananta Yudica <nta040689@gmail.com>
2020-09-13 12:27:16 +0700

    Added class test::ptr::arg::Foreach

---
+++ include/test/ptr/arg/Foreach.h
@@ -0,0 +1,25 @@
+#ifndef TEST_PTR_ARG_FOREACH_H_
+#define TEST_PTR_ARG_FOREACH_H_
+
+#include <cstddef>
+
+namespace test
+{
+namespace ptr
+{
+namespace arg
+{
+
+class Foreach
+{
+public:
+    Foreach() = default;
+};
+
+} //!arg
+
+} //!ptr
+
+} //!test
+
+#endif //!TEST_PTR_ARG_FOREACH_H_
