c193c7fee7436bed06eee9bd88b50c648b0ea720
Ananta Yudica <nta040689@gmail.com>
2020-09-13 12:05:04 +0700

    Updated header guard Renamed class name from Default to Definition

--- include/test/ptr/Definition.h
+++ include/test/ptr/Definition.h
@@ -1,5 +1,5 @@
-#ifndef TEST_PTR_DEFAULT_H_
-#define TEST_PTR_DEFAULT_H_
+#ifndef TEST_PTR_DEFINITION_H_
+#define TEST_PTR_DEFINITION_H_
 
 #include "Flag.h"
 
@@ -14,7 +14,7 @@ namespace ptr
 {
 
 template<typename T, T(*Func)() = nullptr, typename TEnable = void>
-struct Default
+struct Definition
 {
     typedef test::ptr::FlagIntegerType FlagIntegerType;
     typedef T ValueType;
@@ -28,11 +28,11 @@ struct Default
     static void Constructor(void* data, TArg&& arg, TArgs&&... args);
     static void Destructor(void* data);
 
-    Default() = delete;
+    Definition() = delete;
 };
 
 template<typename T, T(*Func)()>
-struct Default<T, Func, 
+struct Definition<T, Func, 
     typename std::enable_if<Func == nullptr>::type>
 {
     typedef test::ptr::FlagIntegerType FlagIntegerType;
@@ -46,82 +46,82 @@ struct Default<T, Func,
     static void Constructor(void* data, TArgs&&... args);
     static void Destructor(void* data);
     
-    Default() = delete;
+    Definition() = delete;
 };
 
 template<typename T, T(*Func)(), typename TEnable>
-void Default<T, Func, TEnable>::Constructor(const std::size_t& size, 
+void Definition<T, Func, TEnable>::Constructor(const std::size_t& size, 
     void* data)
 {
     if (size == 0) return;
     new(data) T(Func());
-    Default<T, Func, TEnable>::Constructor(size - 1, 
+    Definition<T, Func, TEnable>::Constructor(size - 1, 
         ((char*) data) + sizeof(T));
 }
 
 template<typename T, T(*Func)(), typename TEnable>
 template<typename TArg, typename... TArgs>
-void Default<T, Func, TEnable>::Constructor(const std::size_t& size, 
+void Definition<T, Func, TEnable>::Constructor(const std::size_t& size, 
     void* data, TArg&& arg, TArgs&&... args)
 {
     if (size == 0) return;
     new(data) T(std::forward<TArg>(arg));
-    Default<T, Func, TEnable>::Constructor(size - 1, 
+    Definition<T, Func, TEnable>::Constructor(size - 1, 
         ((char*) data) + sizeof(T), std::forward<TArgs>(args)...);
 }
 
 template<typename T, T(*Func)(), typename TEnable>
-void Default<T, Func, TEnable>::Constructor(void* data)
+void Definition<T, Func, TEnable>::Constructor(void* data)
 {
     new(data) T(Func());
 }
 
 template<typename T, T(*Func)(), typename TEnable>
 template<typename TArg, typename... TArgs>
-void Default<T, Func, TEnable>::Constructor(void* data, TArg&& arg, 
+void Definition<T, Func, TEnable>::Constructor(void* data, TArg&& arg, 
     TArgs&&... args)
 {
     new(data) T(std::forward<TArg>(arg), std::forward<TArgs>(args)...);
 }
 
 template<typename T, T(*Func)(), typename TEnable>
-void Default<T, Func, TEnable>::Destructor(void* data)
+void Definition<T, Func, TEnable>::Destructor(void* data)
 {
     T *cast = (T*)data;
     cast->~T();
 }
 
 template<typename T, T(*Func)()>
-void Default<T, Func, typename std::enable_if<Func == nullptr>::type>::
+void Definition<T, Func, typename std::enable_if<Func == nullptr>::type>::
     Constructor(const std::size_t& size, void* data)
 {
     if (size == 0) return;
     new(data) T();
-    Default<T, Func, void>::Constructor(size - 1, ((char*) data) + sizeof(T));
+    Definition<T, Func, void>::Constructor(size - 1, ((char*) data) + sizeof(T));
 }
 
 template<typename T, T(*Func)()>
 template<typename TArg, typename... TArgs>
-void Default<T, Func, typename std::enable_if<Func == nullptr>::type>::
+void Definition<T, Func, typename std::enable_if<Func == nullptr>::type>::
     Constructor(const std::size_t& size, void* data, TArg&& arg, 
         TArgs&&... args)
 {
     if (size == 0) return;
     new(data) T(std::forward<TArg>(arg));
-    Default<T, Func, void>::Constructor(size - 1, 
+    Definition<T, Func, void>::Constructor(size - 1, 
         ((char*) data) + sizeof(T), std::forward<TArgs>(args)...);
 }
 
 template<typename T, T(*Func)()>
 template<typename... TArgs>
-void Default<T, Func, typename std::enable_if<Func == nullptr>::type>::
+void Definition<T, Func, typename std::enable_if<Func == nullptr>::type>::
     Constructor(void* data, TArgs&&... args)
 {
     new(data) T(std::forward<TArgs>(args)...);
 }
 
 template<typename T, T(*Func)()>
-void Default<T, Func, typename std::enable_if<Func == nullptr>::type>::
+void Definition<T, Func, typename std::enable_if<Func == nullptr>::type>::
     Destructor(void* data)
 {
     T *cast = (T*)data;
@@ -129,7 +129,7 @@ void Default<T, Func, typename std::enable_if<Func == nullptr>::type>::
 }
 
 template<typename T, T(*Func)()>
-std::true_type IsBaseOfDefault(Default<T, Func>);
+std::true_type IsBaseOfDefault(Definition<T, Func>);
 
 std::false_type IsBaseOfDefault(...);
 
@@ -137,4 +137,4 @@ std::false_type IsBaseOfDefault(...);
 
 } //!test
 
-#endif //!TEST_PTR_DEFAULT_H_
+#endif //!TEST_PTR_DEFINITION_H_
