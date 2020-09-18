34640966fee2a8abd49c19337069cf49270dc945
Ananta Yudica <nta040689@gmail.com>
2020-09-19 00:50:06 +0700

    aa

--- include/test/out/Standard.h
+++ include/test/out/Standard.h
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cstdarg>
 #include <cstddef>
+#include <cwchar>
 
 #ifndef TEST_ATTRIBUTE
 #ifdef __GNUC__
@@ -33,11 +34,49 @@ public:
 public:
     Standard<TChar>& operator=(const Standard<TChar>&) = delete;
     Standard<TChar>& operator=(Standard<TChar>&&) = delete;
+};
+
+template<>
+class Standard<char>
+{
 public:
-    virtual inline SizeType VPrint(const TChar * format, va_list var_args)
+    typedef std::size_t SizeType; 
+public:
+    Standard() = default;
+public:
+    virtual ~Standard() = default;
+public:
+    Standard(const Standard<char>&) = delete;
+    Standard(Standard<char>&&) = delete;
+public:
+    Standard<char>& operator=(const Standard<char>&) = delete;
+    Standard<char>& operator=(Standard<char>&&) = delete;
+public:
+    virtual inline SizeType VPrint(const char * format, va_list var_args)
         TEST_ATTRIBUTE ((__format__ (__printf__, 2, 0))) = 0;
-    virtual inline SizeType Print(const TChar * format, ...) 
+    virtual inline SizeType Print(const char * format, ...) 
         TEST_ATTRIBUTE((format(printf, 2, 3))) = 0;
+};
+
+template<>
+class Standard<wchar_t>
+{
+public:
+    typedef std::size_t SizeType; 
+public:
+    Standard() = default;
+public:
+    virtual ~Standard() = default;
+public:
+    Standard(const Standard<wchar_t>&) = delete;
+    Standard(Standard<wchar_t>&&) = delete;
+public:
+    Standard<wchar_t>& operator=(const Standard<wchar_t>&) = delete;
+    Standard<wchar_t>& operator=(Standard<wchar_t>&&) = delete;
+public:
+    virtual inline SizeType VPrint(const wchar_t * format, 
+        va_list var_args) = 0;
+    virtual inline SizeType Print(const wchar_t * format, ...) = 0;
 };
 
 } //!out
