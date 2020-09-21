8c8cbbd5fc3ba4f7879165ed2e32e8985abaa280
Ananta Yudica <nta040689@gmail.com>
2020-09-22 04:10:38 +0700

    aa

---
+++ include/test/out/Type.h
@@ -0,0 +1,17 @@
+#ifndef TEST_OUT_TYPE_H_
+#define TEST_OUT_TYPE_H_
+
+namespace test
+{
+namespace out
+{
+
+template<typename TChar>
+struct Type
+{};
+
+} //!out
+
+} //!test
+
+#endif //!TEST_OUT_TYPE_H_
