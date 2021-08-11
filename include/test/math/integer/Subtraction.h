34e803dcab48fd93a8ce193317d3f9ca882e6009
Ananta Yudica <nta040689@gmail.com>
2021-08-11 16:28:52 +0700

    Updated Subtraction function

--- include/test/math/integer/Subtraction.h
+++ include/test/math/integer/Subtraction.h
@@ -2,6 +2,7 @@
 #define TEST_MATH_INTEGER_SUBTRACTION_H_
 
 #include "Addition.h"
+#include "Negation.h"
 
 #include <cstddef>
 
@@ -12,21 +13,65 @@ namespace math
 namespace integer
 {
 
-template<typename TElementValue, typename TValue,
-    typename TExpandValue, typename TSize, TSize N, 
-    TElementValue(FSetValue)(TValue&, const TSize&,
+template<typename TElementValue, typename TAValue, typename TBValue,
+    typename TExpandValue, typename TSize, TSize NA, TSize NB, 
+    TElementValue(FSetAValue)(TAValue&, const TSize&,
         const TElementValue&),
-    TExpandValue(FGetValue)(const TValue&, const TSize&),
-    TExpandValue(FNegationValue)(const TValue&, const TSize&),
+    TElementValue(FSetBValue)(TAValue&, const TSize&,
+        const TElementValue&),
+    TExpandValue(FGetAValue)(const TAValue&, const TSize&),
+    TExpandValue(FGetBValue)(const TBValue&, const TSize&),
+    TExpandValue(FNegationValue)(const TExpandValue&, const TSize&),
     TExpandValue(FSplitValue)(const TExpandValue&),
     TElementValue(FElementValue)(const TExpandValue&),
-    TElementValue(FCarryValue)(const TExpandValue&)>
-static TElementValue Subtraction(TValue& a, const TValue& b,
-    const TSize& bg = 0, const TElementValue& c = 1)
+    TElementValue(FCarryValue)(const TExpandValue&),
+    TElementValue VElementBDefault = 0>
+static TElementValue Subtraction(TAValue& a, const TBValue& b,
+    const TElementValue& c = 0, const TSize& bg_a = 0, 
+    const TSize& bg_b = 0, const TSize& ed_b = NB, 
+    TElementValue c_b_in = 0, TElementValue * c_b_out = nullptr)
 {
-    return test::math::integer::Addition<TElementValue, TValue, 
-        TExpandValue, TSize, N, FSetValue, FGetValue, FSplitValue, 
-        FElementValue, FCarryValue, FNegationValue>(a, b, bg, c);
+    TBValue b_cpy = b;
+    TElementValue carry = c;
+    TElementValue c_b_default;
+    TElementValue & c_b = (c_b_out == nullptr ? c_b_default : *c_b_out);
+    c_b = c_b_in;
+    TExpandValue b_val;
+    TExpandValue diff = 0;
+    for(TSize i = 0, j = bg_b; i < NA; ++i, ++j)
+    {
+        if (j < ed_b)
+        {
+            if (j < NB)
+            {   
+                b_val = FGetBValue(b_cpy, j);
+                c_b = test::math::integer::Negation<TElementValue,
+                    TExpandValue, TSize, FNegationValue, FSplitValue,
+                    FElementValue, FCarryValue>(b_val, c_b, j);
+            }
+            else
+            {
+                b_val = VElementBDefault;
+                c_b = test::math::integer::Negation<TElementValue,
+                    TExpandValue, TSize, FNegationValue, FSplitValue,
+                    FElementValue, FCarryValue>(b_val, c_b, j);
+            }
+        }
+        else
+        {
+            b_val = VElementBDefault;
+        }
+
+        diff = FGetAValue(a, i);
+        diff += b_val;
+        diff += carry;
+
+        const auto split = FSplitValue(diff);
+        FSetAValue(a, i, FElementValue(split));
+        carry = FCarryValue(split);
+    }
+
+    return carry;
 }
 
 template<typename TElementValue, typename TValue,
@@ -34,17 +79,59 @@ template<typename TElementValue, typename TValue,
     TElementValue(FSetValue)(TValue&, const TSize&,
         const TElementValue&),
     TExpandValue(FGetValue)(const TValue&, const TSize&),
-    TExpandValue(FNegationValue)(const TExpandValue&),
+    TExpandValue(FNegationValue)(const TExpandValue&, const TSize&),
     TExpandValue(FSplitValue)(const TExpandValue&),
     TElementValue(FElementValue)(const TExpandValue&),
     TElementValue(FCarryValue)(const TExpandValue&),
-    TElementValue VElementBDefault>
+    TElementValue VElementBDefault = 0>
 static TElementValue Subtraction(TValue& a, const TExpandValue& b,
-    const TSize& bg = 0, const TElementValue& c = 1)
+    const TElementValue& c = 0, const TSize& bg = 0, 
+    TElementValue c_b_in = 0, TElementValue * c_b_out = nullptr)
 {
-    return Addition<TElementValue, TValue, TExpandValue, TSize, N, 
-        FSetValue, FGetValue, FSplitValue, FElementValue, 
-        FCarryValue, VElementBDefault>(a, FNegationValue(b), bg, c);
+    TElementValue carry = c;
+    TElementValue c_b_default;
+    TElementValue & c_b = (c_b_out == nullptr ? c_b_default : *c_b_out);
+    c_b = c_b_in;
+    TSize i = bg, j = 0;
+    if (i < N)
+    {
+        TExpandValue diff = 0;
+        TExpandValue b_val;
+        const TExpandValue split_b = FSplitValue(b);
+        TElementValue elem_b[2]{FElementValue(split_b), FCarryValue(split_b)};
+
+        for (; j < 2; ++j, ++i)
+        {
+            b_val = elem_b[j];
+            c_b = test::math::integer::Negation<TElementValue,
+                TExpandValue, TSize, FNegationValue, FSplitValue,
+                FElementValue, FCarryValue>(b_val, c_b, j);
+
+            diff = FGetValue(a, i);
+            diff += b_val;
+            diff += carry;
+            
+            auto split = FSplitValue(diff);
+            FSetValue(a, i, FElementValue(split));
+            carry = FCarryValue(split);
+        }
+        for (; i < N; ++i)
+        {
+            b_val = VElementBDefault;
+            c_b = test::math::integer::Negation<TElementValue,
+                TExpandValue, TSize, FNegationValue, FSplitValue,
+                FElementValue, FCarryValue>(b_val, c_b, j);
+
+            diff = FGetValue(a, i);
+            diff += b_val;
+            diff += carry;
+
+            const auto split = FSplitValue(diff);
+            FSetValue(a, i, FElementValue(split));
+            carry = FCarryValue(split);
+        }
+    }
+    return carry;
 }
 
 } //!integer
