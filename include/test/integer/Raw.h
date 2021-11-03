82682f4b2a0363f5f66c0beeded4fc0d0ddc2f9a
Ananta Yudica <nta040689@gmail.com>
2021-11-03 10:13:29 +0700

    Updated dependency of the Raw class with the Byte class

--- include/test/integer/Raw.h
+++ include/test/integer/Raw.h
@@ -2,6 +2,7 @@
 #define TEST_INTEGER_RAW_H_
 
 #include "Flag.h"
+#include "../Byte.h"
 
 #include <cstddef>
 #include <cstdint>
@@ -14,21 +15,24 @@ namespace integer
 {
 
 template<std::size_t N>
-class Raw
+class Raw : public test::Byte<N>
 {
 public:
     typedef test::integer::Flag FlagType;
     typedef typename FlagType::ValueType FlagValueType;
 private:
     test::integer::Flag m_flag;
-    std::uint8_t m_value[N];
 public:
     Raw(const FlagValueType& flag_val);
-    template<typename TArg, typename... TArgs, typename _TArg =
-        typename std::remove_cv<typename std::remove_pointer<
-            typename std::remove_reference<TArg>::type>::type>::type,
-        typename std::enable_if<std::is_integral<_TArg>::value, int>::type = 0>
-    Raw(const FlagValueType& flag_val, TArg&& arg, TArgs&&... args);
+    Raw(const FlagType& flag);
+    template<std::size_t N_>
+    Raw(const FlagValueType& flag_val, const test::Byte<N_>& byte_val);
+    template<std::size_t N_>
+    Raw(const FlagType& flag_val, const test::Byte<N_>& byte_val);
+    template<std::size_t N_>
+    Raw(const FlagValueType& flag_val, test::Byte<N_>&& byte_val);
+    template<std::size_t N_>
+    Raw(const FlagType& flag_val, test::Byte<N_>&& byte_val);
     ~Raw();
 public:
     template<std::size_t N_>
@@ -40,90 +44,79 @@ public:
     Raw<N>& operator=(const Raw<N_>& cpy);
     template<std::size_t N_>
     Raw<N>& operator=(Raw<N_>&& mov);
-public:
-    template<typename TInstance>
-    TInstance& Get(const std::size_t& n);
-    template<typename TInstance>
-    TInstance Get(const std::size_t& n) const;
-public:
-    template<typename TInstance>
-    TInstance Set(const std::size_t& n, const TInstance& val);
+    template<std::size_t N_>
+    Raw<N>& operator=(const test::Byte<N_>& byte_val);
+    template<std::size_t N_>
+    Raw<N>& operator=(test::Byte<N_>&& byte_val);
 public:
     FlagType& Flag();
     FlagType Flag() const;
-public:
-    std::uint8_t& operator[](const std::size_t& n);
-    std::uint8_t operator[](const std::size_t& n) const;
 };
 
 template<std::size_t N>
 Raw<N>::Raw(const FlagValueType& flag_val) :
-    m_flag(flag_val),
-    m_value{}
-{
-    for(std::size_t i = 0; i < N; ++i)
-        m_value[i] = 0;
-}
+    test::Byte<N>(),
+    m_flag(flag_val)
+{}
 
 template<std::size_t N>
-template<typename TArg, typename... TArgs, typename _TArg,
-    typename std::enable_if<std::is_integral<_TArg>::value, int>::type>
-Raw<N>::Raw(const FlagValueType& flag_val, TArg&& arg, TArgs&&... args) :
-    m_flag(flag_val),
-    m_value{std::forward<TArg>(arg), std::forward<TArgs>(args)...}
-{
-    const std::size_t rem = N - (sizeof...(TArgs) + 1);
-    for(std::size_t i = 1; i <= rem && i < N; ++i)
-        m_value[N - i] = 0;
-}
+Raw<N>::Raw(const FlagType& flag) :
+    test::Byte<N>(),
+    m_flag(flag)
+{}
+
+template<std::size_t N>
+template<std::size_t N_>
+Raw<N>::Raw(const FlagValueType& flag_val, const test::Byte<N_>& byte_val) :
+    test::Byte<N>(byte_val),
+    m_flag(flag_val)
+{}
+
+template<std::size_t N>
+template<std::size_t N_>
+Raw<N>::Raw(const FlagType& flag, const test::Byte<N_>& byte_val) :
+    test::Byte<N>(byte_val),
+    m_flag(flag)
+{}
+
+template<std::size_t N>
+template<std::size_t N_>
+Raw<N>::Raw(const FlagValueType& flag_val, test::Byte<N_>&& byte_val) :
+    test::Byte<N>(std::forward<test::Byte<N_>>(byte_val)),
+    m_flag(flag_val)
+{}
+
+template<std::size_t N>
+template<std::size_t N_>
+Raw<N>::Raw(const FlagType& flag, test::Byte<N_>&& byte_val) :
+    test::Byte<N>(std::forward<test::Byte<N_>>(byte_val)),
+    m_flag(flag)
+{}
 
 template<std::size_t N>
 Raw<N>::~Raw()
-{
-    for (std::size_t i = 0;i < N; ++i)
-        m_value[i] = 0;
-}
+{}
 
 template<std::size_t N>
 template<std::size_t N_>
 Raw<N>::Raw(const Raw<N_>& cpy) :
+    test::Byte<N>(cpy),
     m_flag(cpy.Flag())
-{
-    for (std::size_t i = 0;i < N && i < N_; ++i)
-        m_value[i] = cpy[i];
-    if (N > N_)
-        for (std::size_t i = N_;i < N; ++i)
-            m_value[i] = 0; 
-}
+{}
 
 template<std::size_t N>
 template<std::size_t N_>
 Raw<N>::Raw(Raw<N_>&& mov) :
+    test::Byte<N>(std::move(mov)),
     m_flag(std::move(mov.Flag()))
-{
-    for (std::size_t i = 0;i < N && i < N_; ++i)
-    {
-        m_value[i] = mov[i];
-        mov[i] = 0;
-    }
-    if (N > N_)
-        for (std::size_t i = N_;i < N; ++i)
-            m_value[i] = 0;
-    else
-        for (std::size_t i = N;i < N_; ++i)
-            mov[i] = 0;
-}
+{}
 
 template<std::size_t N>
 template<std::size_t N_>
 Raw<N>& Raw<N>::operator=(const Raw<N_>& cpy)
 {
+    test::Byte<N>::operator=(cpy);
     m_flag = cpy.Flag();
-    for (std::size_t i = 0;i < N && i < N_; ++i)
-        m_value[i] = cpy[i];
-    if (N > N_)
-        for (std::size_t i = N_;i < N; ++i)
-            m_value[i] = 0; 
     return *this;
 }
 
@@ -131,58 +124,25 @@ template<std::size_t N>
 template<std::size_t N_>
 Raw<N>& Raw<N>::operator=(Raw<N_>&& mov)
 {
+    test::Byte<N>::operator=(std::move(mov));
     m_flag = std::move(mov.Flag());
-    for (std::size_t i = 0;i < N && i < N_; ++i)
-    {
-        m_value[i] = mov[i];
-        mov[i] = 0;
-    }
-    if (N > N_)
-        for (std::size_t i = N_;i < N; ++i)
-            m_value[i] = 0;
-    else
-        for (std::size_t i = N;i < N_; ++i)
-            mov[i] = 0;
     return *this;
 }
 
 template<std::size_t N>
-template<typename TInstance>
-TInstance& Raw<N>::Get(const std::size_t& n)
+template<std::size_t N_>
+Raw<N>& Raw<N>::operator=(const test::Byte<N_>& byte_val)
 {
-    static TInstance instance;
-    const std::size_t off = n * sizeof(TInstance);
-    const std::size_t rem = N > off ? N - off : 0;
-    if (sizeof(TInstance) <= rem)
-    {
-        std::uint8_t * ptr = m_value;
-        return reinterpret_cast<TInstance&>(*(ptr + off));
-    }
-    return instance;
+    test::Byte<N>::operator=(byte_val);
+    return *this;
 }
 
 template<std::size_t N>
-template<typename TInstance>
-TInstance Raw<N>::Get(const std::size_t& n) const
+template<std::size_t N_>
+Raw<N>& Raw<N>::operator=(test::Byte<N_>&& byte_val)
 {
-    const std::size_t off = n * sizeof(TInstance);
-    const std::size_t rem = N > off ? N - off : 0;
-    if (sizeof(TInstance) <= rem)
-    {
-        const std::uint8_t * ptr = m_value;
-        return reinterpret_cast<const TInstance&>(*(ptr + off));
-    }
-    return {};
-}
-
-template<std::size_t N>
-template<typename TInstance>
-TInstance Raw<N>::Set(const std::size_t& n, const TInstance& val)
-{
-    TInstance& ref = this->Get<TInstance>(n);
-    TInstance ret = ref;
-    ref = val;
-    return ret;
+    test::Byte<N>::operator=(std::move(byte_val));
+    return *this;
 }
 
 template<std::size_t N>
@@ -195,18 +155,6 @@ template<std::size_t N>
 typename Raw<N>::FlagType Raw<N>::Flag() const
 {
     return m_flag;
-}
-
-template<std::size_t N>
-std::uint8_t& Raw<N>::operator[](const std::size_t& n)
-{
-    return this->m_value[n];
-}
-
-template<std::size_t N>
-std::uint8_t Raw<N>::operator[](const std::size_t& n) const
-{
-    return this->m_value[n];
 }
 
 } //!integer
