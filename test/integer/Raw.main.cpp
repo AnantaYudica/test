9987dc67fbab3ed7709306c689ffc49f254d2e68
Ananta Yudica <nta040689@gmail.com>
2021-11-03 10:13:40 +0700

    Updated test

--- test/integer/Raw.main.cpp
+++ test/integer/Raw.main.cpp
@@ -12,135 +12,130 @@ int main()
         test::integer::Raw<1> raw(flag);
         const test::integer::Raw<1>& raw_c = raw;
 
-        assert(raw.Get<std::uint8_t>(0) == 0);
-
-        raw.Get<std::uint8_t>(0) = 255;
-
-        assert(raw_c.Get<std::uint8_t>(0) == 255);
-
-        auto old = raw.Set<std::uint8_t>(0, 127);
-
-        assert(old == 255);
-        assert(raw_c.Get<std::uint8_t>(0) == 127);
-
+        assert(raw[0] == 0);
         assert(flag.Format() == raw.Flag().Format());
         assert(flag.Format() == raw_c.Flag().Format());
 
+        raw = test::Byte<1>(0xf8);
+
         test::integer::Raw<1> raw_1(raw);
-        assert(raw_1.Get<std::uint8_t>(0) == 127);
+        assert(raw_1[0] == 0xf8);
+        assert(flag.Format() == raw_1.Flag().Format());
 
         test::integer::Raw<1> raw_2(std::move(raw_1));
-        assert(raw_1.Get<std::uint8_t>(0) == 0);
-        assert(raw_2.Get<std::uint8_t>(0) == 127);
+        assert(raw_1[0] == 0);
+        assert(raw_2[0] == 0xf8);
+        assert(flag.Format() == raw_2.Flag().Format());
 
         raw_1 = raw_2;
-        assert(raw_1.Get<std::uint8_t>(0) == 127);
+        assert(raw_1[0] == 0xf8);
+        assert(flag.Format() == raw_1.Flag().Format());
 
         raw_2 = std::move(raw_1);
-        assert(raw_1.Get<std::uint8_t>(0) == 0);
-        assert(raw_2.Get<std::uint8_t>(0) == 127);
+        assert(raw_1[0] == 0);
+        assert(raw_2[0] == 0xf8);
+        assert(flag.Format() == raw_2.Flag().Format());
     }
     {
         test::integer::Flag flag(test::integer::Flag::kBinaryFormat);
-        test::integer::Raw<4> raw(flag);
+        test::integer::Raw<1> raw(test::integer::Flag::kBinaryFormat);
+        const test::integer::Raw<1>& raw_c = raw;
+
+        assert(raw[0] == 0);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
+        
+        raw = test::Byte<1>(0x68);
+        assert(raw[0] == 0x68);
+    }
+    {
+        test::integer::Flag flag(test::integer::Flag::kBinaryFormat);
+        test::Byte<2> b0(0xfffe);
+        test::integer::Raw<4> raw(flag, b0);
         const test::integer::Raw<4>& raw_c = raw;
 
-        assert(raw.Get<std::uint8_t>(0) == 0);
-        assert(raw.Get<std::uint8_t>(1) == 0);
-        assert(raw.Get<std::uint8_t>(2) == 0);
-        assert(raw.Get<std::uint8_t>(3) == 0);
+        assert(raw[0] == 0xfe);
+        assert(raw[1] == 0xff);
+        assert(raw[2] == 0);
+        assert(raw[3] == 0);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
 
-        raw.Get<std::uint8_t>(0) = 1;
-        raw.Get<std::uint8_t>(1) = 2;
-        raw.Get<std::uint8_t>(2) = 3;
-        raw.Get<std::uint8_t>(3) = 4;
+        raw = test::Byte<4>(0x01020304);
+        assert(raw[0] == 0x04);
+        assert(raw[1] == 0x03);
+        assert(raw[2] == 0x02);
+        assert(raw[3] == 0x01);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
 
-        assert(raw_c.Get<std::uint8_t>(0) == 1);
-        assert(raw_c.Get<std::uint8_t>(1) == 2);
-        assert(raw_c.Get<std::uint8_t>(2) == 3);
-        assert(raw_c.Get<std::uint8_t>(3) == 4);
+    }
+    
+    {
+        test::integer::Flag flag(test::integer::Flag::kBinaryFormat);
+        test::integer::Raw<4> raw(flag, test::Byte<4>(0x12340506));
+        const test::integer::Raw<4>& raw_c = raw;
 
-
-        auto old_1 = raw.Set<std::uint8_t>(0, 5);
-        auto old_2 = raw.Set<std::uint8_t>(1, 6);
-        auto old_3 = raw.Set<std::uint8_t>(2, 7);
-        auto old_4 = raw.Set<std::uint8_t>(3, 8);
-
-        assert(old_1 == 1);
-        assert(old_2 == 2);
-        assert(old_3 == 3);
-        assert(old_4 == 4);
-        assert(raw_c.Get<std::uint8_t>(0) == 5);
-        assert(raw_c.Get<std::uint8_t>(1) == 6);
-        assert(raw_c.Get<std::uint8_t>(2) == 7);
-        assert(raw_c.Get<std::uint8_t>(3) == 8);
-
-        raw.Get<std::uint16_t>(0) = 0x00ff;
-        raw.Get<std::uint16_t>(1) = 0xff00;
-
+        assert(raw[0] == 0x06);
+        assert(raw[1] == 0x05);
+        assert(raw[2] == 0x34);
+        assert(raw[3] == 0x12);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
         
-        assert(raw_c.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_c.Get<std::uint16_t>(1) == 0xff00);
+        test::Byte<2> b0(0x2236);
+        raw = b0;
+        assert(raw[0] == 0x36);
+        assert(raw[1] == 0x22);
+        assert(raw[2] == 0x34);
+        assert(raw[3] == 0x12);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
 
+    }
+    {
+        test::integer::Flag flag(test::integer::Flag::kBinaryFormat);
+        test::Byte<2> b0(0xfffe);
+        test::integer::Raw<4> raw(test::integer::Flag::kBinaryFormat, b0);
+        const test::integer::Raw<4>& raw_c = raw;
+
+        assert(raw[0] == 0xfe);
+        assert(raw[1] == 0xff);
+        assert(raw[2] == 0);
+        assert(raw[3] == 0);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
+
+        raw = test::Byte<4>(0x01020304);
+        assert(raw[0] == 0x04);
+        assert(raw[1] == 0x03);
+        assert(raw[2] == 0x02);
+        assert(raw[3] == 0x01);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
+
+    }
+    {
+        test::integer::Flag flag(test::integer::Flag::kBinaryFormat);
+        test::integer::Raw<4> raw(test::integer::Flag::kBinaryFormat, 
+            test::Byte<4>(0x12340506));
+        const test::integer::Raw<4>& raw_c = raw;
+
+        assert(raw[0] == 0x06);
+        assert(raw[1] == 0x05);
+        assert(raw[2] == 0x34);
+        assert(raw[3] == 0x12);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
         
-        test::integer::Raw<8> raw_1(raw);
-        assert(raw_1.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_1.Get<std::uint16_t>(1) == 0xff00);
-        assert(raw_1.Get<std::uint16_t>(2) == 0);
-        assert(raw_1.Get<std::uint16_t>(3) == 0);
-
-        test::integer::Raw<4> raw_a(raw);
-        test::integer::Raw<8> raw_2(std::move(raw_a));
-        assert(raw_a.Get<std::uint16_t>(0) == 0);
-        assert(raw_a.Get<std::uint16_t>(1) == 0);
-        
-        assert(raw_2.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_2.Get<std::uint16_t>(1) == 0xff00);
-        assert(raw_2.Get<std::uint16_t>(2) == 0);
-        assert(raw_2.Get<std::uint16_t>(3) == 0);
-
-        raw_1 = raw;
-        assert(raw_1.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_1.Get<std::uint16_t>(1) == 0xff00);
-        assert(raw_1.Get<std::uint16_t>(2) == 0);
-        assert(raw_1.Get<std::uint16_t>(3) == 0);
-
-        raw_a = raw;
-        assert(raw_a.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_a.Get<std::uint16_t>(1) == 0xff00);
-        raw_2 = std::move(raw_a);
-        assert(raw_a.Get<std::uint16_t>(0) == 0);
-        assert(raw_a.Get<std::uint16_t>(1) == 0);
-        
-        assert(raw_2.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_2.Get<std::uint16_t>(1) == 0xff00);
-        assert(raw_2.Get<std::uint16_t>(2) == 0);
-        assert(raw_2.Get<std::uint16_t>(3) == 0);
-
-        test::integer::Raw<2> raw_3(raw);
-        assert(raw_3.Get<std::uint16_t>(0) == 0x00ff);
-
-        raw_a = raw;
-        assert(raw_a.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_a.Get<std::uint16_t>(1) == 0xff00);
-        test::integer::Raw<2> raw_4(std::move(raw_a));
-        assert(raw_a.Get<std::uint16_t>(0) == 0);
-        assert(raw_a.Get<std::uint16_t>(1) == 0);
-
-        assert(raw_4.Get<std::uint16_t>(0) == 0x00ff);
-
-        raw_3 = raw;
-        assert(raw_3.Get<std::uint16_t>(0) == 0x00ff);
-
-        raw_a = raw;
-        assert(raw_a.Get<std::uint16_t>(0) == 0x00ff);
-        assert(raw_a.Get<std::uint16_t>(1) == 0xff00);
-        raw_4 = std::move(raw_a);
-        assert(raw_a.Get<std::uint16_t>(0) == 0);
-        assert(raw_a.Get<std::uint16_t>(1) == 0);
-
-        assert(raw_4.Get<std::uint16_t>(0) == 0x00ff);
-        
+        test::Byte<2> b0(0x2236);
+        raw = b0;
+        assert(raw[0] == 0x36);
+        assert(raw[1] == 0x22);
+        assert(raw[2] == 0x34);
+        assert(raw[3] == 0x12);
+        assert(flag.Format() == raw.Flag().Format());
+        assert(flag.Format() == raw_c.Flag().Format());
 
     }
     return 0;
