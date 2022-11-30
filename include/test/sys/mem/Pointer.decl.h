c46529f20f522ad205a94adbe80bf7e6f6c8849a
Ananta Yudica <nta040689@gmail.com>
2022-11-30 18:45:11 +0700

    Added declare class test::sys::mem::Pointer

---
+++ include/test/sys/mem/Pointer.decl.h
@@ -0,0 +1,20 @@
+#ifndef TEST_SYS_MEM_POINTER_H_DECL_
+#define TEST_SYS_MEM_POINTER_H_DECL_
+
+namespace test
+{
+namespace sys
+{
+namespace mem
+{
+
+template<typename TBlock>
+class Pointer;
+
+} //!mem
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_MEM_POINTER_H_DECL_
