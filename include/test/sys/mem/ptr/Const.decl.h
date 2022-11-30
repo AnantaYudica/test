511457c08435c663529f61f09def5a2be20ae8e2
Ananta Yudica <nta040689@gmail.com>
2022-11-30 18:45:45 +0700

    Added declare class test::sys::mem::ptr::Const

---
+++ include/test/sys/mem/ptr/Const.decl.h
@@ -0,0 +1,24 @@
+#ifndef TEST_SYS_MEM_PTR_CONST_H_DECL_
+#define TEST_SYS_MEM_PTR_CONST_H_DECL_
+
+namespace test
+{
+namespace sys
+{
+namespace mem
+{
+namespace ptr
+{
+
+template<typename TBlock>
+class Const;
+
+} //!ptr
+
+} //!mem
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_MEM_PTR_CONST_H_DECL_
