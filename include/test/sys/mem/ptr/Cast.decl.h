0d7cd119269b381d446ff2a63854c1a5f9fb8d5b
Ananta Yudica <nta040689@gmail.com>
2022-11-30 18:46:10 +0700

    Added declare class test::sys::mem::ptr::Cast

---
+++ include/test/sys/mem/ptr/Cast.decl.h
@@ -0,0 +1,24 @@
+#ifndef TEST_SYS_MEM_PTR_CAST_H_DECL_
+#define TEST_SYS_MEM_PTR_CAST_H_DECL_
+
+namespace test
+{
+namespace sys
+{
+namespace mem
+{
+namespace ptr
+{
+
+template<typename T, typename TBlock>
+class Cast;
+
+} //!ptr
+
+} //mem
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_MEM_PTR_CAST_H_DECL_
