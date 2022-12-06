f8f423b130dd84fe6a93bb1e46947bc68466ddc4
Ananta Yudica <nta040689@gmail.com>
2022-12-06 22:39:31 +0700

    Updated dependencies and Added debug and signal

--- include/test/sys/Interface.h
+++ include/test/sys/Interface.h
@@ -1,7 +1,14 @@
 #ifndef TEST_SYS_INTERFACE_H_
 #define TEST_SYS_INTERFACE_H_
 
+#ifndef TEST_SYS_DEBUG_ENABLE
+#define TEST_SYS_DEBUG_ENABLE 0
+#endif //!TEST_SYS_DEBUG_ENABLE
+
 #include "Definition.h"
+#include "Signal.defn.h"
+#include "Debug.h"
+#include "dbg/Type.h"
 
 #include <cstdio>
 #include <cstdarg>
@@ -19,18 +26,21 @@ private:
     private:
         Interface* m_value;
     public:
-        Instance();
-        ~Instance();
+        inline Instance();
+        inline ~Instance();
     public:
-        operator bool();
+        inline operator bool();
     public:
-        void Set(Interface* instance);
-        Interface* Get();
+        inline void Set(Interface* instance);
+        inline Interface* Get();
     };
 public:
+    typedef test::sys::Signal SignalType;
     typedef typename test::sys::Definition::StatusIntegerType 
         StatusIntegerType;
     typedef typename test::sys::Definition::Status Status;
+public:
+    typedef typename test::sys::Debug DebugType;
 private:
     static inline typename Interface::Instance& _GetInstance();
 protected:
@@ -42,6 +52,10 @@ protected:
 public:
     virtual ~Interface() = default;
 public:
+    virtual inline void RegisterSignal(SignalType* signal);
+public:
+    virtual inline void UnregisterSignal(SignalType* signal);
+public:
     virtual inline int Output(const char* format, ...)
         TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
     virtual inline int VOutput(const char* format, va_list args)
@@ -51,6 +65,13 @@ public:
         TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
     virtual inline int VInfo(const char* format, va_list args)
         TEST_ATTRIBUTE ((__format__ (__printf__, 2, 0)));
+public:
+    virtual inline int Debug(DebugType& dbg, std::int8_t level, const void * obj,
+        const char* format, ...)
+        TEST_ATTRIBUTE((__format__ (__printf__, 5, 6)));
+    virtual inline int VDebug(DebugType& dbg, std::int8_t level, const void * obj,
+        const char* format, va_list args)
+        TEST_ATTRIBUTE ((__format__ (__printf__, 5, 0)));
 public:
     virtual inline int Error(Status code, const char* format, ...)
         TEST_ATTRIBUTE((__format__ (__printf__, 3, 4)));
@@ -62,29 +83,35 @@ public:
         va_list args) TEST_ATTRIBUTE ((__format__ (__printf__, 3, 0)));
 };
 
-Interface::Instance::Instance() :
+inline Interface::Instance::Instance() :
     m_value(nullptr)
 {}
 
-Interface::Instance::~Instance()
+inline Interface::Instance::~Instance()
 {
     m_value = nullptr;
 }
 
-Interface::Instance::operator bool()
+inline Interface::Instance::operator bool()
 {
     return m_value != nullptr;
 }
 
-void Interface::Instance::Set(Interface* instance)
+inline void Interface::Instance::Set(Interface* instance)
 {
     m_value = instance;
 }
 
-Interface* Interface::Instance::Get()
+inline Interface* Interface::Instance::Get()
 {
     return m_value;
 }
+
+inline void Interface::RegisterSignal(SignalType*)
+{}
+
+inline void Interface::UnregisterSignal(SignalType*)
+{}
 
 inline typename Interface::Instance& Interface::_GetInstance()
 {
@@ -126,7 +153,7 @@ inline int Interface::Output(const char* format, ...)
 
 inline int Interface::VOutput(const char* format, va_list args)
 {
-    return vprintf(format, args);
+    return vprintf(format, args) + printf("\n");
 }
 
 inline int Interface::Info(const char* format, ...)
@@ -142,6 +169,31 @@ inline int Interface::Info(const char* format, ...)
 inline int Interface::VInfo(const char* format, va_list args)
 {
     return VOutput(format, args);
+}
+
+inline int Interface::Debug(DebugType& dbg, std::int8_t level, const void * obj,
+    const char* format, ...)
+{
+#if TEST_SYS_DEBUG_ENABLE
+    int res = 0;
+    va_list args;
+    va_start(args, format);
+    res = VOutput(format, args);
+    va_end(args);
+    return res;
+#else
+    return 0;
+#endif
+}
+
+inline int Interface::VDebug(DebugType& dbg, std::int8_t level, const void * obj,
+    const char* format, va_list args)
+{
+#if TEST_SYS_DEBUG_ENABLE
+    return VOutput(format, args);
+#else
+    return 0;
+#endif
 }
 
 inline int Interface::Error(Status, const char* format, ...)
