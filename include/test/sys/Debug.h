bf0f913542d7d6ba344b48da320376d8274bde32
Ananta Yudica <nta040689@gmail.com>
2022-12-06 21:28:56 +0700

    Added class Debug

---
+++ include/test/sys/Debug.h
@@ -0,0 +1,79 @@
+#ifndef TEST_SYS_DEBUG_H_
+#define TEST_SYS_DEBUG_H_
+
+#include "Definition.h"
+
+#include <cstring>
+#include <cstdint>
+
+namespace test
+{
+namespace sys
+{
+
+class Debug
+{
+private:
+    typedef test::sys::Definition DefinitionType;
+public:
+    static inline std::size_t WriteTagName(char * tag_out, std::size_t n);
+public:
+    static inline std::size_t WriteName(char * name_out, std::size_t n);
+private:
+    std::int8_t m_level;
+protected:
+    inline Debug();
+public:
+    virtual inline ~Debug();
+public:
+    inline void SetLevel(std::int8_t level);
+public:
+    inline std::int8_t GetLevel();
+public:
+    virtual inline std::size_t TagName(char * tag_out, std::size_t n);
+public:
+    virtual inline std::size_t Name(char * name_out, std::size_t n);
+};
+
+inline std::size_t Debug::WriteTagName(char * tag_out, std::size_t n)
+{
+    return snprintf(tag_out, n, "unknown");
+}
+
+inline std::size_t Debug::WriteName(char * name_out, std::size_t n)
+{
+    return snprintf(name_out, n, "?");
+}
+
+inline Debug::Debug() :
+    m_level(0xFF)
+{}
+
+inline Debug::~Debug()
+{}
+
+inline void Debug::SetLevel(std::int8_t level)
+{
+    m_level = level;
+}
+
+inline std::int8_t Debug::GetLevel()
+{
+    return m_level;
+}
+
+inline std::size_t Debug::TagName(char * tag_out, std::size_t n)
+{
+    return WriteTagName(tag_out, n);
+}
+
+inline std::size_t Debug::Name(char* name_out, std::size_t n)
+{
+    return WriteName(name_out, n);
+}
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_DEBUG_H_
