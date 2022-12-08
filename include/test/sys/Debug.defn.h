bfa980868a65b61f85f207e489338ef6f101ef22
Ananta Yudica <nta040689@gmail.com>
2022-12-08 21:13:12 +0700

    Added definition class test::sys::Debug

---
+++ include/test/sys/Debug.defn.h
@@ -0,0 +1,49 @@
+#ifndef TEST_SYS_DEBUG_H_DEFN_
+#define TEST_SYS_DEBUG_H_DEFN_
+
+#include "dbg/CStr.h"
+#include "Debug.decl.h"
+
+#include <cstring>
+#include <cstdint>
+#include <cstdio>
+
+#define TEST_SYS_DEBUG_T_NAME_BUFF_SIZE 1028
+
+namespace test
+{
+namespace sys
+{
+
+class Debug
+{
+public:
+    typedef test::sys::dbg::CStr<TEST_SYS_DEBUG_T_NAME_BUFF_SIZE> CStrType;
+public:
+    static inline std::size_t WriteTagName(char * tag_out, std::size_t n);
+public:
+    static inline std::size_t WriteName(char * name_out, std::size_t n);
+private:
+    std::int8_t m_level;
+protected:
+    inline Debug();
+public:
+    virtual inline ~Debug();
+public:
+    inline void SetLevel(std::int8_t level);
+public:
+    inline std::int8_t GetLevel();
+public:
+    virtual inline std::size_t TagName(char * tag_out, std::size_t n);
+public:
+    virtual inline std::size_t Name(char * name_out, std::size_t n);
+public:
+    virtual inline CStrType& Name(CStrType& cstr);
+};
+
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_DEBUG_H_DEFN_
