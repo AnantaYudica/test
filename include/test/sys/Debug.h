b6b25e8f50a80ed2037681d6084d000b9e5be4d7
Ananta Yudica <nta040689@gmail.com>
2022-12-08 12:26:11 +0700

    Updated dependency, member function, and macro function

--- include/test/sys/Debug.h
+++ include/test/sys/Debug.h
@@ -2,9 +2,12 @@
 #define TEST_SYS_DEBUG_H_
 
 #include "Definition.h"
+#include "dbg\CStr.h"
 
 #include <cstring>
 #include <cstdint>
+
+#define TEST_SYS_DEBUG_T_NAME_BUFF_SIZE 255
 
 namespace test
 {
@@ -15,6 +18,8 @@ class Debug
 {
 private:
     typedef test::sys::Definition DefinitionType;
+public:
+    typedef test::sys::dbg::CStr<TEST_SYS_DEBUG_T_NAME_BUFF_SIZE> CStrType;
 public:
     static inline std::size_t WriteTagName(char * tag_out, std::size_t n);
 public:
@@ -33,6 +38,8 @@ public:
     virtual inline std::size_t TagName(char * tag_out, std::size_t n);
 public:
     virtual inline std::size_t Name(char * name_out, std::size_t n);
+public:
+    virtual inline CStrType& Name(CStrType& cstr);
 };
 
 inline std::size_t Debug::WriteTagName(char * tag_out, std::size_t n)
@@ -72,6 +79,12 @@ inline std::size_t Debug::Name(char* name_out, std::size_t n)
     return WriteName(name_out, n);
 }
 
+inline typename Debug::CStrType& Debug::Name(CStrType& cstr)
+{
+    WriteName(cstr.Buffer(), cstr.BufferSize());
+    return cstr;
+}
+
 } //!sys
 
 } //!test
@@ -82,7 +95,17 @@ inline std::size_t Debug::Name(char* name_out, std::size_t n)
 test::sys::dbg::Type<__VA_ARGS__>::GetInstance().SetLevel(VAL)
 
 #define TEST_SYS_DEBUG(SYS_TYPE, DEBUG_TYPE, ...)\
-    SYS_TYPE::GetInstance().Debug(DEBUG_TYPE::GetInstance(), __VA_ARGS__);
+    SYS_TYPE::GetInstance().Debug(DEBUG_TYPE::GetInstance(), __VA_ARGS__)
 
+#define TEST_SYS_DEBUG_T_NAME_STR(...)\
+    test::sys::dbg::Type<__VA_ARGS__>::GetInstance().Name(__t_name_cstr__).Buffer()
+
+#define TEST_SYS_DEBUG_T(SYS_TYPE, DEBUG_TYPE, ...)\
+do\
+{\
+    typename test::sys::Debug::CStrType __t_name_cstr__;\
+    SYS_TYPE::GetInstance().Debug(DEBUG_TYPE::GetInstance(), __VA_ARGS__);\
+}\
+while(false)
 
 #endif //!TEST_SYS_DEBUG_H_
