cfa069fce75777698a1d4f3d52460809a0f499ee
Ananta Yudica <nta040689@gmail.com>
2022-12-12 00:43:37 +0700

    Updated class test::sys::Memory and Moved implementation class test::sys::Memory into Memory.impl.h

--- include/test/sys/Memory.h
+++ include/test/sys/Memory.h
@@ -1,283 +1,111 @@
-#ifndef TEST_MEMORY_H_
-#define TEST_MEMORY_H_
+#ifndef TEST_SYS_MEMORY_H_
+#define TEST_SYS_MEMORY_H_
 
-#ifdef USING_TEST_MEMORY
+#ifndef TEST_SYS_MEMORY_DEBUG_ALLOC
+#define TEST_SYS_MEMORY_DEBUG_ALLOC 0
+#endif //!TEST_SYS_MEMORY_DEBUG_ALLOC
+#ifndef TEST_SYS_MEMORY_DEBUG_DEALLOC
+#define TEST_SYS_MEMORY_DEBUG_DEALLOC 0
+#endif //!TEST_SYS_MEMORY_DEBUG_DEALLOC
 
-#ifndef TEST_MEMORY_INIT_BLOCK_SIZE
-#define TEST_MEMORY_INIT_BLOCK_SIZE 10
-#endif //!TEST_MEMORY_INIT_BLOCK_SIZE
-
-#ifndef TEST_MEMORY_MULTIPLY_BLOCK_SIZE
-#define TEST_MEMORY_MULTIPLY_BLOCK_SIZE 10
-#endif //!TEST_MEMORY_MULTIPLY_BLOCK_SIZE
-
-#ifndef TEST_MEMORY_DEBUG_ALLOC
-#define TEST_MEMORY_DEBUG_ALLOC 0
-#endif //!TEST_MEMORY_DEBUG_ALLOC
-#ifndef TEST_MEMORY_DEBUG_DEALLOC
-#define TEST_MEMORY_DEBUG_DEALLOC 0
-#endif //!TEST_MEMORY_DEBUG_DEALLOC
+#include "Definition.h"
+#include "Interface.h"
+#include "Debug.h"
 
 #include "mem/Block.h"
-#include "Status.h"
+#include "mem/ptr/Const.defn.h"
+#include "mem/Pointer.defn.h"
+#include "mem/Record.h"
 
 #include <cstdio>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <functional>
 
-#ifndef TEST_ATTRIBUTE
-#ifdef __GNUC__
-#define TEST_ATTRIBUTE(...) __attribute__(__VA_ARGS__)
-#else
-#define TEST_ATTRIBUTE(...)
-#endif
-#endif //!TEST_ATTRIBUTE
+namespace test::sys
+{
+class Memory;
+}
+
+TEST_SYS_DBG_TYPE_DEFINE("test::sys::Memory", test::sys::Memory);
 
 namespace test
 {
+namespace sys
+{
 
-template<typename TStatus = test::Status>
 class Memory final
 {
-public:
-    typedef TStatus StatusType;
 private:
-    mem::Block* m_blocks;
-    std::size_t m_blocksLength;
-    std::size_t m_blocksSize;
-    StatusType* m_status;
+    typedef test::sys::Definition DefinitionType;
+    typedef test::sys::Interface SystemType;
+    typedef test::sys::dbg::Type<test::sys::Memory> DebugType;
+    typedef test::sys::mem::block::Base::Data DataType;
+    typedef test::sys::mem::block::Base::StatusIntegerType StatusIntegerType;
 public:
-    Memory(StatusType& status);
+#ifdef USING_TEST_MEMORY
+    typedef test::sys::mem::Block<test::sys::mem::block::Base,
+        test::sys::mem::block::FileLine> BlockType;
+#else
+    typedef test::sys::mem::Block<test::sys::mem::block::Base> BlockType;
+#endif 
 public:
-    Memory(const Memory<TStatus>&) = delete;
-    Memory(Memory<TStatus>&& mov);
+    typedef test::sys::mem::Pointer<BlockType> PointerType;
+    typedef test::sys::mem::ptr::Const<BlockType> PointerConstType;
 public:
-    ~Memory();
+    typedef test::sys::mem::Record<BlockType> RecordType;
 public:
-    Memory<TStatus>& operator=(const Memory<TStatus>&) = delete;
-    Memory<TStatus>& operator=(Memory<TStatus>&&) = delete;
-public:
-    void Set(StatusType& status);
+    static inline Memory& GetInstance();
 private:
-    void Info(const char* format, ...) 
-        TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
-    void Debug(const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
-    void Error(const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
+    static inline bool Deallocator(StatusIntegerType status, DataType data);
 private:
-    void ReallocationBlock();
-    void AppendBlock(mem::Block& b);
+    std::size_t m_countId;
+    RecordType m_record;
+    std::mutex m_lock;
+private:
+    inline Memory();
 public:
-    void Register(void* p, const std::size_t s);
+    inline Memory(const Memory&) = delete;
+    inline Memory(Memory&&) = delete;
+public:
+    inline ~Memory();
+public:
+    inline Memory& operator=(const Memory&) = delete;
+    inline Memory& operator=(Memory&&) = delete;
+public:
+    inline void* operator new (std::size_t) = delete;
+    inline void* operator new[] (std::size_t) = delete;
+    inline void operator delete(void*) = delete;
+    inline void operator delete[](void*) = delete;
+public:
+    inline bool HasRegister(const void * p);
+    inline bool HasRegister(const PointerConstType& p);
+public:
     template<std::size_t N>
-    void Register(void* p, const std::size_t s,
+    inline PointerType Allocate(const std::size_t& s,
         const char (&f)[N], const int& l);
-    void Unregister(void * p);
-    bool HasRegister(void * p);
+    inline PointerType Allocate(const std::size_t& s);
+public:
+    inline bool Reallocate(void* p, const std::size_t& s);
+    inline bool Reallocate(PointerType p, const std::size_t& s);
+public:
+    inline bool Free(void* p, bool force = false);
+    inline bool Free(PointerType& p, bool force = false);
+public:
+    inline std::size_t OverheadSize() const;
+public:
+    inline std::size_t Size() const;
 };
 
-template<typename TStatus>
-Memory<TStatus>::Memory(StatusType& status) :
-    m_blocks(nullptr),
-    m_blocksLength(TEST_MEMORY_INIT_BLOCK_SIZE),
-    m_blocksSize(0),
-    m_status(&status)
-{
-    m_blocks = (mem::Block*)std::malloc(m_blocksLength * 
-        sizeof(mem::Block));
-    assert(m_blocks != NULL);
-    const auto && _default = mem::Block();
-    for (std::size_t i = 0; i < m_blocksLength; ++i)
-        memcpy((void*)(m_blocks + i), (void*)&_default, sizeof(mem::Block));
-    Debug("Memory Register Begin {length : %zu, size : %zu bytes}\n",
-        m_blocksLength, m_blocksLength * sizeof(mem::Block));
-}
-
-template<typename TStatus>
-Memory<TStatus>::Memory(Memory<TStatus>&& mov) :
-    m_blocks(mov.m_blocks),
-    m_blocksLength(mov.m_blocksLength),
-    m_blocksSize(mov.m_blocksSize)
-{
-    mov.m_blocks = NULL;
-    mov.m_blocksLength = 0;
-    mov.m_blocksSize = 0;
-}
-
-template<typename TStatus>
-Memory<TStatus>::~Memory()
-{
-    if (m_blocksLength != 0)
-    {
-        if (m_blocksSize != 0)
-        {
-            Debug("Memory Register {size : %zu}\n", m_blocksSize);
-            for (std::size_t i = 0; i < m_blocksSize; ++i)
-            {
-                Error("Memory Block {addr : %p, size : %zu byte} "
-                    "has not deallocation\n", (m_blocks + i)->Pointer(),
-                    (m_blocks + i)->Size());
-                if ((m_blocks + i)->File() != nullptr)
-                {
-                    Error(" in file %s, line %d\n",
-                        (m_blocks + i)->File(), (m_blocks + i)->Line());
-                }
-            }
-        }
-        if (m_blocks != nullptr)
-        {
-            std::free(m_blocks);
-            Debug("Memory Register End\n");
-        }
-    }
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Set(StatusType& status)
-{
-    assert(m_status == NULL);
-    m_status = &status;
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Info(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Debug(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Error(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-    m_status->Error();
-}
-
-template<typename TStatus>
-void Memory<TStatus>::ReallocationBlock()
-{
-    if (m_blocksSize == m_blocksLength)
-    {
-        const auto old_length = m_blocksLength;
-        m_blocksLength *= TEST_MEMORY_MULTIPLY_BLOCK_SIZE;
-        mem::Block* new_blocks = (mem::Block*)std::malloc(m_blocksLength * 
-            sizeof(mem::Block));
-        if (new_blocks == NULL)
-        {
-            Error("Error Realloc {from %zu, byte to %zu bytes}\n", 
-                old_length * sizeof(mem::Block), m_blocksLength * sizeof(mem::Block));
-            m_blocksLength = old_length;
-            return;
-        }
-        memcpy((void*) new_blocks, (void*) m_blocks, sizeof(mem::Block) * old_length);
-        free(m_blocks);
-        m_blocks = new_blocks;
-        const auto && _default = mem::Block();
-        for (std::size_t i = old_length; i < m_blocksLength; ++i)
-            memcpy((void*)(m_blocks + i), (void*)&_default, sizeof(mem::Block));
-        Debug("Memory Register Realloc {length : %zu, " 
-            "size : %zu bytes}\n", m_blocksLength, 
-            m_blocksLength * sizeof(mem::Block));
-    }
-}
-
-template<typename TStatus>
-void Memory<TStatus>::AppendBlock(mem::Block& b)
-{
-    if (!HasRegister(b.Pointer()))
-    {
-#if TEST_MEMORY_DEBUG_ALLOC
-        Debug("alloc-memory {addr : %p, size : %zu bytes}\n",
-                b.Pointer(), b.Size());
-#endif
-        *(m_blocks + m_blocksSize) = std::move(b);
-        ++m_blocksSize; 
-    }
-    else
-    {
-        Error("Memory Register duplicate! {Addrs : %p}\n",
-            b.Pointer());
-    }
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Register(void* p, const std::size_t s)
-{
-    assert(p != NULL);
-    assert(m_blocks != NULL);
-    ReallocationBlock();
-    mem::Block b(p, s);
-    AppendBlock(b);
-}
-
-template<typename TStatus>
-template<std::size_t N>
-void Memory<TStatus>::Register(void* p, const std::size_t s,
-    const char (&f)[N], const int& l)
-{
-    assert(p != NULL);
-    assert(m_blocks != NULL);
-    ReallocationBlock();
-    mem::Block b(p, s, f, l);
-    AppendBlock(b);
-}
-
-template<typename TStatus>
-void Memory<TStatus>::Unregister(void * p)
-{
-    bool found = false;
-    std::size_t iReplace = 0;
-    for (std::size_t i = 0; i < m_blocksSize; ++i)
-    {
-        if (!found)
-        {
-            found = (p == (m_blocks + i)->Pointer());
-            iReplace = i;
-            if (found)
-            {
-#if TEST_MEMORY_DEBUG_DEALLOC
-                Debug("dealloc-memory {addr : %p, "
-                    "size : %zu bytes}\n", (m_blocks + i)->Pointer(),
-                    (m_blocks + i)->Size());
-#endif 
-            }
-        }
-        else
-        {
-            *(m_blocks + iReplace++) =
-                std::move(*(m_blocks + i));
-        }
-    }
-    if (found) --m_blocksSize;
-}
-
-template<typename TStatus>
-bool Memory<TStatus>::HasRegister(void * p)
-{
-    if (m_blocksSize == 0) return false;
-    for (std::size_t i = 0; i < m_blocksSize; ++i)
-    {
-        if (p == (m_blocks + i)->Pointer())
-            return true;
-    }
-    return false;
-}
+} //!sys
 
 } //!test
 
-#endif //!USING_TEST_MEMORY
+#ifndef TEST_SYS_MEMORY_H_IMPL_LATER
 
-#endif //!TEST_MEMORY_H_
+#include "Memory.impl.h"
+
+#endif //!TEST_SYS_MEMORY_H_IMPL_LATER
+
+#endif //!TEST_SYS_MEMORY_H_
---
+++ include/test/sys/Memory.impl.h
@@ -0,0 +1,276 @@
+#ifndef TEST_SYS_MEMORY_H_IMPL_
+#define TEST_SYS_MEMORY_H_IMPL_
+
+#include "Memory.h"
+#include "mem/ptr/Const.h"
+#include "mem/Pointer.h"
+
+namespace test
+{
+namespace sys
+{
+
+inline Memory& Memory::GetInstance()
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, NULL, "GetInstance()");
+        
+    static Memory instance;
+    return instance;
+}
+
+inline bool Memory::Deallocator(StatusIntegerType status, DataType data)
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, NULL, 
+        "Deallocator(status=0x%hx, data={id=%zu, pointer=%p, size=%zu})", 
+            status, data.id, data.pointer, data.size);
+    
+    if (data.pointer != nullptr)
+    {
+        std::free(data.pointer);
+        return true;
+    }
+    return false;
+}
+
+inline Memory::Memory() :
+    m_countId(1),
+    m_record(),
+    m_lock()
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Default Constructor");
+
+}
+
+inline Memory::~Memory()
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Destructor");
+
+    if (m_record.Size() != 0)
+    {
+        TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+            "Memory not clear on finalize {size=%zu}", m_record.Size());
+        m_record.ForEach([&](BlockType* block){
+            if (block->Line() != -1)
+            {
+                
+                TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+                    "Memory Block {addr=%p, size=%zu byte,} in "
+                    "file=%s, line=%d", block->Pointer(), block->Size(),
+                        block->File(), block->Line());
+            }
+            else 
+            {
+                TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+                    "Memory Block {addr=%p, size=%zu byte}", 
+                        block->Pointer(), block->Size());
+            }
+        });
+        SystemType::GetInstance().Error(
+            Definition::Status::sMemoryNotClearOnFinalize,
+            "Memory not clear on finalize {size=%zu}", m_record.Size());
+    }
+}
+
+template<std::size_t N>
+inline typename Memory::PointerType Memory::Allocate(const std::size_t& s,
+    const char (&f)[N], const int& l)
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Allocate<%zu>(s=%zu, f=%s, l=%d)", N, s, f, l);
+    
+    auto p = std::malloc(s);
+
+    if (p == NULL)
+    {
+        SystemType::GetInstance().Error(
+            Definition::Status::sMemBlockAllocationFailed,
+            "Allocation memory failed");
+        return {};
+    }
+    
+    {
+        std::lock_guard<std::mutex> guard{m_lock};
+        test::sys::mem::block::Pointer<BlockType> added;
+
+        auto res = m_record.Register(&added, 
+        [&](int status) -> void{
+            std::free(p);
+            p = NULL;
+        }, p, s, m_countId, f, l);
+
+        if (res != RecordType::sOk)
+        {
+            return {};
+        }
+        
+        added->SetDeallocator(Deallocator);
+        
+        ++m_countId;
+
+        return {&*added};
+    }
+}
+
+inline typename Memory::PointerType Memory::Allocate(const std::size_t& s)
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, "Allocate(s=%zu", s);
+    
+    return Allocate(s, __FILE__, __LINE__);
+}
+
+inline bool Memory::Reallocate(void* p, const std::size_t& s)
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Reallocate(p=%p, s=%zu)", p, s);
+        
+    std::lock_guard<std::mutex> guard{m_lock};
+
+    auto* block = m_record[p];
+    if (block == nullptr)
+    {
+        return false;
+    }
+    bool need_repos = false;
+    bool res = block->Reallocate([&](void*& p)->std::size_t{
+        void* alloc = std::realloc(p, s);
+        if (alloc == NULL)
+        {
+            SystemType::GetInstance().Error(
+                Definition::Status::sMemBlockReallocationFailed,
+                "Memory realocation failed {pointer=%p, size=%zu}", p, s);
+            return 0;
+        }
+        if (alloc != p)
+        {
+            need_repos = true;
+            p = alloc;
+        }
+        return s;
+    });
+    if (need_repos)
+    {
+        m_record.Reposition(p);
+    }
+    return res;
+}
+
+inline bool Memory::Reallocate(PointerType p, const std::size_t& s)
+{
+    TEST_SYS_DEBUG_V(SystemType, DebugType, 2, this, 
+        "Reallocate(p=%s, s=%zu)", 
+            TEST_SYS_DEBUG_TARGS_VALUE_STR(p), s);
+
+    std::lock_guard<std::mutex> guard{m_lock};
+
+    auto* block = m_record[p];
+    if (block == nullptr)
+    {
+        return false;
+    }
+    bool need_repos = false;
+    bool res = block->Reallocate([&](void*& p)->std::size_t{
+        void* alloc = std::realloc(p, s);
+        if (alloc == NULL)
+        {
+            SystemType::GetInstance().Error(
+                Definition::Status::sMemBlockReallocationFailed,
+                "Memory realocation failed {pointer=%p, size=%zu}", p, s);
+            return 0;
+        }
+        if (alloc != p)
+        {
+            need_repos = true;
+            p = alloc;
+        }
+        return s;
+    });
+    if (need_repos)
+    {
+        m_record.Reposition(p);
+    }
+    return res;
+}
+
+inline bool Memory::Free(void* p, bool force)
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Free(p=%p, force=%s)", p, (force?"true":"false"));
+
+    std::lock_guard<std::mutex> guard{m_lock};
+
+    test::sys::mem::block::Pointer<BlockType> removed;
+    auto res = m_record.Unregister(std::move(p), &removed, force);
+    
+    if (res == RecordType::sOk)
+    {
+        TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+            "Remove pointer(id=%zu, p=%p, size=%zu)", removed->ID(),
+                removed->Pointer(), removed->Size());
+        
+        removed->Release();
+    }
+    return res;
+}
+
+inline bool Memory::Free(PointerType& p, bool force)
+{
+    TEST_SYS_DEBUG_V(SystemType, DebugType, 2, this, 
+        "Free(p=%s, force=%s)", 
+            TEST_SYS_DEBUG_TARGS_VALUE_STR(p), (force?"true":"false"));
+
+    std::lock_guard<std::mutex> guard{m_lock};
+
+    test::sys::mem::block::Pointer<BlockType> removed;
+    auto res = m_record.Unregister(std::move(p), &removed, force);
+    
+    if (res == RecordType::sOk)
+    {
+        TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+            "Remove pointer(id=%zu, p=%p, size=%zu)", removed->ID(),
+                removed->Pointer(), removed->Size());
+        
+        removed->Release();
+    }
+    return res;
+}
+
+inline std::size_t Memory::OverheadSize() const
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, "OverheadSize() const");
+
+    Memory* cast = const_cast<Memory*>(this);
+    std::lock_guard<std::mutex> guard{cast->m_lock};
+
+    return cast->m_record.AllocationSize(); 
+}
+
+inline std::size_t Memory::Size() const
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, "Size() const");
+    
+    Memory* cast = const_cast<Memory*>(this);
+    std::lock_guard<std::mutex> guard{cast->m_lock};
+
+    std::size_t sum = 0;
+
+    cast->m_record.ForEach([&](BlockType* block){
+        std::size_t old_sum = sum;
+        sum += block->Size();
+        if (sum < old_sum)
+        {
+            sum = (std::size_t) -1;
+        }
+    });
+
+    return sum;
+}
+
+#include "mem/Pointer.h"
+#include "mem/ptr/Const.h"
+
+
+} //!sys
+
+} //!test
+
+#endif //!TEST_SYS_MEMORY_H_IMPL_
