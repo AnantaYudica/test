57aaf947a9fe040e35f1b228d53edb364dc54fb1
Ananta Yudica <nta040689@gmail.com>
2022-12-17 02:00:19 +0700

    Added debug and updated constructor

--- include/test/arg/Flag.h
+++ include/test/arg/Flag.h
@@ -1,8 +1,17 @@
 #ifndef TEST_ARG_FLAG_H_
 #define TEST_ARG_FLAG_H_
 
+#include "../System.h"
+
 #include <cstddef>
 #include <cstdint>
+
+namespace test::arg
+{
+class Flag;
+}
+
+TEST_SYS_DBG_TYPE_DEFINE("test::arg::Flag", test::arg::Flag);
 
 namespace test
 {
@@ -11,10 +20,13 @@ namespace arg
 
 class Flag
 {
+private:
+    typedef test::sys::Interface SystemType;
+    typedef test::sys::dbg::Type<test::arg::Flag> DebugType;
 public:
     typedef std::uint8_t IntegerValueType;
 public:
-    enum : IntegerValueType
+    enum ValueEnum : IntegerValueType
     {
         forward_end = 1,
         backward_end = 2
@@ -23,7 +35,7 @@ private:
     IntegerValueType m_value;
 public:
     inline Flag();
-    inline Flag(const IntegerValueType& flag);
+    inline Flag(const ValueEnum& flag);
     inline Flag(const std::size_t& size);
 public:
     inline ~Flag();
@@ -54,37 +66,64 @@ public:
 
 inline Flag::Flag() :
     m_value(forward_end | backward_end)
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Default Constructor");
 
-inline Flag::Flag(const IntegerValueType& flag) :
+}
+
+inline Flag::Flag(const ValueEnum& flag) :
     m_value(flag)
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Constructor(flag=%hhu)", flag);
+
+}
 
 inline Flag::Flag(const std::size_t& n) :
     m_value(n != 0 ? 0 : (forward_end | backward_end))
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Constructor(n=%zu)", n);
+    
+}
 
 inline Flag::~Flag()
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Destructor");
+    
     m_value = 0;
 }
 
 inline Flag::Flag(const Flag& cpy) :
     m_value(cpy.m_value)
-{}
+{    
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Copy Constructor(cpy=%p)", &cpy);
+
+}
 
 inline Flag::Flag(Flag&& mov) :
     m_value(mov.m_value)
-{}
+{    
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Move Constructor(mov=%p)", &mov);
+
+}
 
 inline Flag& Flag::operator=(const Flag& cpy)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Copy Assignment(cpy=%p)", &cpy);
+    
     m_value = cpy.m_value;
     return *this;
 }
 
 inline Flag& Flag::operator=(Flag&& mov)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Move Assignment(mov=%p)", &mov);
+    
     m_value = mov.m_value;
     return *this;
 }
@@ -92,6 +131,9 @@ inline Flag& Flag::operator=(Flag&& mov)
 inline std::size_t Flag::Forward(const std::size_t& n, const std::size_t& pos,
     const std::size_t end)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Forward(n=%zu, pos=%zu, end=%zu)", n, pos, end);
+
     if (n == 0) return pos;
     if (m_value & (forward_end | backward_end)) return end;
     else if (IsBackwardEnd()) 
@@ -111,6 +153,9 @@ inline std::size_t Flag::Forward(const std::size_t& n, const std::size_t& pos,
 inline std::size_t Flag::Backward(const std::size_t& n, const std::size_t& pos,
     const std::size_t end)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Backward(n=%zu, pos=%zu, end=%zu)", n, pos, end);
+
     if (n == 0) return pos;
     if (m_value & (forward_end | backward_end)) return end;
     else if (IsForwardEnd())
@@ -128,6 +173,9 @@ inline std::size_t Flag::Backward(const std::size_t& n, const std::size_t& pos,
 inline std::size_t Flag::Position(const std::size_t& pos, 
     const std::size_t end) const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Position(pos=%zu, end=%zu)", pos, end);
+
     if (m_value & (forward_end | backward_end))
         return end;
     return pos;
@@ -135,31 +183,45 @@ inline std::size_t Flag::Position(const std::size_t& pos,
 
 inline bool Flag::IsForwardEnd() const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, "IsForwardEnd() const");
+
     return m_value & forward_end;
 }
 
 inline bool Flag::IsBackwardEnd() const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, "IsBackwardEnd() const");
+
     return m_value & backward_end;
 }
 
 inline bool Flag::IsEnd() const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, "IsEnd() const");
+
     return m_value & (forward_end | backward_end);
 }
 
 inline void Flag::Reset()
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, "Reset()");
+
     m_value = 0;
 }
 
 inline bool Flag::operator==(const Flag& other) const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this,
+        "operator==(other=%p)", &other);
+
     return IsEnd() == other.IsEnd();
 }
 
 inline bool Flag::operator!=(const Flag& other) const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this,
+        "operator!=(other=%p)", &other);
+    
     return IsEnd() != other.IsEnd();
 }
 
