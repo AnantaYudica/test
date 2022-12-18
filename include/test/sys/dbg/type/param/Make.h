bbd45db9329f18974d5a539d84254bd5eb4eb5f1
Ananta Yudica <nta040689@gmail.com>
2022-12-18 11:44:31 +0700

    Added definition

--- include/test/sys/dbg/type/param/Make.h
+++ include/test/sys/dbg/type/param/Make.h
@@ -2,6 +2,7 @@
 #define TEST_SYS_DBG_TYPE_MAKE_H_
 
 #include "../../Type.defn.h"
+#include "../Value.h"
 #include "../Parameter.h"
 
 namespace test
@@ -20,6 +21,20 @@ struct Make
 {
     typedef test::sys::dbg::type::Parameter<
         test::sys::dbg::Type<TArgs>...> Type;
+};
+
+template<typename TArg, typename... TArgs>
+struct Make<test::sys::dbg::Type<TArg>, TArgs...>
+{
+    typedef test::sys::dbg::type::Parameter<
+        test::sys::dbg::Type<TArg>, TArgs...> Type;
+};
+
+template<typename TArg, TArg VArg, typename... TArgs>
+struct Make<test::sys::dbg::type::Value<TArg, VArg>, TArgs...>
+{
+    typedef test::sys::dbg::type::Parameter<
+        test::sys::dbg::type::Value<TArg, VArg>, TArgs...> Type;
 };
 
 } //!param
