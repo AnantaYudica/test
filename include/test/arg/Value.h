f8f9a8371338e9fe0c818bee0f62afff19c5aa61
Ananta Yudica <nta040689@gmail.com>
2022-12-20 05:43:53 +0700

    Added debug

--- include/test/arg/Value.h
+++ include/test/arg/Value.h
@@ -1,10 +1,25 @@
 #ifndef TEST_ARG_VALUE_H_
 #define TEST_ARG_VALUE_H_
 
+#include "../System.h"
 #include "../Pointer.h"
 
 #include <cstddef>
 #include <cstdint>
+
+namespace test::arg
+{
+class Value;
+}
+
+#ifndef TEST_ARG_VALUE_DLEVEL
+
+#define TEST_ARG_VALUE_DLEVEL 2
+
+#endif //!TEST_ARG_VALUE_DLEVEL
+
+TEST_SYS_DBG_TYPE_LEVEL_DEFINE(TEST_ARG_VALUE_DLEVEL, 
+    "test::arg::Value", test::arg::Value);
 
 namespace test
 {
@@ -13,6 +28,9 @@ namespace arg
 
 class Value
 {
+private:
+    typedef test::sys::Interface SystemType;
+    typedef test::sys::dbg::Type<test::arg::Value> DebugType;
 private:
     test::Pointer<std::uint8_t> m_values;
 public:
@@ -41,32 +59,56 @@ public:
 
 inline Value::Value() :
     m_values()
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Default Constructor");
+
+}
 
 inline Value::Value(const std::size_t& alloc_size) :
     m_values(test::ptr::arg::Array(alloc_size + 1), 
         test::ptr::arg::Foreach{}, 0)
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this,
+        "Constructor(alloc_size=%zu)", alloc_size);
+
+}
 
 inline Value::~Value()
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, "Destructor");
+
+}
 
 inline Value::Value(const Value& cpy) :
     m_values(cpy.m_values)
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Copy Constructor(cpy=%p)", &cpy);
+
+}
 
 inline Value::Value(Value&& mov) :
     m_values(mov.m_values)
-{}
+{
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Move Constructor(mov=%p)", &mov);
+
+}
 
 inline Value& Value::operator=(const Value& cpy)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Copy Assignment(cpy=%p)", &cpy);
+
     m_values = cpy.m_values;
     return *this;
 }
 
 inline Value& Value::operator=(Value&& mov)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 1, this, 
+        "Move Assignment(mov=%p)", &mov);
+
     m_values = mov.m_values;
     return *this;
 }
@@ -74,6 +116,11 @@ inline Value& Value::operator=(Value&& mov)
 template<typename T>
 inline void Value::Set(const std::size_t& off, const T& val)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 2, this, 
+        "Set<%s>(off=%zu, val=%s)", 
+        TEST_SYS_DEBUG_TARGS_NAME_STR(T), off,
+        TEST_SYS_DEBUG_TARGS_VALUE_STR(val));
+
     m_values.SetIndex(off);
     auto cast = m_values.template ReinterpretCast<T>();
     *cast = val;
@@ -82,6 +129,9 @@ inline void Value::Set(const std::size_t& off, const T& val)
 template<typename T>
 inline T* Value::Get(const std::size_t& off)
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+        "Set<%s>(off=%zu)", TEST_SYS_DEBUG_TARGS_NAME_STR(T), off);
+
     m_values.SetIndex(off);
     auto cast = m_values.template ReinterpretCast<T>();
     return &*cast;
@@ -89,16 +139,25 @@ inline T* Value::Get(const std::size_t& off)
 
 inline std::size_t Value::AllocationSize() const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+        "AllocationSize() const");
+
     return m_values.AllocationSize() - 1;
 }
 
 inline bool Value::operator==(const Value& other) const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+        "operator==(other=%p) const", &other);
+
     return m_values == other.m_values.GetData();
 }
 
 inline bool Value::operator!=(const Value& other) const
 {
+    TEST_SYS_DEBUG(SystemType, DebugType, 3, this, 
+        "operator!=(other=%p) const", &other);
+
     return m_values != other.m_values.GetData();
 }
 
