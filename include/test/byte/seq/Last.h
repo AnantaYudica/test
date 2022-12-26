5f917613bff9ecc995b0e2b7cf6462fc9efbb851
Ananta Yudica <nta040689@gmail.com>
2022-12-27 03:31:36 +0700

    Added functions test::byte::seq::Last

---
+++ include/test/byte/seq/Last.h
@@ -0,0 +1,133 @@
+#ifndef TEST_BYTE_SEQ_LAST_H_
+#define TEST_BYTE_SEQ_LAST_H_
+
+#include "../../System.h"
+#include "../Sequence.h"
+#include "../Iterator.h"
+
+#ifndef TEST_BYTE_SEQ_NAMESPACE_DLEVEL
+
+#define TEST_BYTE_SEQ_NAMESPACE_DLEVEL 2
+
+#endif //!TEST_BYTE_SEQ_NAMESPACE_DLEVEL
+
+#ifndef TEST_BYTE_SEQ_NAMESPACE_DEBUG
+#define TEST_BYTE_SEQ_NAMESPACE_DEBUG
+TEST_SYS_DBG_NAMESPACE_LEVEL_DEFINE(TEST_BYTE_SEQ_NAMESPACE_DLEVEL, 
+    "test::byte::seq");
+#endif //!TEST_BYTE_SEQ_NAMESPACE_DEBUG
+
+namespace test
+{
+namespace byte
+{
+namespace seq
+{
+
+template<typename T>
+inline test::byte::Sequence<test::byte::seq::Flag::last, T> Last(T&& rval)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last<%s>(rval=%p)",
+        TEST_SYS_DEBUG_TARGS_NAME_STR(T), &rval);
+
+    return {std::move(rval)};
+}
+
+template<typename T>
+inline test::byte::Sequence<test::byte::seq::Flag::last, T> Last(T& lval)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last<%s>(lval=%p)",
+        TEST_SYS_DEBUG_TARGS_NAME_STR(T), &lval);
+
+    return {std::forward<T>(lval)};
+}
+
+template<typename T>
+inline test::byte::Sequence<test::byte::seq::Flag::last, T>
+    Last(T&& rval, const std::size_t& off)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last<%s>(rval=%p, off=%zu)",
+        TEST_SYS_DEBUG_TARGS_NAME_STR(T), &rval, off);
+
+    return {std::move(rval), off};
+}
+
+template<typename T>
+inline test::byte::Sequence<test::byte::seq::Flag::last, T>
+    Last(T& lval, const std::size_t& off)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last<%s>(lval=%p, off=%zu)",
+        TEST_SYS_DEBUG_TARGS_NAME_STR(T), &lval, off);
+    
+    return {std::forward<T>(lval), off};
+}
+
+inline test::byte::Sequence<test::byte::seq::Flag::last, 
+    test::byte::Iterator<const std::uint8_t*>>
+Last(test::byte::Iterator<const std::uint8_t*>&& begin,
+    test::byte::Iterator<const std::uint8_t*>&& end)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last(begin=%p, end=%p)", &begin, &end);
+    
+    return {std::move(begin), std::move(end)};
+}
+
+inline test::byte::Sequence<test::byte::seq::Flag::last, 
+    test::byte::Iterator<const std::uint8_t*>>
+Last(test::byte::Iterator<const std::uint8_t*>& begin,
+    test::byte::Iterator<const std::uint8_t*>& end)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last(begin=%p, end=%p)", &begin, &end);
+    
+    typedef test::byte::Iterator<const std::uint8_t*> ConstIteratorType;
+    return {std::forward<ConstIteratorType>(begin), 
+        std::forward<ConstIteratorType>(end)};
+}
+
+inline test::byte::Sequence<test::byte::seq::Flag::last, 
+    test::byte::Iterator<const std::uint8_t*>>
+Last(test::byte::Iterator<const std::uint8_t*>&& begin,
+    test::byte::Iterator<const std::uint8_t*>&& end,
+    const std::size_t& off)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last(begin=%p, end=%p, off=%zu)", &begin, &end, off);
+    
+    return {std::move(begin), std::move(end), off};
+}
+
+inline test::byte::Sequence<test::byte::seq::Flag::last, 
+    test::byte::Iterator<const std::uint8_t*>>
+Last(test::byte::Iterator<const std::uint8_t*>& begin,
+    test::byte::Iterator<const std::uint8_t*>& end,
+    const std::size_t& off)
+{
+    TEST_SYS_DEBUG(test::sys::Interface, 
+        TEST_SYS_DEBUG_NAMESPACE_TYPE("test::byte::seq"), 
+            2, NULL, "Last(begin=%p, end=%p, off=%zu)", &begin, &end, off);
+    
+    typedef test::byte::Iterator<const std::uint8_t*> ConstIteratorType;
+    return {std::forward<ConstIteratorType>(begin), 
+        std::forward<ConstIteratorType>(end), off};
+}
+
+} //!seq
+
+} //!byte
+
+} //!test
+
+#endif //!TEST_BYTE_SEQ_LAST_H_
