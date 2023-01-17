84b804cdf0579c32768be4999378a2fcc359cd80
Ananta Yudica <nta040689@gmail.com>
2023-01-17 22:44:26 +0700

    Fixed default value and functions

--- include/test/sys/dbg/Type.defn.h
+++ include/test/sys/dbg/Type.defn.h
@@ -16,6 +16,8 @@ class Type : public test::sys::Debug
 public:
     typedef typename test::sys::Debug::CStrType CStrType;
 public:
+    static constexpr bool HasSuffix = false;
+public:
     inline static test::sys::Debug& GetInstance()
     {
         static Type<T> instance;
@@ -25,6 +27,21 @@ protected:
     inline Type() = default;
 public:
     inline virtual ~Type() = default;
+public:
+    static inline std::size_t WritePrefixName(char * name_out, std::size_t n)
+    {
+        return test::sys::Debug::WriteName(name_out, n);
+    }
+public:
+    static inline std::size_t WriteSufixName(char * name_out, std::size_t n)
+    {
+        return 0;
+    }
+public:
+    static inline std::size_t WriteInfixName(char * name_out, std::size_t n)
+    {
+        return 0;
+    }
 };
 
 } //!dbg
