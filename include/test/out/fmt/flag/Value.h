93e8d7b75edbb13f1dc23f01de6589791822a4a1
Ananta Yudica <nta040689@gmail.com>
2023-01-30 16:51:55 +0700

    Added class test::out::fmt::flag::Value

---
+++ include/test/out/fmt/flag/Value.h
@@ -0,0 +1,78 @@
+#ifndef TEST_OUT_FMT_FLAG_VALUE_H_
+#define TEST_OUT_FMT_FLAG_VALUE_H_
+
+#include "../../../System.h"
+
+namespace test
+{
+namespace out
+{
+namespace fmt
+{
+namespace flag
+{
+
+template<typename T>
+class Value
+{
+private:
+    bool m_default;
+    T m_value;
+public:
+    constexpr Value();
+    constexpr Value(const T& val);
+public:
+    constexpr bool IsDefault() const;
+    constexpr T GetValue() const;
+};
+
+template<typename T>
+constexpr Value<T>::Value() :
+    m_default(true),
+    m_value()
+{}
+
+template<typename T>
+constexpr Value<T>::Value(const T& val) :
+    m_default(false),
+    m_value(val)
+{}
+
+template<typename T>
+constexpr bool Value<T>::IsDefault() const
+{
+    return m_default;
+}
+
+template<typename T>
+constexpr T Value<T>::GetValue() const
+{
+    return m_value;
+}
+
+} //!flag
+
+} //!fmt
+
+} //!out
+
+} //!test
+
+#ifndef TEST_OUT_FMT_FLAG_VALUE_DLEVEL
+
+#define TEST_OUT_FMT_FLAG_VALUE_DLEVEL 2
+
+#endif //!TEST_OUT_FMT_FLAG_VALUE_DLEVEL
+
+#define TEST_SYS_DBG_TYPE_PARAMETER_DEFINE_ARGS\
+    test::sys::dbg::Type<T>,
+
+template<typename T>
+TEST_SYS_DBG_TYPE_PARAMETER_LEVEL_DEFINE(
+    TEST_OUT_FMT_FLAG_VALUE_DLEVEL, 
+    "test::out::fmt::flag::Value", 
+    test::out::fmt::flag::Value<T>);
+
+#undef TEST_SYS_DBG_TYPE_PARAMETER_DEFINE_ARGS
+
+#endif //!TEST_OUT_FMT_FLAG_VALUE_H_
