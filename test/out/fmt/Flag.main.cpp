aeaef6f6cc2e5df31ba623ed08f1891c4f21dcab
Ananta Yudica <nta040689@gmail.com>
2023-01-31 11:21:44 +0700

    Updated test for class test::out::fmt::Flag

--- test/out/fmt/Flag.main.cpp
+++ test/out/fmt/Flag.main.cpp
@@ -31,6 +31,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.undefined);
 
@@ -69,6 +70,11 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+        
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
     }
     {
         test::out::fmt::Flag<std::atomic<std::uint32_t>> fl1;
@@ -94,6 +100,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.undefined);
 
@@ -132,6 +139,11 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
     }
     {
         const auto specifier1 = test::out::fmt::Flag<>::specifier_ch;
@@ -157,6 +169,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.specifier_ch);
 
@@ -195,6 +208,11 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
     }
     {
         const auto specifier1 = 
@@ -221,6 +239,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.specifier_ch);
 
@@ -259,6 +278,11 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+        
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
     }
     {
         const auto specifier1 = test::out::fmt::Flag<>::specifier_str;
@@ -284,6 +308,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.specifier_str);
 
@@ -322,6 +347,11 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
     }
     {
         const auto specifier1 = 
@@ -349,6 +379,7 @@ int main()
         assert(fl1.IsSpecifierFloatingPoint() == false);
         assert(fl1.IsSpecifierNumberCharacter() == false);
         assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
         
         assert(fl1.GetSpecifierType() == fl1.specifier_str);
 
@@ -387,6 +418,86 @@ int main()
         assert(fl1.IsSpecifierUpperCase() == false);
 
         assert(fl1.GetSpecifierCase() == fl1.undefined);
+
+        assert(fl1.HasInputValue() == false);
+        assert(fl1.HasInputWidth() == false);
+        assert(fl1.HasInputLength() == false);
+        assert(fl1.HasInputPrecision() == false);
+    }
+    {
+        test::out::fmt::Flag<> fl1{test::out::fmt::Flag<>::specifier_str, 
+            test::out::fmt::flag::Define<char>{},
+            test::out::fmt::flag::Value<char*>{nullptr},
+            test::out::fmt::flag::Width{1},
+            test::out::fmt::flag::Length{1}};
+            
+        assert(fl1.IsGood() == true);
+        assert(fl1.IsBad() == false);
+
+        assert(fl1.IsDefine() == true);
+        assert(fl1.IsDefineSigned() == false);
+        assert(fl1.IsDefineUnsigned() == false);
+
+        assert(fl1.GetDefine() == fl1.define_char);
+        assert(fl1.GetDefineSigned() == fl1.undefined);
+        assert(fl1.GetDefineType() == fl1.define_char);
+
+        assert(fl1.GetSpecifier() == (fl1.specifier_str | 
+            fl1.specifier_sub_width | fl1.specifier_sub_length));
+
+        assert(fl1.IsSpecifierUndefined() == false);
+        assert(fl1.IsSpecifierCharacter() == false);
+        assert(fl1.IsSpecifierString() == true);
+        assert(fl1.IsSpecifierPointer() == false);
+        assert(fl1.IsSpecifierInteger() == false);
+        assert(fl1.IsSpecifierFloatingPoint() == false);
+        assert(fl1.IsSpecifierNumberCharacter() == false);
+        assert(fl1.IsSpecifierBlank() == false);
+        assert(fl1.IsSpecifierBoolean() == false);
+        
+        assert(fl1.GetSpecifierType() == fl1.specifier_str);
+
+        assert(fl1.IsSpecifierFlagUndefined() == true);
+        assert(fl1.IsSpecifierFlagPrefix() == false);
+        assert(fl1.IsSpecifierFlagPrefixPlus() == false);
+        assert(fl1.IsSpecifierFlagPrefixSpace() == false);
+        assert(fl1.IsSpecifierFlagPrefixZero() == false);
+
+        assert(fl1.GetSpecifierFlag() == fl1.undefined);
+
+        assert(fl1.IsSpecifierSubWidth() == true);
+        assert(fl1.IsSpecifierSubPrecision() == true);
+        assert(fl1.IsSpecifierSubLength() == true);
+
+        assert(fl1.GetSpecifierSub() == 
+            (fl1.specifier_sub_width | fl1.specifier_sub_length));
+        
+        assert(fl1.IsSpecifierIntegerSigned() == false);
+        assert(fl1.IsSpecifierIntegerUnsigned() == false);
+
+        assert(fl1.GetSpecifierInteger() == fl1.undefined);
+
+        assert(fl1.IsSpecifierFloatingPointFixed() == false);
+        assert(fl1.IsSpecifierFloatingPointExponent() == false);
+        assert(fl1.IsSpecifierFloatingPointShort() == false);
+        
+        assert(fl1.GetSpecifierFloatingPoint() == fl1.undefined);
+
+        assert(fl1.IsSpecifierBaseDecimal() == false);
+        assert(fl1.IsSpecifierBaseOctal() == false);
+        assert(fl1.IsSpecifierBaseHexadecimal() == false);
+
+        assert(fl1.GetSpecifierFloatingPoint() == fl1.undefined);
+
+        assert(fl1.IsSpecifierLowerCase() == false);
+        assert(fl1.IsSpecifierUpperCase() == false);
+
+        assert(fl1.GetSpecifierCase() == fl1.undefined);
+
+        assert(fl1.HasInputValue() == true);
+        assert(fl1.HasInputWidth() == true);
+        assert(fl1.HasInputLength() == true);
+        assert(fl1.HasInputPrecision() == true);
     }
     return 0;
 }
