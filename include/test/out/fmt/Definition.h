58c2af3387a0f6db25a2b79c3f0767782731ad2b
Ananta Yudica <nta040689@gmail.com>
2023-02-08 03:24:34 +0700

    Updated class test::out::fmt::Definition

--- include/test/out/fmt/Definition.h
+++ include/test/out/fmt/Definition.h
@@ -28,6 +28,8 @@ public:
         void* value, std::size_t value_size, FlagType flag, 
         int width, int len_pred);
 public:
+    typedef std::uint8_t OutputIntegerType;
+public:
     static constexpr std::size_t string_max_len = 1024;
 public:
     static constexpr int value_character = 1;
@@ -39,8 +41,32 @@ public:
     static constexpr int value_nothing = 7;
     static constexpr int value_object = 8;
 public:
-
+    static constexpr OutputIntegerType output_undefined = 0;
+    static constexpr OutputIntegerType output_char = 1;
+    static constexpr OutputIntegerType output_wchar = 2;
+public:
+    static constexpr inline OutputIntegerType OutputValue(...);
+    static constexpr inline OutputIntegerType OutputValue(char&&);
+    static constexpr inline OutputIntegerType OutputValue(wchar_t&&);
 };
+
+constexpr inline typename Definition::OutputIntegerType 
+Definition::OutputValue(...)
+{
+    return output_undefined;
+}
+
+constexpr inline typename Definition::OutputIntegerType 
+Definition::OutputValue(char&&)
+{
+    return output_char;
+}
+
+constexpr inline typename Definition::OutputIntegerType 
+Definition::OutputValue(wchar_t&&)
+{
+    return output_wchar;
+}
 
 } //!fmt
 
