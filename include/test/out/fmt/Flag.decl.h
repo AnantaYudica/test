8e88ae82f6ad10c6c8c65de5efd1cb387e989077
Ananta Yudica <nta040689@gmail.com>
2023-02-10 04:48:12 +0700

    Added declaration class test::out::fmt::Flag

---
+++ include/test/out/fmt/Flag.decl.h
@@ -0,0 +1,24 @@
+#ifndef TEST_OUT_FMT_FLAG_H_DECL_
+#define TEST_OUT_FMT_FLAG_H_DECL_
+
+#include <cstdint>
+
+namespace test
+{
+namespace out
+{
+namespace fmt
+{
+
+template<typename TValue = std::uint32_t, 
+    typename TIntegerValue = std::uint32_t>
+class Flag;
+
+
+} //!fmt
+
+} //!out
+
+} //!test
+
+#endif //!TEST_OUT_FMT_FLAG_H_DECL_
