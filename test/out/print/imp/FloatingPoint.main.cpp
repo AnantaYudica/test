3bdf6c5dd3b717832db078b1349da51844c40e66
Ananta Yudica <nta040689@gmail.com>
2023-02-20 03:18:22 +0700

    Added main test for class test::out::ptrint::imp::FloatingPoint

---
+++ test/out/print/imp/FloatingPoint.main.cpp
@@ -0,0 +1,19 @@
+#ifdef DEBUG
+#define TEST_SYS_DEBUG_ENABLE 1
+#endif //!DEBUG
+
+#include "test/out/print/imp/FloatingPoint.h"
+#include "test/out/CString.h"
+
+#include <cassert>
+#include <cstring>
+
+typedef typename test::out::Definition::FlagType FlagType;
+
+int main()
+{
+
+    //todo
+    
+    return 0;
+}
