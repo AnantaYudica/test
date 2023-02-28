a5372867c0a719593facaba1e58cc880c0af5ff6
Ananta Yudica <nta040689@gmail.com>
2023-02-28 11:40:39 +0700

    Added test for class test::out::print::sub::Length

---
+++ test/out/print/sub/Length.main.cpp
@@ -0,0 +1,18 @@
+#ifdef DEBUG
+#define TEST_SYS_DEBUG_ENABLE 1
+#endif //!DEBUG
+
+#include "test/out/print/sub/Length.h"
+
+#include <cassert>
+
+int main()
+{
+    {
+        assert((test::out::print::sub::Length::len_fmt &
+            test::out::print::sub::Length::MaskValue) ==
+            test::out::print::sub::Length::len_fmt);
+            
+    }
+    return 0;
+}
