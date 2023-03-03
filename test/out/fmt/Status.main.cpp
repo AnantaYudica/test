b609eb756cedc7034ac31c21f7ddcefe4aa36a85
Ananta Yudica <nta040689@gmail.com>
2023-03-03 22:42:11 +0700

    Updated tests for class test::out::fmt::Status

--- test/out/fmt/Status.main.cpp
+++ test/out/fmt/Status.main.cpp
@@ -12,100 +12,100 @@
 int main()
 {
     {
-        test::out::fmt::Status<> s1;
+        test::out::fmt::Status s1;
 
-        assert(s1.IsBad() == false);
-        assert(s1.IsGood() == true);
-        assert(s1.GetBadCode() == s1.good);
+        assert(s1.IsBad() == true);
+        assert(s1.IsGood() == false);
+        assert(s1.GetBadCode() == s1.value_not_set);
 
-        assert(s1.Bad(s1.flag_undefined) == true);
+        assert(s1.Bad(s1.load_failed) == true);
         
         assert(s1.IsBad() == true);
         assert(s1.IsGood() == false);
-        assert(s1.GetBadCode() == s1.flag_undefined);
+        assert(s1.GetBadCode() == s1.load_failed);
 
         
         assert(s1.Bad(s1.load_failed) == false);
         
         assert(s1.IsBad() == true);
         assert(s1.IsGood() == false);
-        assert(s1.GetBadCode() == s1.flag_undefined);
+        assert(s1.GetBadCode() == s1.load_failed);
 
         s1.Reset();
+        
+        assert(s1.IsBad() == true);
+        assert(s1.IsGood() == false);
+        assert(s1.GetBadCode() == s1.value_not_set);
+        
+        assert(s1.Bad(s1.good) == true);
         
         assert(s1.IsBad() == false);
         assert(s1.IsGood() == true);
         assert(s1.GetBadCode() == s1.good);
-        
+    }
+    {
+        test::out::fmt::Status s1;
+
         assert(s1.Bad(s1.load_failed) == true);
+
+        assert(s1.IsBad() == true);
+        assert(s1.IsGood() == false);
+        assert(s1.GetBadCode() == s1.load_failed);
+        
+        test::out::fmt::Status s2{s1};
         
         assert(s1.IsBad() == true);
         assert(s1.IsGood() == false);
         assert(s1.GetBadCode() == s1.load_failed);
-    }
-    {
-        test::out::fmt::Status<> s1;
-
-        assert(s1.Bad(s1.value_not_set) == true);
-
-        assert(s1.IsBad() == true);
-        assert(s1.IsGood() == false);
-        assert(s1.GetBadCode() == s1.value_not_set);
-        
-        test::out::fmt::Status<> s2{s1};
-        
-        assert(s1.IsBad() == true);
-        assert(s1.IsGood() == false);
-        assert(s1.GetBadCode() == s1.value_not_set);
         
         assert(s2.IsBad() == true);
         assert(s2.IsGood() == false);
-        assert(s2.GetBadCode() == s1.value_not_set);
+        assert(s2.GetBadCode() == s1.load_failed);
         
-        test::out::fmt::Status<> s3;
+        test::out::fmt::Status s3;
 
         s3 = s1;
         
         assert(s1.IsBad() == true);
         assert(s1.IsGood() == false);
-        assert(s1.GetBadCode() == s1.value_not_set);
+        assert(s1.GetBadCode() == s1.load_failed);
         
         assert(s3.IsBad() == true);
         assert(s3.IsGood() == false);
-        assert(s3.GetBadCode() == s1.value_not_set);
+        assert(s3.GetBadCode() == s1.load_failed);
         
     }
     
     {
-        test::out::fmt::Status<> s1;
+        test::out::fmt::Status s1;
 
-        assert(s1.Bad(s1.value_not_set) == true);
+        assert(s1.Bad(s1.load_failed) == true);
 
+        assert(s1.IsBad() == true);
+        assert(s1.IsGood() == false);
+        assert(s1.GetBadCode() == s1.load_failed);
+        
+        test::out::fmt::Status s2{std::move(s1)};
+        
         assert(s1.IsBad() == true);
         assert(s1.IsGood() == false);
         assert(s1.GetBadCode() == s1.value_not_set);
         
-        test::out::fmt::Status<> s2{std::move(s1)};
+        assert(s2.IsBad() == true);
+        assert(s2.IsGood() == false);
+        assert(s2.GetBadCode() == s1.load_failed);
         
-        assert(s1.IsBad() == false);
-        assert(s1.IsGood() == true);
-        assert(s1.GetBadCode() == s1.good);
+        test::out::fmt::Status s3;
+
+        s3 = std::move(s2);
         
         assert(s2.IsBad() == true);
         assert(s2.IsGood() == false);
         assert(s2.GetBadCode() == s1.value_not_set);
         
-        test::out::fmt::Status<> s3;
-
-        s3 = std::move(s2);
-        
-        assert(s2.IsBad() == false);
-        assert(s2.IsGood() == true);
-        assert(s2.GetBadCode() == s1.good);
-        
         assert(s3.IsBad() == true);
         assert(s3.IsGood() == false);
-        assert(s3.GetBadCode() == s1.value_not_set);
+        assert(s3.GetBadCode() == s1.load_failed);
         
     }
     return 0;
