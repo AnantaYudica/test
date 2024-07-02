4aa776d2d52ace3f0b4440c5265cd6bbd0e1ab72
Ananta Yudica <nta040689@gmail.com>
2024-07-03 03:18:18 +0700

    Updated class test::sys::Log

--- include/test/sys/Log.h
+++ include/test/sys/Log.h
@@ -1,6 +1,7 @@
 #ifndef TEST_SYS_LOG_H_
 #define TEST_SYS_LOG_H_
 
+#include "out/Interface.h"
 #include "Definition.h"
 #include "Interface.h"
 #include "Debug.h"
@@ -9,10 +10,6 @@
 #include <mutex>
 #include <cstdarg>
 #include <chrono>
-
-#ifndef TEST_SYS_LOG_OUTPUT_BUFFER
-#define TEST_SYS_LOG_OUTPUT_BUFFER 1024
-#endif //!TEST_SYS_LOG_OUTPUT_BUFFER
 
 namespace test
 {
@@ -49,7 +46,7 @@ namespace sys
 {
 
 template<typename TStatus>
-class Log
+class Log : public test::sys::out::Interface<TStatus>
 {
 private:
     typedef test::sys::Interface SystemType;
@@ -60,114 +57,33 @@ public:
     typedef typename test::sys::Definition::StatusIntegerType StatusIntegerType;
     typedef test::sys::Debug DebugType;
 public:
-    typedef int(*FormatDebugCallbackFunc)(char* buffer, 
-        const std::size_t& buffer_size, DebugType& dbg, std::int8_t level, 
-        const void * obj, const StatusType& status, 
-        const char* msg);
+    typedef typename test::sys::out::Interface<TStatus>::
+        FormatDebugCallbackFunc FormatDebugCallbackFunc;
 public:
-    typedef int(*FormatCallbackFunc)(char* buffer, 
-        const std::size_t& buffer_size, const StatusType& status, 
-        const char* msg);
-private:
-    static int DefaultFormat(char* buffer, 
-        const std::size_t& buffer_size, const StatusType& status, 
-        const char* msg);
-private:
-    static int DefaultDebugFormat(char* buffer, 
-        const std::size_t& buffer_size, DebugType& dbg, std::int8_t level, 
-        const void * obj, const StatusType& status, 
-        const char* msg);
+    typedef typename test::sys::out::Interface<TStatus>::
+        FormatCallbackFunc FormatCallbackFunc;
 private:
     FILE* m_file;
-    StatusType& m_status;
-    std::mutex m_mutex;
-    FormatCallbackFunc m_outFmtCb;
-    FormatCallbackFunc m_infoFmtCb;
-    FormatCallbackFunc m_errFmtCb;
-    FormatDebugCallbackFunc m_debugFmtCb;
 public:
     Log(StatusType& status);
 public:
     Log(const Log&) = delete;
     Log(Log&&) = delete;
 public:
-    ~Log();
+    virtual ~Log();
 public:
     Log& operator=(const Log& cpy) = delete;
     Log& operator=(Log&& mov) = delete;
 public:
     void SetFileOutput(FILE* fout);
-public:
-    void SetOutputFormatCallback(FormatCallbackFunc cb);
-    void SetInfoFormatCallback(FormatCallbackFunc cb);
-    void SetErrorFormatCallback(FormatCallbackFunc cb);
-    void SetDebugFormatCallback(FormatDebugCallbackFunc cb);
-public:
-    int Output(const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
-public:
-    int VOutput(const char* format, va_list args)
-        TEST_ATTRIBUTE ((__format__ (__printf__, 2, 0)));
-public:
-    int Info(const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 2, 3)));
-public:
-    int VInfo(const char* format, va_list args)
-        TEST_ATTRIBUTE ((__format__ (__printf__, 2, 0)));
-public:
-    int Debug(DebugType& dbg, std::int8_t level, const void * obj,
-        const char* format, ...)
-            TEST_ATTRIBUTE((__format__ (__printf__, 5, 6)));
-public:
-    int VDebug(DebugType& dbg, std::int8_t level, const void * obj,
-        const char* format, va_list args)
-            TEST_ATTRIBUTE ((__format__ (__printf__, 5, 0)));
-public:
-    int Error(StatusIntegerType code, const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 3, 4)));
-    int Error(StatusEnumType code, const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 3, 4)));
-public:
-    int VError(StatusIntegerType code, const char* format, va_list args)
-        TEST_ATTRIBUTE ((__format__ (__printf__, 3, 0)));
-    int VError(StatusEnumType code, const char* format, va_list args)
-        TEST_ATTRIBUTE ((__format__ (__printf__, 3, 0)));
-public:
-    int OutputCallback(FormatCallbackFunc cb, const char* format, ...)
-        TEST_ATTRIBUTE((__format__ (__printf__, 3, 4)));
-public:
-    int VOutputCallback(FormatCallbackFunc cb, const char* format, 
-        va_list args) TEST_ATTRIBUTE ((__format__ (__printf__, 3, 0)));
-    int VOutputCallback(FormatDebugCallbackFunc cb, DebugType& dbg, 
-        std::int8_t level, const void * obj, const char* format, 
-        va_list args) TEST_ATTRIBUTE ((__format__ (__printf__, 6, 0)));
+protected:
+    virtual int BufferOutput(char * buffer);
 };
 
 template<typename TStatus>
-int Log<TStatus>::DefaultFormat(char* buffer, 
-    const std::size_t& buffer_size, const StatusType&, 
-    const char* msg)
-{
-    return snprintf(buffer, buffer_size, "%s", msg);
-}
-
-template<typename TStatus>
-int Log<TStatus>::DefaultDebugFormat(char* buffer, 
-    const std::size_t& buffer_size, DebugType& dbg, std::int8_t level, 
-    const void * obj, const StatusType& status, 
-    const char* msg)
-{
-    return snprintf(buffer, buffer_size, "%s", msg);
-}
-
-template<typename TStatus>
 Log<TStatus>::Log(StatusType& status) :
-    m_file(NULL),
-    m_status(status),
-    m_mutex(),
-    m_outFmtCb(&DefaultFormat),
-    m_infoFmtCb(nullptr),
-    m_errFmtCb(nullptr)
+    test::sys::out::Interface<TStatus>(status),
+    m_file(NULL)
 {
     TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
         "Constructor(status=%p)", status);
@@ -187,7 +103,6 @@ void Log<TStatus>::SetFileOutput(FILE* fout)
     TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
         "SetFileOutput(fout=%p)", fout);
     
-    std::lock_guard<std::mutex> guard(m_mutex);
     if (fout == stdout)
     {
         m_file = NULL;
@@ -199,275 +114,17 @@ void Log<TStatus>::SetFileOutput(FILE* fout)
 }
 
 template<typename TStatus>
-void Log<TStatus>::SetOutputFormatCallback(FormatCallbackFunc cb)
+int Log<TStatus>::BufferOutput(char * buffer)
 {
-    TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
-        "SetOutputFormatCallback(cb=%p)", cb);
-    
-    std::lock_guard<std::mutex> guard(m_mutex);
-    if (cb == nullptr)
-    {
-        m_outFmtCb = &DefaultFormat;
-    }
-    else
-    {
-        m_outFmtCb = cb;
-    }
-}
-
-template<typename TStatus>
-void Log<TStatus>::SetInfoFormatCallback(FormatCallbackFunc cb)
-{
-    TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
-        "SetInfoFormatCallback(cb=%p)", cb);
-
-    std::lock_guard<std::mutex> guard(m_mutex);
-    if (cb == nullptr)
-    {
-        m_infoFmtCb = &DefaultFormat;
-    }
-    else
-    {
-        m_infoFmtCb = cb;
-    }
-}
-
-template<typename TStatus>
-void Log<TStatus>::SetErrorFormatCallback(FormatCallbackFunc cb)
-{
-    TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
-        "SetErrorFormatCallback(cb=%p)", cb);
-    
-    std::lock_guard<std::mutex> guard(m_mutex);
-    if (cb == nullptr)
-    {
-        m_errFmtCb = &DefaultFormat;
-    }
-    else
-    {
-        m_errFmtCb = cb;
-    }
-}
-
-template<typename TStatus>
-void Log<TStatus>::SetDebugFormatCallback(FormatDebugCallbackFunc cb)
-{
-    TEST_SYS_DEBUG(SystemType, _DebugType, 1, this, 
-        "SetDebugFormatCallback(cb=%p)", cb);
-    
-    std::lock_guard<std::mutex> guard(m_mutex);
-    if (cb == nullptr)
-    {
-        m_debugFmtCb = &DefaultDebugFormat;
-    }
-    else
-    {
-        m_debugFmtCb = cb;
-    }
-}
-
-template<typename TStatus>
-int Log<TStatus>::Output(const char* format, ...)
-{
-    va_list vlist;
     int res = 0;
-    va_start(vlist, format);
-    res = VOutput(format, vlist);
-    va_end(vlist);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VOutput(const char* format, va_list args)
-{
-    return VOutputCallback(m_outFmtCb, format, args);
-}
-
-
-template<typename TStatus>
-int Log<TStatus>::Info(const char* format, ...)
-{
-    va_list vlist;
-    int res = 0;
-    va_start(vlist, format);
-    res = VInfo(format, vlist);
-    va_end(vlist);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VInfo(const char* format, va_list args)
-{
-    if (m_infoFmtCb == nullptr)
-    {
-        return VOutputCallback(m_outFmtCb, format, args);
-    }
-    return VOutputCallback(m_infoFmtCb, format, args);
-}
-
-template<typename TStatus>
-int Log<TStatus>::Debug(DebugType& dbg, std::int8_t level, const void * obj,
-    const char* format, ...)
-{
-    va_list vlist;
-    int res = 0;
-    va_start(vlist, format);
-    res = VDebug(dbg, level, obj, format, vlist);
-    va_end(vlist);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VDebug(DebugType& dbg, std::int8_t level, const void * obj,
-    const char* format, va_list args)
-{
-    if (level > dbg.GetLevel())
-    {
-        return 0;
-    }
-    if (m_infoFmtCb == nullptr)
-    {
-        return VOutputCallback(m_outFmtCb, format, args);
-    }
-    return VOutputCallback(m_debugFmtCb, dbg, level, obj, format, args);
-}
-
-template<typename TStatus>
-int Log<TStatus>::Error(StatusIntegerType code, const char* format, ...)
-{
-    va_list vlist;
-    int res = 0;
-    va_start(vlist, format);
-    res = VError(code, format, vlist);
-    va_end(vlist);
-    return res;
-}
-    
-template<typename TStatus>
-int Log<TStatus>::Error(StatusEnumType code, const char* format, ...)
-{
-    va_list vlist;
-    int res = 0;
-    va_start(vlist, format);
-    res = VError(static_cast<StatusIntegerType>(code), format, vlist);
-    va_end(vlist);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VError(StatusIntegerType code, const char* format, 
-    va_list args)
-{
-    m_status.Error(code);
-    if (m_errFmtCb == nullptr)
-    {
-        return VOutputCallback(m_outFmtCb, format, args);
-    }
-    return VOutputCallback(m_errFmtCb, format, args);
-}
-
-template<typename TStatus>
-int Log<TStatus>::VError(StatusEnumType code, const char* format, 
-    va_list args)
-{
-    m_status.Error(code);
-    if (m_errFmtCb == nullptr)
-    {
-        return VOutputCallback(m_outFmtCb, format, args);
-    }
-    return VOutputCallback(m_errFmtCb, format, args);
-}
-
-template<typename TStatus>
-int Log<TStatus>::OutputCallback(FormatCallbackFunc cb, const 
-    char* format, ...)
-{
-    va_list vlist;
-    int res = 0;
-    va_start(vlist, format);
-    res = VOutputCallback(cb, format, vlist);
-    va_end(vlist);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VOutputCallback(FormatCallbackFunc cb, const char* format, 
-    va_list args)
-{
-    char * msg_buff = (char*)malloc(TEST_SYS_LOG_OUTPUT_BUFFER + 1);
-    char * buff = (char*)malloc(TEST_SYS_LOG_OUTPUT_BUFFER + 1);
-    msg_buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-    buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-
-    vsnprintf(msg_buff, 1024, format, args);
-    msg_buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-    int res = 0;
-    FormatCallbackFunc call_fn;
-    if (cb == nullptr)
-    {
-        std::lock_guard<std::mutex> guard(m_mutex);
-        call_fn = m_outFmtCb;
-    }
-    else
-    {
-        call_fn = cb;
-    }
-
-    res = call_fn(buff, TEST_SYS_LOG_OUTPUT_BUFFER, m_status, msg_buff);
-    buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
     if(m_file == NULL)
     {
-        fprintf(stdout, "%s\n", buff);
+        res = fprintf(stdout, "%s\n", buffer);
     }
     else
     {
-        fprintf(m_file, "%s\n", buff);
+        res = fprintf(m_file, "%s\n", buffer);
     }
-
-    free(msg_buff);
-    free(buff);
-    return res;
-}
-
-template<typename TStatus>
-int Log<TStatus>::VOutputCallback(FormatDebugCallbackFunc cb, DebugType& dbg, 
-    std::int8_t level, const void * obj, const char* format, 
-    va_list args)
-{
-    char * msg_buff = (char*)malloc(TEST_SYS_LOG_OUTPUT_BUFFER + 1);
-    char * buff = (char*)malloc(TEST_SYS_LOG_OUTPUT_BUFFER + 1);
-    msg_buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-    buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-
-    vsnprintf(msg_buff, 1024, format, args);
-    msg_buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-    int res = 0;
-    if (cb == nullptr)
-    {
-        FormatCallbackFunc fun;
-        {
-            std::lock_guard<std::mutex> guard(m_mutex);
-            fun = m_outFmtCb;
-        }
-        res = fun(buff, TEST_SYS_LOG_OUTPUT_BUFFER, m_status, msg_buff);
-    }
-    else
-    {
-        res = cb(buff, TEST_SYS_LOG_OUTPUT_BUFFER, dbg, level, obj, m_status, msg_buff);
-    }
-    buff[TEST_SYS_LOG_OUTPUT_BUFFER] = '\0';
-
-    if(m_file == NULL)
-    {
-        fprintf(stdout, "%s\n", buff);
-    }
-    else
-    {
-        fprintf(m_file, "%s\n", buff);
-    }
-    
-    free(msg_buff);
-    free(buff);
     return res;
 }
 
