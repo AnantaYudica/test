ad283c1de74aa2d475b1a3fff6b0f6c182bb2521
Ananta Yudica <nta040689@gmail.com>
2024-12-22 22:50:53 +0700

    Updated struct test::trait::binary::Definition

--- include/test/trait/binary/Definition.h
+++ include/test/trait/binary/Definition.h
@@ -16,7 +16,7 @@ namespace trait
 namespace binary
 {
 
-template<std::size_t N, std::size_t W = 1>
+template<std::size_t NOff = 0, std::size_t N = 1>
 struct Definition;
 
 } //binary
@@ -26,12 +26,12 @@ struct Definition;
 } //!test
 
 #define TEST_SYS_DBG_TYPE_PARAMETER_DEFINE_ARGS \
-    test::sys::dbg::type::Value<std::size_t, N>,\
-    test::sys::dbg::type::Value<std::size_t, W>
+    test::sys::dbg::type::Value<std::size_t, NOff>,\
+    test::sys::dbg::type::Value<std::size_t, N>
 
-template<std::size_t N, std::size_t W>
+template<std::size_t NOff, std::size_t N>
 TEST_SYS_DBG_TYPE_PARAMETER_DEFINE("test::trait::binary::Definition", 
-    test::trait::binary::Definition<N, W>);
+    test::trait::binary::Definition<NOff, N>);
 
 #undef TEST_SYS_DBG_TYPE_PARAMETER_DEFINE_ARGS
 
@@ -42,60 +42,70 @@ namespace trait
 namespace binary
 {
 
-template<std::size_t N, std::size_t W>
+template<std::size_t NOff, std::size_t N>
 struct Definition
 {
-    typedef typename Definition<((N + (W - 1)) / 8) * 8, 1>::ValueType 
+    typedef typename Definition<((NOff + (N - 1)) / 8) * 8, 1>::ValueType 
         ValueType;
     static constexpr std::size_t IsImplemented = 
-        Definition<((N + (W - 1)) / 8) * 8, 1>::IsImplemented;
+        Definition<((NOff + (N - 1)) / 8) * 8, 1>::IsImplemented;
     static constexpr std::size_t BitFloorIndex = 
-        Definition<((N + (W - 1)) / 8) * 8, 1>::BitFloorIndex;
+        Definition<((NOff + (N - 1)) / 8) * 8, 1>::BitFloorIndex;
     static constexpr std::size_t AllocationSize = 
-        Definition<((N + (W - 1)) / 8) * 8, 1>::AllocationSize;
-    static constexpr std::size_t BitSize = W;
-    static constexpr std::size_t BitBeginIndex = N;
-    static constexpr std::size_t BitEndIndex = N + W;
+        Definition<((NOff + (N - 1)) / 8) * 8, 1>::AllocationSize;
+    static constexpr std::size_t BitSize = N;
+    static constexpr std::size_t BitBeginIndex = NOff;
+    static constexpr std::size_t BitEndIndex = NOff + N;
     static constexpr ValueType Mask = 
-        ValueType(ValueType(~0x0) >> ((sizeof(ValueType) * 8) - W)) << N;
+        ValueType(ValueType(~0x0) >> ((sizeof(ValueType) * 8) - N)) << NOff;
     static constexpr ValueType NegationMask = ValueType(~Mask);
     static constexpr std::size_t BitCarryIndex = 
-        Definition<N + (W - 1)>::BitCarryIndex;
+        Definition<NOff + (N - 1)>::BitCarryIndex;
     static constexpr ValueType BitCarryMask = 
-        Definition<N + (W - 1)>::BitCarryMask;
+        Definition<NOff + (N - 1)>::BitCarryMask;
     Definition() = delete;
 };
 
 
-template<std::size_t N>
-struct Definition<N, 0>
+template<std::size_t NOff>
+struct Definition<NOff, 0>
 {
     static constexpr std::size_t IsImplemented = false;
+    typedef void* ValueType;
+    static constexpr std::size_t BitFloorIndex = 0;
+    static constexpr std::size_t AllocationSize = 0;
+    static constexpr std::size_t BitSize = 0;
+    static constexpr std::size_t BitBeginIndex = 0;
+    static constexpr std::size_t BitEndIndex = 0;
+    static constexpr ValueType Mask = NULL;
+    static constexpr ValueType NegationMask = NULL;
+    static constexpr std::size_t BitCarryIndex = 0;
+    static constexpr ValueType BitCarryMask = NULL;
     Definition() = delete;
 };
 
 
-template<std::size_t N>
-struct Definition<N, 1>
+template<std::size_t NOff>
+struct Definition<NOff, 1>
 {
     static constexpr std::size_t BitFloorIndex = 
-        (((N / 8) & 0x80) == 0x80 ? 0x80 * 8 : 0) +
-        (((N / 8) & 0xC0) == 0x40 ? 0x40 * 8 : 0) +
-        (((N / 8) & 0xE0) == 0x20 ? 0x20 * 8 : 0) +
-        (((N / 8) & 0xF0) == 0x10 ? 0x10 * 8 : 0) +
-        (((N / 8) & 0xF8) == 0x08 ? 0x08 * 8 : 0) +
-        (((N / 8) & 0xFC) == 0x04 ? 0x04 * 8 : 0) +
-        (((N / 8) & 0xFE) == 0x02 ? 0x02 * 8 : 0) +
-        (((N / 8) & 0xFF) == 0x01 ? 0x01 * 8 : 0);
+        (((NOff / 8) & 0x80) == 0x80 ? 0x80 * 8 : 0) +
+        (((NOff / 8) & 0xC0) == 0x40 ? 0x40 * 8 : 0) +
+        (((NOff / 8) & 0xE0) == 0x20 ? 0x20 * 8 : 0) +
+        (((NOff / 8) & 0xF0) == 0x10 ? 0x10 * 8 : 0) +
+        (((NOff / 8) & 0xF8) == 0x08 ? 0x08 * 8 : 0) +
+        (((NOff / 8) & 0xFC) == 0x04 ? 0x04 * 8 : 0) +
+        (((NOff / 8) & 0xFE) == 0x02 ? 0x02 * 8 : 0) +
+        (((NOff / 8) & 0xFF) == 0x01 ? 0x01 * 8 : 0);
     typedef typename Definition<BitFloorIndex, 1>::ValueType ValueType;
     static constexpr std::size_t IsImplemented =
          Definition<BitFloorIndex, 1>::IsImplemented;
     static constexpr std::size_t AllocationSize =
          Definition<BitFloorIndex, 1>::AllocationSize;
     static constexpr std::size_t BitSize = 1;
-    static constexpr std::size_t BitBeginIndex = N;
-    static constexpr std::size_t BitEndIndex = N + 1;
-    static constexpr ValueType Mask = ValueType(0x01) << N;
+    static constexpr std::size_t BitBeginIndex = NOff;
+    static constexpr std::size_t BitEndIndex = NOff + 1;
+    static constexpr ValueType Mask = ValueType(0x01) << NOff;
     static constexpr ValueType NegationMask = ValueType(~Mask);
     static constexpr std::size_t BitCarryIndex = 
         BitEndIndex % (AllocationSize * 8); 
@@ -177,6 +187,16 @@ template<>
 struct Definition<64, 1>
 {
     static constexpr bool IsImplemented = false;
+    typedef void* ValueType;
+    static constexpr std::size_t BitFloorIndex = 0;
+    static constexpr std::size_t AllocationSize = 0;
+    static constexpr std::size_t BitSize = 0;
+    static constexpr std::size_t BitBeginIndex = 0;
+    static constexpr std::size_t BitEndIndex = 0;
+    static constexpr ValueType Mask = NULL;
+    static constexpr ValueType NegationMask = NULL;
+    static constexpr std::size_t BitCarryIndex = 0;
+    static constexpr ValueType BitCarryMask = NULL;
     Definition() = delete;
 };
 
